a54e8ed293f5f480c7c28958bf2f9cf2263862a5
Seth Meranda <smeranda2@unl.edu>
2009-06-03 02:11:30 +0000

    Added a check to see if input has content from user using the back button. If so, then hide the label.

--- wdn/templates_3.0/scripts/search.js
+++ wdn/templates_3.0/scripts/search.js
@@ -2,6 +2,9 @@ var WDN_Search = function() {
 	return {
 		initialize : function() {
 			jQuery('#wdn_search_form fieldset input#q').focus(WDN_Search.hideLabel);
+			if (jQuery('#wdn_search_form fieldset input#q').val() != "") {
+				WDN_Search.hideLabel();
+			}
 		},
 		hideLabel : function() {
 			jQuery('#wdn_search_form fieldset label').hide();
