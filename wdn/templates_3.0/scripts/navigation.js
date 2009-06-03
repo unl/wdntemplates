4163de977185cb7e4a20accf56c022ae0fa48cd9
Brett Bieber <brett.bieber@gmail.com>
2009-06-03 15:48:43 +0000

    Add documentation to the navigation js.

--- wdn/templates_3.0/scripts/navigation.js
+++ wdn/templates_3.0/scripts/navigation.js
@@ -2,22 +2,51 @@
 var WDN_Navigation = function() {
 	var expandedHeight = 0;
 	return {
+		/**
+		 * Stores an expand/collapse timout.
+		 */
 		timeout : false,
+		
+		/**
+		 * The delay before expand occurs
+		 */
 		expandDelay : 400,
+		
+		/**
+		 * The delay before collapse occurs
+		 */
 		collapseDelay : 60,
+		
+		/**
+		 * Initialize the navigation, and determine what the correct state
+		 * should be (expanded/collapsed).
+		 * @todo determine what it should be
+		 */
 		initialize : function() {
 			jQuery('#wdn_navigation_wrapper,#breadcrumbs ul li').hover(WDN_Navigation.startExpandDelay,
 												WDN_Navigation.startCollapseDelay);
 			//WDN_Navigation.collapse();
 		},
+		
+		/**
+		 * Expand the navigation section.
+		 */
 		expand : function() {
+			/**
+			 * Because we don't know the height, slowly expand to a set height
+			 * then snap the rest of the way.
+			 */
 			jQuery('#navigation ul').animate({height:'198px'},60,0,function(){
 					jQuery('#navigation ul').css({height:'auto'});
 				});
-			jQuery('#navigation ul ul li').show();
+			jQuery('#navigation ul ul li').show(10);
 			jQuery('#navigation ul ul').show(300);
 			
 		},
+		
+		/**
+		 * Collapse the navigation
+		 */
 		collapse : function(animate) {
 			if (expandedHeight == 0) {
 				//expandedHeight = jQuery('#navigation').height();
@@ -27,10 +56,18 @@ var WDN_Navigation = function() {
 			jQuery('#navigation ul').animate({height:'50px'});
 			jQuery('#navigation ul ul li:not(:first-child)').hide(10);
 		},
+		
+		/**
+		 * Set a delay for expanding the navigation.
+		 */
 		startExpandDelay : function () {
 			clearTimeout(WDN_Navigation.timeout);
 			WDN_Navigation.timeout = setTimeout(WDN_Navigation.expand, WDN_Navigation.expandDelay);
 		},
+		
+		/**
+		 * Set a delay for collapsing the navigation.
+		 */
 		startCollapseDelay : function() {
 			clearTimeout(WDN_Navigation.timeout);
 			WDN_Navigation.timeout = setTimeout(WDN_Navigation.collapse, WDN_Navigation.collapseDelay);
