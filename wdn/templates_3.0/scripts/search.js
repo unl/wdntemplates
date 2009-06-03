74e394960a2662f429e7fa4fa4fffcf41fb8a7d6
Seth Meranda <smeranda2@unl.edu>
2009-06-03 21:03:00 +0000

    Added logic to check if input was empty on blur. If so, show the label again.

--- wdn/templates_3.0/scripts/search.js
+++ wdn/templates_3.0/scripts/search.js
@@ -4,10 +4,18 @@ var WDN_Search = function() {
 			jQuery('#wdn_search_form fieldset input#q').focus(WDN_Search.hideLabel);
 			if (jQuery('#wdn_search_form fieldset input#q').val() != "") {
 				WDN_Search.hideLabel();
-			}
+			};
+			jQuery('#wdn_search_form fieldset input#q').blur(function() {
+				if (jQuery('#wdn_search_form fieldset input#q').val() == "") {
+					WDN_Search.showLabel();
+				};
+			});
 		},
 		hideLabel : function() {
 			jQuery('#wdn_search_form fieldset label').hide();
+		},
+		showLabel : function() {
+			jQuery('#wdn_search_form fieldset label').show();
 		}
 	};
 }();
