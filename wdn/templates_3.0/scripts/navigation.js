bd8530b368a6f362418cb942b9836f667804037d
Brett Bieber <brett.bieber@gmail.com>
2009-06-04 15:03:29 +0000

    Dynamically add in the close button when navigation is initialized.

--- wdn/templates_3.0/scripts/navigation.js
+++ wdn/templates_3.0/scripts/navigation.js
@@ -26,6 +26,7 @@ var WDN_Navigation = function() {
 			jQuery('#wdn_navigation_wrapper,#breadcrumbs ul li').hover(WDN_Navigation.startExpandDelay,
 												WDN_Navigation.startCollapseDelay);
 			jQuery('#navigation ul:first li:nth-child(6) a:visible').css({width:'95%'});
+			jQuery('#navigation').append('<div id="navigation-close"></div>');
 			jQuery('#navigation-close').click(WDN_Navigation.startCollapseDelay);
 			//WDN_Navigation.collapse();
 		},
