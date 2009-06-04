7c1306860f0de6de0f9a6e70a00c76ea23405e2c
Seth Meranda <smeranda2@unl.edu>
2009-06-04 14:58:34 +0000

    added comments

--- wdn/templates_3.0/scripts/search.js
+++ wdn/templates_3.0/scripts/search.js
@@ -1,10 +1,16 @@
 var WDN_Search = function() {
 	return {
 		initialize : function() {
+		/**
+		 * Hide the label when the user starts a search
+		 */
 			jQuery('#wdn_search_form fieldset input#q').focus(WDN_Search.hideLabel);
 			if (jQuery('#wdn_search_form fieldset input#q').val() != "") {
 				WDN_Search.hideLabel();
 			};
+			/**
+			 * Show the label if the user abandons an empty search box
+			 */
 			jQuery('#wdn_search_form fieldset input#q').blur(function() {
 				if (jQuery('#wdn_search_form fieldset input#q').val() == "") {
 					WDN_Search.showLabel();
