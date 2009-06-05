e58c47bfcbedb102c89c6914cb3ab8a7b838d5a2
Eric Rasmussen <erasmussen2@unl.edu>
2009-06-05 22:57:10 +0000

    ajax weather content for the toolbar modal box

--- wdn/templates_3.0/scripts/toolbar.js
+++ wdn/templates_3.0/scripts/toolbar.js
@@ -1,5 +1,6 @@
 WDN.toolbar = function() {
     var expandedHeight = 0;
+    var weatherreq = new WDN.proxy_xmlhttp();
     return {
         initialize : function() {
             WDN.loadCSS('wdn/templates_3.0/scripts/plugins/colorbox/colorbox.css');
@@ -7,14 +8,42 @@ WDN.toolbar = function() {
                 WDN.loadCSS('wdn/templates_3.0/scripts/plugins/colorbox/colorbox-ie.css');
             }
             WDN.loadJS('wdn/templates_3.0/scripts/plugins/colorbox/jquery.colorbox.js', WDN.toolbar.colorboxSetup);
+            
+            jQuery('#header').append('<div class="hidden"><div id="feedcontent"></div></div>');
+        	jQuery('#header').append('<div class="hidden"><div id="weathercontent"></div></div>');
+        	jQuery('#header').append('<div class="hidden"><div id="calendarcontent"></div></div>');
+        	jQuery('#header').append('<div class="hidden"><div id="directorycontent"></div></div>');
+        	jQuery('#header').append('<div class="hidden"><div id="cameracontent"></div></div>');
         },
         colorboxSetup : function() {
             WDN.log('Setting up colorbox');
-            jQuery("#wdn_tool_links li a.camera").colorbox({width:"100%", height:"80%", iframe:true});
+            jQuery("#wdn_tool_links li a.feed").colorbox({width:"1000", height:"550", iframe:true});
+            jQuery("#wdn_tool_links li a.weather").colorbox({width:"1000", height:"550", inline:true, href:"#weathercontent"}, WDN.toolbar.displayUNLWeather);
+            jQuery("#wdn_tool_links li a.calendar").colorbox({width:"1000", height:"550", iframe:true});
+            jQuery("#wdn_tool_links li a.directory").colorbox({width:"1000", height:"550", iframe:true});
+            jQuery("#wdn_tool_links li a.camera").colorbox({width:"1000", height:"550", iframe:true});
             
         },
         getContent : function(url) {
             
-        }    
+        },  
+        displayUNLWeather : function() { 
+        	var weatherurl = "http://www.unl.edu/ucomm/templatedependents/templatesharedcode/scripts/current.html";
+        	weatherreq.open("GET", weatherurl, true);
+        	weatherreq.onreadystatechange = WDN.toolbar.updateWeatherResults;
+        	weatherreq.send(null);
+        },
+        updateWeatherResults : function() {
+        	if (weatherreq.readyState == 4) {
+        		if (weatherreq.status == 200) {
+        			document.getElementById("weathercontent").innerHTML = weatherreq.responseText;
+        		} else {
+        			document.getElementById("weathercontent").innerHTML = 'Error loading results.';
+        		}
+        	}
+        	wait = false;
+        	weatherreq = new WDN.proxy_xmlhttp();
+        }
+
     };
 }();
