4ec67c3b03ad118f8c1f7ed24b86b90be55590a1
Brett Bieber <brett.bieber@gmail.com>
2009-06-07 01:15:47 +0000

    Add a file for debugging so compressed versions don't have to be built every time.

---
+++ wdn/templates_3.0/scripts/debug.js
@@ -0,0 +1,19 @@
+
+var e = document.createElement("script");
+e.setAttribute('src', 'wdn/templates_3.0/scripts/wdn.js');
+e.setAttribute('type','text/javascript');
+document.getElementsByTagName('head').item(0).appendChild(e);
+
+
+var executeCallback = function() {
+    WDN.initializeTemplate();
+};
+
+e.onreadystatechange = function() {
+    if (e.readyState == "loaded" || e.readyState == "complete"){
+        executeCallback();
+    }
+};
+e.onload = executeCallback;
+        
+    
