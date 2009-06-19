192c848d6b4911190ffbc4b9dd85be5a517f5012
Brett Bieber <brett.bieber@gmail.com>
2009-06-19 14:27:02 +0000

    Consistently use 'tool' instead of 'plugin' for tools added to the toolbar.

--- wdn/templates_3.0/scripts/toolbar.js
+++ wdn/templates_3.0/scripts/toolbar.js
@@ -1,7 +1,7 @@
 WDN.toolbar = function() {
     var expandedHeight = 0;
     return {
-    	plugins : {},
+    	tools : {},
     	
         initialize : function() {
             WDN.loadCSS('wdn/templates_3.0/css/header/colorbox.css');
@@ -14,10 +14,18 @@ WDN.toolbar = function() {
         	*/
         	WDN.loadJS('wdn/templates_3.0/scripts/plugins/colorbox/jquery.colorbox.js', WDN.toolbar.colorboxSetup);
         },
+        toolTabsSetup : function() {
+        	jQuery('#cboxWrapper').append('<div id="tooltabs"><ul></ul></div>');
+        	WDN.toolbar.registerTool('weather', 'Weather', 1002, 510);
+        	WDN.toolbar.registerTool('events', 'Events', 1002, 550);
+        	WDN.toolbar.registerTool('peoplefinder', 'Peoplefinder', 1002, 550);
+        	WDN.toolbar.registerTool('webcams', 'Webcams', 1002, 390);
+        },
         colorboxSetup : function() {
             WDN.log('Setting up colorbox');
             WDN.toolbar.toolTabsSetup();
         },
+        
         /**
          * Just register a tool so we know about it.
          * 
@@ -29,7 +37,7 @@ WDN.toolbar = function() {
         registerTool : function(plugin_name, title, pwidth, pheight) {
         	jQuery('#tooltabs ul').append('<li class="'+plugin_name+'"><a href="#" class="'+plugin_name+'">'+title+'</a></li>');
         	jQuery("a."+plugin_name).colorbox({width:pwidth, height:pheight, inline:true, href:"#toolbarcontent"});
-        	jQuery("a."+plugin_name).click(function(){WDN.toolbar.toolTabsCurrent(plugin_name);});
+        	jQuery("a."+plugin_name).click(function(){WDN.toolbar.switchToolFocus(plugin_name);});
         },
         setToolContent : function(plugin_name, content) {
         	jQuery("#toolbarcontent").append('<div id="toolbar_'+plugin_name+'" class="toolbar_plugin">'+content+'</div>');
@@ -38,21 +46,19 @@ WDN.toolbar = function() {
         	eval('WDN.toolbar_'+type+'.display();');
         	jQuery("#cboxTitle").css({color:'#f2f2f2'}); //Hide the cboxTitle at the bottom
         },
-        toolTabsSetup : function() {
-        	jQuery('#cboxWrapper').append('<div id="tooltabs"><ul></ul></div>');
-        	WDN.toolbar.registerTool('weather', 'Weather', 1002, 510);
-        	WDN.toolbar.registerTool('events', 'Events', 1002, 550);
-        	WDN.toolbar.registerTool('peoplefinder', 'Peoplefinder', 1002, 550);
-        	WDN.toolbar.registerTool('webcams', 'Webcams', 1002, 390);
-        },
-        toolTabsCurrent : function(selected) {
+        /**
+         * Switches focus to a different tool.
+         * 
+         * @param string selected The tool to select
+         */
+        switchToolFocus : function(selected) {
         	jQuery('#toolbarcontent .toolbar_plugin').hide();
         	WDN.initializePlugin('toolbar_'+selected,
         			function(){
-        				if (!WDN.toolbar.plugins[selected]) {
+        				if (!WDN.toolbar.tools[selected]) {
 	        				eval('var content = WDN.toolbar_'+selected+'.setupToolContent();'); 
 	        				WDN.toolbar.setToolContent(selected, content);
-	        				WDN.toolbar.plugins[selected] = true;
+	        				WDN.toolbar.tools[selected] = true;
         				}
 		        		eval('WDN.toolbar_'+selected+'.initialize();');
 		        		WDN.toolbar.getContent(selected);
