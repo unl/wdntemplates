31ac8fae427a24955140bbd2e12e48f2c89a1426
Brett Bieber <brett.bieber@gmail.com>
2009-06-23 18:22:16 +0000

    Remove duplicate code.

--- wdn/templates_3.0/scripts/toolbar_feeds.js
+++ wdn/templates_3.0/scripts/toolbar_feeds.js
@@ -6,7 +6,7 @@ WDN.toolbar_feeds = function() {
     	feedAddress1 : 'http://www1.unl.edu/mediahub/?format=xml',
     	feedSite1 : 'http://www1.unl.edu/mediahub/',
     	feedName1 : 'UNL Media Hub',
-    	feedAddress2 : 'http://newsroom.unl.edu/releases/?&format=xml',
+    	feedAddress2 : 'http://newsroom.unl.edu/releases/?format=xml',
     	feedSite2 : 'http://newsroom.unl.edu/',
     	feedName2 : 'UNL Newsroom',
     	feedAddress3 : 'http://www.huskers.com/rss.dbml?db_oem_id=100&media=news',
@@ -35,10 +35,8 @@ WDN.toolbar_feeds = function() {
         	return '<div class="col left"><h3><span><a href="'+WDN.toolbar_feeds.feedSite1+'">'+WDN.toolbar_feeds.feedName1+'</a></span><a href="'+WDN.toolbar_feeds.feedAddress1+'"><span class="rssicon"></span></a></h3><ul id="wdn_feed_col1"></ul></div><div class="col middle1"><h3><span><a href="'+WDN.toolbar_feeds.feedSite2+'">'+WDN.toolbar_feeds.feedName2+'</a></span><a href="'+WDN.toolbar_feeds.feedAddress2+'"><span class="rssicon"></span></a></h3><ul id="wdn_feed_col2"></ul></div><div class="col middle2"><h3><span><a href="'+WDN.toolbar_feeds.feedSite3+'">'+WDN.toolbar_feeds.feedName3+'</a></span><a href="'+WDN.toolbar_feeds.feedAddress3+'"><span class="rssicon"></span></a></h3><ul id="wdn_feed_col3"></ul></div>';
         },
         display : function() {
-        	if(localRSS)
-        	{
-        		if(wehaveinsertedthelocalrsshtml == false)
-        		{
+        	if (localRSS) {
+        		if (wehaveinsertedthelocalrsshtml == false) {
         			jQuery('#toolbar_feeds').append('<div class="col right"><h3><span>'+localRSSTitle+'</span><a href="'+WDN.toolbar_feeds.feedAddressLocal+'"><span class="rssicon"></span></a></h3><ul id="wdn_feed_local"></ul></div>');
         			wehaveinsertedthelocalrsshtml = true;
         		}
@@ -47,29 +45,25 @@ WDN.toolbar_feeds = function() {
         		jQuery('#toolbar_feeds .col.middle2').css({width:"220px", padding:"0 10px 0 10px"});
         		WDN.loadJS('http://query.yahooapis.com/v1/public/yql?q=select+link%2Ctitle+from+rss+where+url%3D%27'+escape(WDN.toolbar_feeds.feedAddressLocal)+'%27+limit+7&format=json&callback=WDN.toolbar_feeds.showPostsLocal');
         	}
-        	else {}
         	WDN.loadJS('http://query.yahooapis.com/v1/public/yql?q=select+link%2Ctitle+from+rss+where+url%3D%27'+escape(WDN.toolbar_feeds.feedAddress1)+'%27+limit+7&format=json&callback=WDN.toolbar_feeds.showPosts1');
         	WDN.loadJS('http://query.yahooapis.com/v1/public/yql?q=select+link%2Ctitle+from+rss+where+url%3D%27'+escape(WDN.toolbar_feeds.feedAddress2)+'%27+limit+7&format=json&callback=WDN.toolbar_feeds.showPosts2');
         	WDN.loadJS('http://query.yahooapis.com/v1/public/yql?q=select+link%2Ctitle+from+rss+where+url%3D%27'+escape(WDN.toolbar_feeds.feedAddress3)+'%27+limit+7&format=json&callback=WDN.toolbar_feeds.showPosts3');
         },
         showPosts1 : function(data) {
-        	for (var i=0; i<data.query.count; i++) {
-                jQuery("#wdn_feed_col1").append("<li><a href='"+data.query.results.item[i].link+"'>"+data.query.results.item[i].title+"</a></li>");
-            }
+        	WDN.toolbar_feeds.showRSSItems('#wdn_feed_col1', data);
         },
         showPosts2 : function(data) {
-        	for (var i=0; i<data.query.count; i++) {
-                jQuery("#wdn_feed_col2").append("<li><a href='"+data.query.results.item[i].link+"'>"+data.query.results.item[i].title+"</a></li>");
-            }
+        	WDN.toolbar_feeds.showRSSItems('#wdn_feed_col2', data);
         },
         showPosts3 : function(data) {
-        	for (var i=0; i<data.query.count; i++) {
-                jQuery("#wdn_feed_col3").append("<li><a href='"+data.query.results.item[i].link+"'>"+data.query.results.item[i].title+"</a></li>");
-            }
+        	WDN.toolbar_feeds.showRSSItems('#wdn_feed_col3', data);
         },
         showPostsLocal : function(data) {
+        	WDN.toolbar_feeds.showRSSItems('#wdn_feed_local', data);
+        },
+        showRSSItems : function(ul_id, data) {
         	for (var i=0; i<data.query.count; i++) {
-                jQuery("#wdn_feed_local").append("<li><a href='"+data.query.results.item[i].link+"'>"+data.query.results.item[i].title+"</a></li>");
+                jQuery(ul_id).append("<li><a href='"+data.query.results.item[i].link+"'>"+data.query.results.item[i].title+"</a></li>");
             }
         }
     };
