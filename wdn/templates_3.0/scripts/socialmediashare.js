0fe90ec64543c9884f312219ea12f356e8dc3cc5
Eric Rasmussen <erasmussen2@unl.edu>
2009-07-07 20:59:48 +0000

    updates to the footer: changed social media icons to css per Brett's instructions

--- wdn/templates_3.0/scripts/socialmediashare.js
+++ wdn/templates_3.0/scripts/socialmediashare.js
@@ -3,7 +3,7 @@ WDN.socialmediashare = function() {
 		initialize : function() {
 			jQuery("#wdn_facebook").attr("href", "http://www.facebook.com/share.php?u="+window.location+"");
 			jQuery("#wdn_twitter").attr("href", "http://twitter.com/home?status=Currently reading "+window.location+"");
-			jQuery("#wdn_plurk").attr("href", "http://twitter.com/home?status=Currently reading "+window.location+"");
+			jQuery("#wdn_plurk").attr("href", "http://www.plurk.com/");
 			jQuery("#wdn_myspace").attr("href", "http://www.myspace.com/Modules/PostTo/Pages/?l=3&u="+window.location+"&t=University%20of%20Nebraska-Lincoln&c=");
 			jQuery("#wdn_digg").attr("href", "http://digg.com/submit?phase=2&url="+window.location+"&title=University%20of%20Nebraska-Lincoln");
 			
