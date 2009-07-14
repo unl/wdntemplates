89334aa98efaa375d94157948f234a828be32b40
Brett Bieber <brett.bieber@gmail.com>
2009-07-14 14:52:43 +0000

    Allow a local site search page to be defined.

--- wdn/templates_3.0/scripts/search.js
+++ wdn/templates_3.0/scripts/search.js
@@ -1,9 +1,9 @@
 WDN.search = function() {
 	return {
 		initialize : function() {
-		/**
-		 * Hide the label when the user starts a search
-		 */
+		   /**
+			* Hide the label when the user starts a search
+			*/
 			jQuery('#wdn_search_form fieldset input#q').focus(WDN.search.hideLabel);
 			if (jQuery('#wdn_search_form fieldset input#q').val() != "") {
 				WDN.search.hideLabel();
@@ -16,7 +16,23 @@ WDN.search = function() {
 					WDN.search.showLabel();
 				};
 			});
+			
+			var localSearch = WDN.search.hasLocalSearch();
+			if (localSearch) {
+				// Change form action to the local search
+				jQuery('#wdn_search_form').attr('action', localSearch);
+			}
 		},
+		hasLocalSearch : function() {
+        	var pagelinks = document.getElementsByTagName('link');
+        	for (var i=0;i<pagelinks.length;i++) {
+        	    relatt = pagelinks[i].getAttribute('rel');
+        	    if (relatt=='search') {
+        	    	return pagelinks[i].getAttribute('href');
+        	    }
+        	}
+        	return false;
+        },
 		hideLabel : function() {
 			jQuery('#wdn_search_form fieldset label').hide();
 		},
