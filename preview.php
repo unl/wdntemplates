ff917786c4cf038bc9299b6a06d8e5d81a35bf52
Brett Bieber <brett.bieber@gmail.com>
2009-07-14 15:38:59 +0000

    Add latest templates to the UNLTemplates.zip, updated compressed css and js

--- preview.php
+++ preview.php
@@ -75,7 +75,7 @@ foreach (array('maincontentarea','head') as $key) {
 echo ''.PHP_EOL;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><!-- InstanceBegin template="/Templates/fixed.dwt" codeOutsideHTMLIsLocked="false" -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <!--
     Membership and regular participation in the UNL Web Developer Network
