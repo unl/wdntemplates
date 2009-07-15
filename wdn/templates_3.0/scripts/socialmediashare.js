40d15574d2a0bb007522517a63250a861a1b7cbd
Eric Rasmussen <erasmussen2@unl.edu>
2009-07-15 18:41:56 +0000

    finalized social media icons

--- wdn/templates_3.0/scripts/socialmediashare.js
+++ wdn/templates_3.0/scripts/socialmediashare.js
@@ -2,7 +2,7 @@ WDN.socialmediashare = function() {
 	return {
 		initialize : function() {
 			jQuery("#wdn_facebook").attr("href", "http://www.facebook.com/share.php?u="+window.location+"");
-			jQuery("#wdn_twitter").attr("href", "http://twitter.com/home?status=Reading "+window.location+"");
+			jQuery("#wdn_twitter").attr("href", "http://twitter.com/home?status=Reading "+window.location+" #unl");
 			jQuery("#wdn_plurk").attr("href", "http://www.plurk.com/?status="+window.location+" from University%20of%20Nebraska-Lincoln&qualifier=shares");
 			jQuery("#wdn_myspace").attr("href", "http://www.myspace.com/Modules/PostTo/Pages/?l=3&u="+window.location+"&t=University%20of%20Nebraska-Lincoln: "+document.title+"");
 			jQuery("#wdn_digg").attr("href", "http://digg.com/submit?phase=2&url="+window.location+"&title=University%20of%20Nebraska-Lincoln: "+document.title+"");
