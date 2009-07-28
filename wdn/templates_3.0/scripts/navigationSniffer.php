a3b13e0ea3aaa68d9520d88e3d60a2feb20ac6da
Brett Bieber <brett.bieber@gmail.com>
2009-07-28 20:48:23 +0000

    tabs to spaces

--- wdn/templates_3.0/scripts/navigationSniffer.php
+++ wdn/templates_3.0/scripts/navigationSniffer.php
@@ -1,7 +1,7 @@
 <?php
 
 if (empty($_GET['u'])) {
-	throwError();
+    throwError();
 }
 
 /**
@@ -10,12 +10,12 @@ if (empty($_GET['u'])) {
 require_once 'Validate.php';
 $v = new Validate();
 if (!$v->uri($_GET['u'], array('allowed_schemes' => array('http', 'https')))) {
-	throwError();
+    throwError();
 }
 
 if (!preg_match('/\.unl\.edu/', $_GET['u'])
-	&& !preg_match('/quiltstudy\.org/', $_GET['u'])
-	&& !preg_match('/digital-community\.com/', $_GET['u'])
+    && !preg_match('/quiltstudy\.org/', $_GET['u'])
+    && !preg_match('/digital-community\.com/', $_GET['u'])
     && !preg_match('/huskeralum\.org/', $_GET['u'])
     && !preg_match('/huskeralum\.com/', $_GET['u'])) {
     throwError('Requested host is not allowed');
@@ -34,18 +34,18 @@ $parts = parse_url($_GET['u']);
 $clean_url = $parts['scheme'].'://'.$parts['host'].$parts['path'];
 
 if ($page = @file_get_contents($clean_url)) {
-	$dom = new DOMDocument();
-	if (!@$dom->loadHTML($page)) {
-		throwError('Bad html');
-	}
-	$xpath = new DOMXpath($dom);
-	$element = $xpath->query('//*[@id="navigation"]/ul[1]|//*[@id="navigation"]/*/ul[1]');
-	if ($element->length) {
-		$navigation = simplexml_import_dom($element->item(0));
-		echo removeRelativePaths($navigation->asXML(), $clean_url);
-	}
+    $dom = new DOMDocument();
+    if (!@$dom->loadHTML($page)) {
+        throwError('Bad html');
+    }
+    $xpath = new DOMXpath($dom);
+    $element = $xpath->query('//*[@id="navigation"]/ul[1]|//*[@id="navigation"]/*/ul[1]');
+    if ($element->length) {
+        $navigation = simplexml_import_dom($element->item(0));
+        echo removeRelativePaths($navigation->asXML(), $clean_url);
+    }
 } else {
-	throwError();
+    throwError();
 }
 
 function removeRelativePaths($html, $base_url)
@@ -56,7 +56,7 @@ function removeRelativePaths($html, $base_url)
     
     if (substr($base_url,-1) != '/') {
         $path = pathinfo($base_url_parts['path']);
-    	$new_base_url = substr($new_base_url, 0, strlen($new_base_url)-strlen($path['basename']));
+        $new_base_url = substr($new_base_url, 0, strlen($new_base_url)-strlen($path['basename']));
     }
 
     foreach ($needles as $needle) {
@@ -84,8 +84,8 @@ function removeRelativePaths($html, $base_url)
 
 function throwError($msg = '')
 {
-	header("HTTP/1.0 500 Could not process request");
-	echo $msg;
-	exit();
+    header("HTTP/1.0 500 Could not process request");
+    echo $msg;
+    exit();
 }
 
