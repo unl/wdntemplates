9ea10e16f1dcc6334fdd23eb3e21dcbdc3b80f6d
Seth Meranda <smeranda2@unl.edu>
2009-08-05 20:52:15 +0000

    script to handle wnd_tabs. Initial creation.

---
+++ wdn/templates_3.0/scripts/tabs.js
@@ -0,0 +1,30 @@
+WDN.tabs = function() {
+	return {
+		initialize : function() {
+		WDN.log ("tabs JS loaded");
+		//start by grabbing the #hash in the URL in case we need it	
+		if (window.location.hash) {
+				var hash = window.location.hash;
+				WDN.tabs.hideDiv('#'+hash);
+				jQuery('a[href='+hash+']').parent().addClass('selected');
+			} else {
+				var hash = false;
+			};
+		jQuery('ul.wdn_tabs:not(.disableSwitching) a').click(function() { //do something when a tab is clicked
+			jQuery(this).parent().addClass('selected').siblings().removeClass('selected');
+			var href = jQuery(this).attr("href");
+			window.location.hash = href;
+			WDN.log(href);
+			WDN.tabs.hideDiv(href);
+			return false;
+		});
+		},
+		
+		hideDiv: function(theDiv) {
+			WDN.log('hiding: '+theDiv);
+			jQuery('ul.wdn_tabs').siblings('div[id^=tab]').hide(); //hide all the content divs expect the one selected
+			jQuery('div'+theDiv).show();
+		}
+	};
+}();
+	
