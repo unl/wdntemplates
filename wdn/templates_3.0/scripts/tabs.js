92a45dc0871bad5c49d8694453806ab9d74b551f
Seth Meranda <smeranda2@unl.edu>
2009-08-10 20:07:14 +0000

    updated to the WDN namespace

--- wdn/templates_3.0/scripts/tabs.js
+++ wdn/templates_3.0/scripts/tabs.js
@@ -6,21 +6,21 @@ WDN.tabs = function() {
 		if (window.location.hash) {
 				var hash = window.location.hash;
 				WDN.tabs.hideDiv('#'+hash);
-				jQuery('a[href='+hash+']').parent().addClass('selected');
+				WDN.jQuery('a[href='+hash+']').parent().addClass('selected');
 			} else {
 				var hash = false;
-				jQuery('ul.wdn_tabs:not(.disableSwitching) li:first').addClass('selected');
-				var href = jQuery('ul.wdn_tabs:not(.disableSwitching) li:first a').attr('href');
+				WDN.jQuery('ul.wdn_tabs:not(.disableSwitching) li:first').addClass('selected');
+				var href = WDN.jQuery('ul.wdn_tabs:not(.disableSwitching) li:first a').attr('href');
 				WDN.tabs.hideDiv(href);
 			};
-		jQuery('ul.wdn_tabs li').each(function(){
-			var content = jQuery(this).children('a').text();
-			var contentTitle = jQuery(this).children('a').attr('href');
-			jQuery('div#'+contentTitle).prepend("<h5 class='yesprint'>"+content+"</h5>");
+			WDN.jQuery('ul.wdn_tabs li').each(function(){
+			var content = WDN.jQuery(this).children('a').text();
+			var contentTitle = WDN.jQuery(this).children('a').attr('href');
+			WDN.jQuery('div#'+contentTitle).prepend("<h5 class='yesprint'>"+content+"</h5>");
 		}),
-		jQuery('ul.wdn_tabs:not(.disableSwitching) a').click(function() { //do something when a tab is clicked
-			jQuery(this).parent().addClass('selected').siblings().removeClass('selected');
-			var href = jQuery(this).attr("href");
+		WDN.jQuery('ul.wdn_tabs:not(.disableSwitching) a').click(function() { //do something when a tab is clicked
+			WDN.jQuery(this).parent().addClass('selected').siblings().removeClass('selected');
+			var href = WDN.jQuery(this).attr("href");
 			window.location.hash = href;
 			WDN.tabs.hideDiv(href);
 			return false;
@@ -28,8 +28,8 @@ WDN.tabs = function() {
 		},
 		
 		hideDiv: function(theDiv) {
-			jQuery('div.wdn_tabs_content div').hide(); //hide all the content divs expect the one selected
-			jQuery('div'+theDiv).show();
+			WDN.jQuery('div.wdn_tabs_content div').hide(); //hide all the content divs expect the one selected
+			WDN.jQuery('div'+theDiv).show();
 		}
 	};
 }();
