e64f4890614a301b923edf31dc02b0f478d50bc2
Brett Bieber <brett.bieber@gmail.com>
2009-08-11 16:53:10 +0000

    Pack all the javascript together. This gives us about 10kb in savings. Still huge though.

--- wdn/templates_3.0/build/compress.php
+++ wdn/templates_3.0/build/compress.php
@@ -26,27 +26,25 @@ $pre_compressed = '';
 
 foreach (array('jquery') as $already_compressed) {
     $pre_compressed .= file_get_contents(dirname(__FILE__)."/../scripts/$already_compressed.js").PHP_EOL;
-    $loaded .= 'WDN.loadedJS["wdn/templates_3.0/scripts/'.$already_compressed.'.js"]=true;'.PHP_EOL;
 }
 
 require_once dirname(__FILE__).'/JavaScriptPacker.php';
 foreach ($files as $file) {
     $filename = dirname(__FILE__)."/../scripts/$file.js";
-    $packer = new JavaScriptPacker(file_get_contents($filename), 'Normal', true, false);
-    $all .= '//'.$file.PHP_EOL.$packer->pack();
+    $all .= file_get_contents($filename).PHP_EOL;
     if ($file == 'wdn') {
-        $all .= 'WDN.jQuery = jQuery.noConflict(true);'.PHP_EOL;
+        $all .= 'WDN.jQuery = jQuery.noConflict(true);WDN.loadedJS["wdn/templates_3.0/scripts/jquery.js"]=true;WDN.template_path = "/";'.PHP_EOL;
     }
-    $loaded .= 'WDN.loadedJS["wdn/templates_3.0/scripts/'.$file.'.js"]=true;'.PHP_EOL;
+    $all .= 'WDN.loadedJS["wdn/templates_3.0/scripts/'.$file.'.js"]=true;'.PHP_EOL;
 }
+$packer = new JavaScriptPacker($all, 'Normal', true, false);
+$all = $packer->pack();
 
-$loaded .= 'WDN.template_path = "/";'.PHP_EOL;
-
-$compressed = $pre_compressed.PHP_EOL.$all.PHP_EOL.$loaded.'WDN.initializeTemplate();';
+$compressed = $pre_compressed.PHP_EOL.$all.PHP_EOL.'WDN.initializeTemplate();';
 
 $compressed = '/**
  * This file is part of the UNL WDN templates.
- * http://wdn.unl.edu/
+ * @see http://wdn.unl.edu/
  * $Id$
  */'.PHP_EOL.$compressed;
 
