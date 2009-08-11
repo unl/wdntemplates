f1872072875f6a4043348720634ae3bf1ee8ac64
Brett Bieber <brett.bieber@gmail.com>
2009-08-11 17:08:57 +0000

    Add colorbox into toolbar so it can be de-bugged. Only load the weather background when it is called for.

--- wdn/templates_3.0/scripts/toolbar_weather.js
+++ wdn/templates_3.0/scripts/toolbar_weather.js
@@ -9,6 +9,7 @@ WDN.toolbar_weather = function() {
     		return '<div class="col"><h3>Local Weather</h3><div id="currentcond" class="toolbarMask"></div></div><div class="col middle"><h3>Lincoln Forecast</h3><div id="weatherforecast" class="toolbarMask"></div></div><div class="two_col"><h3>Local Radar</h3><div id="showradar"><a href="http://radar.weather.gov/radar_lite.php?rid=oax&product=N0R&overlay=11101111&loop=yes"><img src="'+WDN.template_path+'wdn/templates_3.0/css/images/transpixel.gif" /></a></div></div>';
     	},
         display : function() {
+    		WDN.jQuery('#showradar img').css({background:'url(http://radar.weather.gov/lite/N0R/OAX_loop.gif)  -5px -140px no-repeat'});
         	var weatherurl = "http://www.unl.edu/wdn/templates_3.0/scripts/weatherCurrent.html";
         	weatherreq.open("GET", weatherurl, true);
         	weatherreq.onreadystatechange = WDN.toolbar_weather.updateWeatherResults;
