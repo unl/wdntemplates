43cdaa6f7e6fbc6f4effef3509f11c481c8e36c1
Brett Bieber <brett.bieber@gmail.com>
2009-11-24 15:29:59 +0000

    Add some documentation, abstract out setting the user.

--- wdn/templates_3.0/scripts/idm.js
+++ wdn/templates_3.0/scripts/idm.js
@@ -1,14 +1,29 @@
 WDN.idm = function() {
 	return {
 		
+		/**
+		 * If populated, the public directory details for the logged in user
+		 * 
+		 * @var object
+		 */
 		user : false,
 		
+		/**
+		 * Initialize the IdM related scripts
+		 * 
+		 * @return void
+		 */
 		initialize : function() {
 			if (WDN.idm.isLoggedIn()) {
 				WDN.idm.displayNotice(WDN.idm.getUserID());
 			}
 		},
 		
+		/**
+		 * Checks if the user is logged in
+		 * 
+		 * @return bool
+		 */
 		isLoggedIn : function() {
 			var user = WDN.getCookie('sso');
 			if (user != null) {
@@ -17,11 +32,23 @@ WDN.idm = function() {
 			return false;
 		},
 		
+		/**
+		 * Returns the uid of the logged in user.
+		 * 
+		 * @return string
+		 */
 		getUserID : function() {
 			var user = WDN.getCookie('sso');
 			return user;
 		},
 		
+		/**
+		 * Update the SSO tab and display user info
+		 * 
+		 * @param string uid
+		 * 
+		 * @return void
+		 */
 		displayNotice : function(uid) {
 			if (WDN.jQuery('#wdn_identity_management').length == 0) {
 				WDN.jQuery('#header').append('<div id="wdn_identity_management"></div>');
@@ -31,13 +58,29 @@ WDN.idm = function() {
 			WDN.idm.getFriendlyName(uid);
 		},
 		
+		/**
+		 * Retrieves user info and updates the name.
+		 * 
+		 * @param string uid
+		 */
 		getFriendlyName : function(uid) {
+			WDN.idm.setUser(uid, function(){WDN.jQuery('#wdn_identity_management .username').html(WDN.idm.user.cn);});
+		},
+		
+		/**
+		 * Sets the user details
+		 * 
+		 * @param string   uid
+		 * @param function callback
+		 * 
+		 * @return void
+		 */
+		setUser : function(uid, callback) {
 			WDN.get('http://peoplefinder.unl.edu/service.php?format=json&uid='+uid, null, function(data, textStatus){
 				if (textStatus == 'success') {
 					eval('WDN.idm.user='+data);
-					WDN.jQuery('#wdn_identity_management .username').html(WDN.idm.user.cn);
+					if (callback) callback();
 				}
-				
 			});
 		}
 	};
