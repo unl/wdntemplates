0d0e5008dd5d509497d586b5d3f952452a4fe5df
Brett Bieber <brett.bieber@gmail.com>
2010-01-20 15:08:18 +0000

    Add links to webcam live view.

--- wdn/templates_3.0/scripts/toolbar_webcams.js
+++ wdn/templates_3.0/scripts/toolbar_webcams.js
@@ -7,7 +7,7 @@ WDN.toolbar_webcams = function() {
 
         },
         setupToolContent : function() {
-        	return '<div class="col left"><h3>Nebraska Union Plaza</h3><img class="frame" src="http://www.unl.edu/unlpub/cam/cam1.jpg" alt="Plaze Cam" id="webcamuri1" /></div><div class="col middle"><h3>Nebraska Union Rotunda</h3><img class="frame" src="http://www.unl.edu/unlpub/cam/cam2.jpg" alt="Rotunda Cam" id="webcamuri2" /></div><div class="col right"><h3>Nebraska East Union</h3><img class="frame" src="http://www.unl.edu/unlpub/cam/cam3.jpg" alt="East Union" id="webcamuri3" /></div>';
+        	return '<div class="col left"><h3>Nebraska Union Plaza <a href="http://www.unl.edu/unlpub/cam/cam1.shtml" class="external">(live view)</a></h3><img class="frame" src="http://www.unl.edu/unlpub/cam/cam1.jpg" alt="Plaze Cam" id="webcamuri1" /></div><div class="col middle"><h3>Nebraska Union Rotunda <a href="http://www.unl.edu/unlpub/cam/cam2.shtml" class="external">(live view)</a></h3><img class="frame" src="http://www.unl.edu/unlpub/cam/cam2.jpg" alt="Rotunda Cam" id="webcamuri2" /></div><div class="col right"><h3>Nebraska East Union <a href="http://www.unl.edu/unlpub/cam/cam3.shtml" class="external">(live view)</a></h3><img class="frame" src="http://www.unl.edu/unlpub/cam/cam3.jpg" alt="East Union" id="webcamuri3" /></div>';
         },
         display : function() {
     		document.getElementById('webcamuri1').src = unlwebcam;
