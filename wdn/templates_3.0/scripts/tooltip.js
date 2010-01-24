31a614d156454ee4c2b89911a849b7864d86e29b
Brett Bieber <brett.bieber@gmail.com>
2010-01-24 02:34:46 +0000

    Fix jslint errors

--- wdn/templates_3.0/scripts/tooltip.js
+++ wdn/templates_3.0/scripts/tooltip.js
@@ -1,6 +1,6 @@
 WDN.tooltip = function($) {
 	return {
-        initialize : function() {
+		initialize : function() {
 			WDN.log("initialize tooltip");
 			WDN.loadJS('wdn/templates_3.0/scripts/plugins/qtip/jquery.qtip.js', WDN.tooltip.idInit);
 		},
@@ -13,40 +13,40 @@ WDN.tooltip = function($) {
 		tooltipSetup : function(id) {
 			// Tooltips can be added to any links by calling this function with
 			// the container id and adding a 'title' attribute to the anchor tag or image tag
-			$('#'+id+' a.tooltip, #'+id+' img.tooltip').each(function() {	
-					  $(this).qtip({
-		
-					      content: $(this).attr('title'),
-					      show: { 
-						  	effect: { type: 'fade', length: 0 }
-					  	  },
-					  	  hide: { 
-							effect: { type: 'fade', length: 0 }
-						  },
-					      style: { 
-					    	  width: 200,
-					          padding: 5,
-					          'font-family': 'Arial, sans-serif',
-					          'font-size': '12px',
-					          background: '#faf7aa',
-					          color: '#434343',
-					          textAlign: 'center',
-					          border: {
-					             width: 1,
-					             radius: 5,
-					             color: '#f8e98e'
-					          },
-					          tip: 'bottomLeft',
-					          name: 'cream'
-					      },
-					      position: { 
-					    	  adjust: { screen: true },
-					      	  corner: { target: 'topMiddle', tooltip: 'bottomMiddle' }
-					      }
-					   });
-					  $(this).removeAttr('title');
-					  $(this).removeAttr('alt');
-					});
+			$('#'+id+' a.tooltip, #'+id+' img.tooltip').each(function() {
+				$(this).qtip({
+
+					content: $(this).attr('title'),
+					show: {
+						effect: { type: 'fade', length: 0 }
+					},
+					hide: {
+						effect: { type: 'fade', length: 0 }
+					},
+					style: {
+						width: 200,
+						padding: 5,
+						'font-family': 'Arial, sans-serif',
+						'font-size': '12px',
+						background: '#faf7aa',
+						color: '#434343',
+						textAlign: 'center',
+						border: {
+							width: 1,
+							radius: 5,
+							color: '#f8e98e'
+						},
+						tip: 'bottomLeft',
+						name: 'cream'
+					},
+					position: { 
+						adjust: { screen: true },
+						corner: { target: 'topMiddle', tooltip: 'bottomMiddle' }
+					}
+				});
+				$(this).removeAttr('title');
+				$(this).removeAttr('alt');
+			});
 		}
 	};
 }(WDN.jQuery);
