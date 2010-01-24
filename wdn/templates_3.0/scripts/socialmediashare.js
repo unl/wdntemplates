fb3b2b1e57afcbdd66696ba375b62b83bf5f8895
Brett Bieber <brett.bieber@gmail.com>
2010-01-24 02:40:02 +0000

    whitespace and a few jslint errors

--- wdn/templates_3.0/scripts/socialmediashare.js
+++ wdn/templates_3.0/scripts/socialmediashare.js
@@ -4,7 +4,7 @@ WDN.socialmediashare = function() {
             /* No need to use the attr method or even jQuery when working with single elements. Doing it the following way will speed up performance: */
             function e (id) {
                 return document.getElementById(id);
-            };
+            }
             try {
                 e("wdn_facebook").href = "http://www.facebook.com/share.php?u="+window.location+"";
                 e("wdn_twitter").href = "http://twitter.com/home?status=Reading: "+window.location+" %23UNL";
@@ -17,59 +17,59 @@ WDN.socialmediashare = function() {
                 e("wdn_reddit").href = "http://reddit.com/submit?url="+window.location+"&title=University%20of%20Nebraska-Lincoln: "+document.title+"";
                 e("wdn_stumbleupon").href = "http://www.stumbleupon.com/submit?url="+window.location+"&title=University%20of%20Nebraska-Lincoln: "+document.title+"";
                 e("wdn_newsvine").href = "http://www.newsvine.com/_tools/seed&save?popoff=0&u="+window.location+"&h=University%20of%20Nebraska-Lincoln: "+document.title+"";
-            } catch(e) {}
+            } catch(f) {}
             
             WDN.jQuery('a#wdn_createGoURL').click(function() {
-            	WDN.jQuery(this).remove();
-            	WDN.socialmediashare.createURL(window.location.href, 
-            		function(data) {
-            			WDN.jQuery('.socialmedia:last').after("<input type='text' id='goURLResponse' value='"+data+"' />");
-            			WDN.jQuery('#goURLResponse').focus().select();
-            		}
-            	);
-            	return false;
+                WDN.jQuery(this).remove();
+                WDN.socialmediashare.createURL(window.location.href, 
+                    function(data) {
+                        WDN.jQuery('.socialmedia:last').after("<input type='text' id='goURLResponse' value='"+data+"' />");
+                        WDN.jQuery('#goURLResponse').focus().select();
+                    }
+                );
+                return false;
             });
             //change the href to a goURL with GA campaign tagging
             var utm_source = "";
             var utm_campaign = "wdn_social";
             var utm_medium = "share_this";
             WDN.jQuery('.socialmedia a').click(function() {
-            	utm_source = WDN.jQuery(this).attr('id');
-            	gaTagging = "utm_campaign="+utm_campaign+"&utm_medium="+utm_medium+"&utm_source="+utm_source;
-            	//Let's build the URL to be shrunk
-            	thisPage = new String(window.location.href);
-            	if (thisPage.indexOf('?') != -1) { //check to see if has a ?, if not then go ahead with the ?. Otherwise add with &
-            		thisURL = thisPage+"&"+gaTagging;
-            	} else {
-            		thisURL = thisPage+"?"+gaTagging;
-            	}
-            	WDN.socialmediashare.createURL(
-            		thisURL,
-            		function(data) { //now we have a GoURL, let's replace the href with this new URL.
-            			var strLocation = new String(window.location);
-            			strLocation = strLocation.replace(/\?/g,'\\?');
-            			var regExpURL = new RegExp(strLocation);
-            			WDN.log("regExpURL: "+regExpURL);
-	            		var currentHref = WDN.jQuery('#'+utm_source).attr('href');
-	            		WDN.log("currentHref: "+currentHref);
-	            		WDN.jQuery('#'+utm_source).attr({href : currentHref.replace(regExpURL, data)});
-	                	window.location.href = WDN.jQuery('#'+utm_source).attr('href');
-            		}
-            	);
-            	return false;
+                utm_source = WDN.jQuery(this).attr('id');
+                gaTagging = "utm_campaign="+utm_campaign+"&utm_medium="+utm_medium+"&utm_source="+utm_source;
+                //Let's build the URL to be shrunk
+                thisPage = new String(window.location.href);
+                if (thisPage.indexOf('?') != -1) { //check to see if has a ?, if not then go ahead with the ?. Otherwise add with &
+                    thisURL = thisPage+"&"+gaTagging;
+                } else {
+                    thisURL = thisPage+"?"+gaTagging;
+                }
+                WDN.socialmediashare.createURL(
+                    thisURL,
+                    function(data) { //now we have a GoURL, let's replace the href with this new URL.
+                        var strLocation = new String(window.location);
+                        strLocation = strLocation.replace(/\?/g,'\\?');
+                        var regExpURL = new RegExp(strLocation);
+                        WDN.log("regExpURL: "+regExpURL);
+                        var currentHref = WDN.jQuery('#'+utm_source).attr('href');
+                        WDN.log("currentHref: "+currentHref);
+                        WDN.jQuery('#'+utm_source).attr({href : currentHref.replace(regExpURL, data)});
+                        window.location.href = WDN.jQuery('#'+utm_source).attr('href');
+                    }
+                );
+                return false;
             });
         },
-	    createURL : function(createThisURL, callback) { //function to create a GoURL
-	    	WDN.post(
-				"http://go.unl.edu/api_create.php", 
-				{theURL: createThisURL},
-				function(data) {
-					WDN.log("current URL: "+createThisURL+" GoURL: "+data);
-					if (data != "There was an error. ") {
-						callback(data);
-					}
-				}
-	    	);
-	    }
+        createURL : function(createThisURL, callback) { //function to create a GoURL
+            WDN.post(
+                "http://go.unl.edu/api_create.php", 
+                {theURL: createThisURL},
+                function(data) {
+                    WDN.log("current URL: "+createThisURL+" GoURL: "+data);
+                    if (data != "There was an error. ") {
+                        callback(data);
+                    }
+                }
+            );
+        }
     };
 }();
