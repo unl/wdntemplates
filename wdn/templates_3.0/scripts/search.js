878f2e8d9dfe899382ad7a9da67cee144453a838
Brett Bieber <brett.bieber@gmail.com>
2010-01-24 02:45:01 +0000

    fix jslint errors

--- wdn/templates_3.0/scripts/search.js
+++ wdn/templates_3.0/scripts/search.js
@@ -1,20 +1,20 @@
 WDN.search = function() {
 	return {
 		initialize : function() {
-		   /**
-			* Hide the label when the user starts a search
-			*/
+			/**
+			 * Hide the label when the user starts a search
+			 */
 			WDN.jQuery('#wdn_search_form fieldset input#q').focus(WDN.search.hideLabel);
 			if (WDN.jQuery('#wdn_search_form fieldset input#q').val() != "") {
 				WDN.search.hideLabel();
-			};
+			}
 			/**
 			 * Show the label if the user abandons an empty search box
 			 */
 			WDN.jQuery('#wdn_search_form fieldset input#q').blur(function() {
 				if (WDN.jQuery('#wdn_search_form fieldset input#q').val() == "") {
 					WDN.search.showLabel();
-				};
+				}
 			});
 			
 			var localSearch = WDN.search.hasLocalSearch();
@@ -23,25 +23,26 @@ WDN.search = function() {
 				WDN.jQuery('#wdn_search_form').attr('action', localSearch);
 			} else {
 				WDN.jQuery('#wdn_search_form').attr('action', 'http://www1.unl.edu/search/');
-				if (WDN.navigation.siteHomepage != false && WDN.navigation.siteHomepage != 'http://www.unl.edu/') {
+				if (WDN.navigation.siteHomepage !== false && WDN.navigation.siteHomepage != 'http://www.unl.edu/') {
 					// Add local site to the search parameters
 					WDN.jQuery('#wdn_search_form').append('<input type="hidden" name="u" value="'+WDN.navigation.siteHomepage+'" />');
 				}
 			}
 		},
 		hasLocalSearch : function() {
-        	var pagelinks = document.getElementsByTagName('link');
-        	for (var i=0;i<pagelinks.length;i++) {
-        	    relatt = pagelinks[i].getAttribute('rel');
-        	    typeatt = pagelinks[i].getAttribute('type');
-        	    if (relatt=='search' && typeatt != 'application/opensearchdescription+xml') {
-        	    	return pagelinks[i].getAttribute('href');
-        	    }
-        	}
-        	return false;
-        },
+			var pagelinks = document.getElementsByTagName('link');
+			var relatt, typeatt;
+			for (var i=0; i<pagelinks.length; i++) {
+				relatt = pagelinks[i].getAttribute('rel');
+				typeatt = pagelinks[i].getAttribute('type');
+				if (relatt=='search' && typeatt != 'application/opensearchdescription+xml') {
+					return pagelinks[i].getAttribute('href');
+				}
+			}
+			return false;
+		},
 		hideLabel : function() {
-        	WDN.jQuery('#wdn_search_form fieldset label').hide();
+			WDN.jQuery('#wdn_search_form fieldset label').hide();
 		},
 		showLabel : function() {
 			WDN.jQuery('#wdn_search_form fieldset label').show();
