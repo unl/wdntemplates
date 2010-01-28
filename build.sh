c941c295eb0aac7a346bd85a6f66a5c4d9545fce
Brett Bieber <brett.bieber@gmail.com>
2010-01-28 20:17:22 +0000

    Add zipping the affiliate psds to the build script

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-zip -r wdn wdn -x "*/.svn/*"
-zip -r UNLTemplates Templates sharedcode -x "*/.svn/*"
+zip -r wdn.zip wdn -x "*/.svn/*"
+echo "Done building the wdn.zip file."
+
+zip -r UNLTemplates.zip Templates sharedcode -x "*/.svn/*"
+echo "Done building the UNLTemplates.zip file."
+
+zip -r affiliatepsds.zip designfiles/affiliate -x "*/.svn/*"
+echo "Done building the affiliatepsds.zip file."
 
 php wdn/templates_3.0/build/compress.php
