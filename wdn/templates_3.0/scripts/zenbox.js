8297f51d4108aafbc09cca34d35da413aa85bd12
Seth Meranda <smeranda2@unl.edu>
2010-02-15 22:14:42 +0000

    js for zenbox show/hide

---
+++ wdn/templates_3.0/scripts/zenbox.js
@@ -0,0 +1,21 @@
+/**
+ * This plugin is intended for use on pages containing zenbox
+ * 
+ */
+WDN.zenbox = function() {
+	return {
+		initialize : function() {
+			WDN.log ('zenbox show hide initialized');
+			WDN.jQuery('.zenbox.showHide').each(function() {
+				WDN.jQuery(this).children('h3:first-child').nextAll().wrapAll('<div class="toggledContent">');
+				WDN.jQuery(this).children('h3').after('<a href="#" class="showHide">show/hide this</a>');
+			});
+			WDN.jQuery('.zenbox a.showHide').click(function() {
+				WDN.jQuery(this).siblings('.toggledContent').slideToggle(function() {
+					WDN.jQuery(this).siblings('a.showHide').toggleClass('show');
+					});
+				return false;
+			});
+		}
+	};
+}();
