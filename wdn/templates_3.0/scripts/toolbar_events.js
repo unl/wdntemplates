7fb35ee892d8d482af2e7c33668f4919bfef0be4
Seth Meranda <smeranda2@unl.edu>
2010-02-17 22:10:21 +0000

    use jQuery selectors to grab the link rel=events directly instead of looping through all the links.

--- wdn/templates_3.0/scripts/toolbar_events.js
+++ wdn/templates_3.0/scripts/toolbar_events.js
@@ -2,19 +2,12 @@ WDN.toolbar_events = function() {
 	var havelocalevents = false;
 	return {
 		initialize : function() {
-
+		
 		},
 		setupToolContent : function() {
-			var pagelinks = document.getElementsByTagName('link');
-			var relatt;
-			for (i=0;i<pagelinks.length;i++) {
-				relatt = pagelinks[i].getAttribute('rel');
-				if (relatt=='events') {
-					havelocalevents = true;
-					localeventshref = pagelinks[i].getAttribute('href');
-					localeventstitle = pagelinks[i].getAttribute('title');
-					return '<div class="col left"><h3><span>UNL Events <em><a href="http://events.unl.edu">(See the full calendar at events.unl.edu)</a></em></span><a href="http://events.unl.edu/upcoming/?format=rss"><span class="rssicon"></span></a>&nbsp;</h3><div id="allunlevents" class="toolbarMask"></div></div><div class="col right"><h3><span>Upcoming '+localeventstitle+' Events <em><a href="'+localeventshref+'/upcoming/">(See all events)</a></em></span><a href="'+localeventshref+'/upcoming/?format=rss"><span class="rssicon"></span></a>&nbsp;</h3><div id="localsiteevents" class="toolbarMask"></div></div>';
-				}
+			if (WDN.jQuery('link[rel=events]').attr('href') != null) {
+				havelocalevents = true;
+				return '<div class="col left"><h3><span>UNL Events <em><a href="http://events.unl.edu">(See the full calendar at events.unl.edu)</a></em></span><a href="http://events.unl.edu/upcoming/?format=rss"><span class="rssicon"></span></a>&nbsp;</h3><div id="allunlevents" class="toolbarMask"></div></div><div class="col right"><h3><span>Upcoming '+WDN.jQuery('link[rel=events]').attr('title')+' Events <em><a href="'+WDN.jQuery('link[rel=events]').attr('href')+'/upcoming/">(See all events)</a></em></span><a href="'+WDN.jQuery('link[rel=events]').attr('href')+'/upcoming/?format=rss"><span class="rssicon"></span></a>&nbsp;</h3><div id="localsiteevents" class="toolbarMask"></div></div>';
 			}
 			return '<div class="col left"><h3><span>UNL Events <em><a href="http://events.unl.edu">(See the full calendar at events.unl.edu)</a></em></span><a href="http://events.unl.edu/upcoming/?format=rss"><span class="rssicon"></span></a>&nbsp;</h3><div id="allunlevents" class="toolbarMask"></div></div><div class="col right"><h3><span>Upcoming UNL Events <em><a href="http://events.unl.edu/upcoming/">(See all events)</a></em></span><a href="http://events.unl.edu/upcoming/?format=rss"><span class="rssicon"></span></a>&nbsp;</h3><div id="localsiteevents" class="toolbarMask"></div></div>';
 		},
@@ -28,7 +21,7 @@ WDN.toolbar_events = function() {
 			var calurl = "http://events.unl.edu/?format=hcalendar";
 			WDN.get(calurl, null, WDN.toolbar_events.updateCalendarResults);
 			if (havelocalevents) {
-				calurl = localeventshref+'/upcoming/?format=hcalendar';
+				calurl = WDN.jQuery('link[rel=events]').attr('href')+'/upcoming/?format=hcalendar';
 				WDN.get(calurl, null, WDN.toolbar_events.updateLocalCalendarResults);
 			}
 			
