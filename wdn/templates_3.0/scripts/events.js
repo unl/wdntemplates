b8cec5647b320d847e145bbfb5ccd4c09b5b3d2e
Brett Bieber <brett.bieber@gmail.com>
2010-02-18 02:31:28 +0000

    Allow both the calendar and limit to be set with javascript. Fix some jslint errors.

--- wdn/templates_3.0/scripts/events.js
+++ wdn/templates_3.0/scripts/events.js
@@ -1,18 +1,25 @@
 /**
- * This plugin is intended to format calendar for pages. It takes the upcoming evnets feed found in the link rel=events
+ * This plugin is intended to format calendar for pages. It takes the upcoming events feed found in the link rel=events
  * 
  */
 WDN.events = function() {
-	var calURL = WDN.jQuery('link[rel=events]').attr('href')+'/upcoming/?format=hcalendar';
 	return {
+		limit : 10,
+		
+		calURL : false,
+		
 		initialize : function() {
-			if (WDN.jQuery('link[rel=events]').attr('href') && WDN.jQuery('#wdn_calendarDisplay').length != 0) {
-				WDN.loadCSS('/wdn/templates_3.0/css/content/events.css');
+			if (!this.calURL) {
+				this.calURL = WDN.jQuery('link[rel=events]').attr('href');
+			}
+			
+			if (WDN.jQuery('#wdn_calendarDisplay').length != 0) {
+				WDN.loadCSS('wdn/templates_3.0/css/content/events.css');
 				WDN.events.display();
 			}
 		},
 		display : function() {
-			WDN.get(calURL, null, function(content) {
+			WDN.get(this.calURL+'/upcoming/?format=hcalendar&limit='+this.limit, null, function(content) {
 				WDN.jQuery('#wdn_calendarDisplay').hide().html(content);
 				WDN.jQuery('#wdn_calendarDisplay abbr').each(
 						function() {
@@ -20,13 +27,19 @@ WDN.events = function() {
 							var eventdate = WDN.jQuery(this).html();
 							var month,day,time = '';
 							var xp = new RegExp(/[A-Za-z]{3,3}/);
-							if (xp.test(eventdate)) month = '<span class="month">'+xp.exec(eventdate)+'</span>';
+							if (xp.test(eventdate)) {
+								month = '<span class="month">'+xp.exec(eventdate)+'</span>';
+							}
 							eventdate.replace(xp.exec(eventdate),'');
 							xp = new RegExp(/[\d]+:[\d]+\s?[a,p]m/);
-							if (xp.test(eventdate)) time = '<span class="time">'+xp.exec(eventdate)+'</span>';
+							if (xp.test(eventdate)) {
+								time = '<span class="time">'+xp.exec(eventdate)+'</span>';
+							}
 							time = time.replace('0 ','0');
 							xp = new RegExp(/([\d]{1,2})[a-z]{2}/);
-							if (xp.test(eventdate)) day = '<span class="day">'+xp.exec(eventdate)[1]+'</span>';
+							if (xp.test(eventdate)) {
+								day = '<span class="day">'+xp.exec(eventdate)[1]+'</span>';
+							}
 							WDN.jQuery(this).replaceWith('<div>'+month+' '+day+' '+time+'</div>');
 						}
 				);
