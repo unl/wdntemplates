f5e28291e4c7e892bb8dbd8adf61782f0c937441
Brett Bieber <brett.bieber@gmail.com>
2010-04-27 14:45:35 +0000

    Add uncommitted file from Seth's zenforms. Add missing semicolon. Expand if statement for readability.

---
+++ wdn/templates_3.0/scripts/zenform.js
@@ -0,0 +1,22 @@
+/**
+ * This plugin is intended for use on pages containing zenforms
+ * 
+ */
+WDN.zenform = function() {
+	return {
+		
+		initialize : function() {
+			WDN.log('zenform initialized');
+			WDN.loadCSS('wdn/templates_3.0/css/content/zenform.css');
+			if (WDN.jQuery.browser.msie) {
+				WDN.zenform.fixIE();
+			}
+		},
+		
+		fixIE : function() {
+			WDN.jQuery('form.zenform legend').each(function() {
+				WDN.jQuery(this).wrapInner('<span class="fixIE" />');
+			});
+		}
+	};
+}();
