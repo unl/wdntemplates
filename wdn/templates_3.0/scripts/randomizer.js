33113208a86ba8a5be4c07a01e7762b7aacf7414
Kevin Abel <kabel2@unl.edu>
2010-07-13 21:19:13 +0000

    * Make the randomizer work with multiple instances

--- wdn/templates_3.0/scripts/randomizer.js
+++ wdn/templates_3.0/scripts/randomizer.js
@@ -4,9 +4,11 @@ WDN.randomizer = function() {
 			WDN.log ("randomizer JS loaded");
 			WDN.loadCSS('/wdn/templates_3.0/css/content/randomizer.css');
 			
-			var howManyDivs = WDN.jQuery('div.wdn_randomizer').children().size();
-			var randomNum = Math.floor(Math.random()*howManyDivs);
-			WDN.jQuery('div.wdn_randomizer div:eq(' + randomNum + ')').css("display", "block");
+			WDN.jQuery('.wdn_randomizer').each(function() {
+				var scope = WDN.jQuery(this).children();
+				var n = scope.size();
+				scope.eq(Math.floor(Math.random() * n)).css('display', 'block');
+			});
 			
 			return true;
 		}
