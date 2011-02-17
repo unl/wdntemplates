99786b40f1358b027a6f20b063816f9bfbfe7500
Seth Meranda <smeranda2@unl.edu>
2011-02-17 19:16:00 +0000

    Initial implementation of a directory autocomplete plugin for form inputs.

---
+++ wdn/templates_3.0/scripts/directory_autoComplete.js
@@ -0,0 +1,46 @@
+WDN.directory_autoComplete = function() {
+	return {
+		keymatch : false,
+		
+		initialize : function() {
+			WDN.loadJS('/wdn/templates_3.0/scripts/plugins/ui/jQuery.ui.js', function(){
+				WDN.loadCSS('/wdn/templates_3.0/css/content/autocomplete.css');
+				WDN.jQuery('input.wdn_directory_autoComplete').attr("autocomplete", "off");
+				WDN.jQuery('input.wdn_directory_autoComplete').autocomplete({
+					delay: 555,
+					minLength: 3,
+					source: function(request, response) {
+						WDN.jQuery.ajax({
+							delay: 555,
+			    			url: 'http://ucommmeranda.unl.edu/workspace/UNL_Peoplefinder/www/service.php?q='+request.term+'&format=json',
+			    			dataType: "json",
+			    			success: function(data) {
+			    				var rows = new Array();
+			    				for(var e=0; e<data.length; e++){
+		    						if (data[e].displayName){
+		    							rows[e] = {
+			    								label : '<span class="fn">'+data[e].displayName[0]+'</span>',
+			    								value : data[e].displayName[0],
+			    								key : data[e].uid
+			    						};
+		    						}
+			    					if(rows.length == 10){
+			    						break;
+			    					}
+							    };
+			    				response(rows);
+						    }
+			    		});
+			    	},
+					select: function(e, ui) {
+						WDN.directory_autoComplete.callback(ui.item.key);
+					}
+				});
+			});
+		},
+		
+		callback : function(uid) {
+			alert('You need your own callback! However, I\'ll send you the uid: ' + uid);
+		}
+	};
+}();
