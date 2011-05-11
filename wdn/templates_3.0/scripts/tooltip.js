679779e68c5ba9743cd8c9079029a4726856678d
Brett Bieber <brett.bieber@gmail.com>
2011-05-11 15:38:16 +0000

    Abstract qtip setup so other elements can be added after this plugin has been initialized.

--- wdn/templates_3.0/scripts/tooltip.js
+++ wdn/templates_3.0/scripts/tooltip.js
@@ -8,41 +8,44 @@ WDN.tooltip = function($) {
 			WDN.loadCSS('/wdn/templates_3.0/css/header/tooltip.css');
 			// Tooltips can only be used in the appropriate sections, and must have the correct class name and a title attribute
 			$('#wdn_tool_links .tooltip[title], #maincontent .tooltip[title], #footer .tooltip[title]').each(function() {
-				$(this).qtip({
-
-					content: $(this).attr('title'),
-					show: {
-						effect: { length: 0 }
-					},
-					hide: {
-						effect: { length: 0 }
-					},
-					style: {
-						tip: {
-							corner : 'bottomMiddle',
-							size : {x : 17, y : 10}
-						},
-						width : {
-							min : 100
-						},
-						'background' : 'url("/wdn/templates_3.0/css/header/images/qtip/defaultBG.png") repeat-x bottom',
-						border : {
-							color : '#f7e77c',
-							width : 2
-						},
-						'color' : '#504500'
-					},
-					position: { 
-						adjust: { 
-							screen: true,
-							y : -5
-						},
-						corner: { target: 'topMiddle', tooltip: 'bottomMiddle' }
-					}
-				});
-				$(this).removeAttr('title');
-				$(this).removeAttr('alt');
+				WDN.tooltip.addTooltip(this);
 			});
+		},
+		addTooltip: function(el) {
+			$(el).qtip({
+
+				content: $(el).attr('title'),
+				show: {
+					effect: { length: 0 }
+				},
+				hide: {
+					effect: { length: 0 }
+				},
+				style: {
+					tip: {
+						corner : 'bottomMiddle',
+						size : {x : 17, y : 10}
+					},
+					width : {
+						min : 100
+					},
+					'background' : 'url("/wdn/templates_3.0/css/header/images/qtip/defaultBG.png") repeat-x bottom',
+					border : {
+						color : '#f7e77c',
+						width : 2
+					},
+					'color' : '#504500'
+				},
+				position: { 
+					adjust: { 
+						screen: true,
+						y : -5
+					},
+					corner: { target: 'topMiddle', tooltip: 'bottomMiddle' }
+				}
+			});
+			$(el).removeAttr('title');
+			$(el).removeAttr('alt');
 		}
 	};
 }(WDN.jQuery);
