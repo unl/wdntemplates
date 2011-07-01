8e27f04a6359169e4d5e375ab5439a63f3f031ba
Brett Bieber <brett.bieber@gmail.com>
2011-07-01 04:55:49 +0000

    Minor updates to the qtip plugin that needed to be merged

--- wdn/templates_3.0/scripts/all_uncompressed.js
+++ wdn/templates_3.0/scripts/all_uncompressed.js
@@ -8614,41 +8614,44 @@ WDN.tooltip = function($) {
 		},
 		addTooltip : function($elements) {
 			$elements.each(function() {
-				$(this).qtip({
-
-					content: $(this).attr('title'),
-					show: {
-						effect: { length: 0 }
-					},
-					hide: {
-						effect: { length: 0 }
-					},
-					style: {
-						tip: {
-							corner : 'bottomMiddle',
-							size : {x : 17, y : 10}
-						},
-						width : {
-							min : 100
-						},
-						'background' : 'url("/wdn/templates_3.0/css/header/images/qtip/defaultBG.png") repeat-x bottom',
-						border : {
-							color : '#f7e77c',
-							width : 2
-						},
-						'color' : '#504500'
-					},
-					position: { 
-						adjust: { 
-							screen: true,
-							y : -5
-						},
-						corner: { target: 'topMiddle', tooltip: 'bottomMiddle' }
-					}
-				});
-				$(this).removeAttr('title');
-				$(this).removeAttr('alt');
+				WDN.tooltip.addTooltip(this);
 			});
+		},
+		addTooltip: function(el) {
+			$(el).qtip({
+
+				content: $(el).attr('title'),
+				show: {
+					effect: { length: 0 }
+				},
+				hide: {
+					effect: { length: 0 }
+				},
+				style: {
+					tip: {
+						corner : 'bottomMiddle',
+						size : {x : 17, y : 10}
+					},
+					width : {
+						min : 100
+					},
+					'background' : 'url("/wdn/templates_3.0/css/header/images/qtip/defaultBG.png") repeat-x bottom',
+					border : {
+						color : '#f7e77c',
+						width : 2
+					},
+					'color' : '#504500'
+				},
+				position: { 
+					adjust: { 
+						screen: true,
+						y : -5
+					},
+					corner: { target: 'topMiddle', tooltip: 'bottomMiddle' }
+				}
+			});
+			$(el).removeAttr('title');
+			$(el).removeAttr('alt');
 		}
 	};
 }(WDN.jQuery);
