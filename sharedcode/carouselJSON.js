415da0b768517e7b7e515f41d60f3e6ecc1d09aa
Seth Meranda <smeranda2@unl.edu>
2011-08-26 17:11:53 +0000

    Replace current carousel with a much lighter version that more easily supports responsive design. This uses a new jQuery Plugin, and we've updated styles to accomodate. The JS now fixes common deprecated markup to assist in transition.

---
+++ sharedcode/carouselJSON.js
@@ -0,0 +1,122 @@
+/*
+
+Create a JSON object for all the carousel files
+
+Put all the images and their associated data in this file. Do not include the default image used on the page (unless you want it repeated).
+
+*/
+WDN.carousel.buildFoundation(
+	[
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_bigten.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Proud Member of the Big Ten Conference",
+		},	
+		
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_LocatedInStateCapitol.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Located in the State Capitol",
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_NationallyRecognizedCriminalAndCivil.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Nationally Recognized Civil and Criminal Clinical Law Programs"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_TeachingIsOurPassion.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Teaching is our Passion"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_LowStudentTeacherRatio.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Low Student-Teacher Ratio"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_SportsLawConcentration.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Individualized Sports Law Concentration"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_CloseKnitCommunity.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Close-Knit Community"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_InteractionWithCommunityLeaders.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Interaction With Community Leaders"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_ExcellenceAtAffordableTuition.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Excellence At Affordable Tuition"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_NewlyRenovatedBuilding.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Newly Renovated Facilities"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_120Years.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "120 Years of Tradition and Excellence"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_StateOfTheArtTechnologyClassrooms.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "State of the Art Technology in Classrooms"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_NationallyProminentFaculty.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Nationally and Internationally Prominent Faculty"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_PremierLawPsychology.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Premier Law-Psychology Program in the Nation"
+		},	
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_AccessToResearchOne.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Access to Resources of a Research One University"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_SpaceTelecomLaw.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Innovative Space &amp; Telecommunications Program"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_SuccessfulEngagedAlumni.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Successful &amp; Engaged Alumni"
+		},
+		{ 
+			"img" : "http://law.unl.edu/splash/images/Reasons_CivicEngagement.jpg",
+			"link" : "/prospective/whynebraska.shtml",
+			"linktext" : "Click here to see all the reasons to attend Nebraska Law",
+			"title" : "Opportunities for Civic Engagement"
+		},
+	]				 
+)
+
+
--- wdn/templates_3.0/scripts/carousel.js
+++ wdn/templates_3.0/scripts/carousel.js
@@ -24,8 +24,10 @@ WDN.carousel = function() {
 		
 		initialize : function() {
 			WDN.log('carousel initialized');
-			WDN.loadCSS('/wdn/templates_3.0/css/content/carousel.css');
+			WDN.loadCSS('/wdn/templates_3.0/scripts/plugins/blueberry/blueberry.css');
 			WDN.loadJQuery(function(){
+				WDN.carousel.cleanUpDeprecated();
+				WDN.jQuery('#wdn_Carousel img').removeAttr('width').removeAttr('height');
 				if (WDN.carousel.supportJSON) {
 					WDN.loadJS(WDN.carousel.supportJSON);
 				} else {
@@ -34,14 +36,24 @@ WDN.carousel = function() {
 			});
 		},
 		
-		start : function() {
-			WDN.loadJS('wdn/templates_3.0/scripts/plugins/infinitecarousel/jquery.infinitecarousel.min.js', function () {
-				WDN.jQuery('#wdn_Carousel').infiniteCarousel({imagePath: '/wdn/templates_3.0/css/content/images/carousel/', 
-					displayThumbnails: WDN.carousel.displayThumbnails, textholderHeight: WDN.carousel.textholderHeight
+		cleanUpDeprecated : function() {
+			if (WDN.jQuery('#wdn_Carousel .wdn_slides').length) {
+				return;
+			} else {
+				WDN.jQuery('#wdn_Carousel').parent('.three_col').css({'padding' : 0});
+				WDN.jQuery('#wdn_Carousel > ul:first').addClass('wdn_slides').children('li').each(function(){
+					console.log(WDN.jQuery(this));
+					WDN.jQuery(this).wrapInner('<div />');
 				});
-				if (WDN.jQuery(window).width() <= 480){
-					WDN.jQuery('#wdn_Carousel img').css({'top' : (WDN.carousel.imageHeight() * -1), 'left' : (WDN.carousel.imageWidth())});
+				if (WDN.jQuery('#wdn_Carousel').parent(".zenbox")){
+					WDN.jQuery('#wdn_Carousel').unwrap(".zenbox");
 				}
+			}
+		},
+		
+		start : function() {
+			WDN.loadJS('wdn/templates_3.0/scripts/plugins/blueberry/blueberry.v0.4.js', function () {
+				WDN.jQuery('#wdn_Carousel').blueberry();
 			});
 		},
 		
@@ -55,7 +67,7 @@ WDN.carousel = function() {
 			WDN.log('remainingObjects: '+remainingObjects);
 			for (var i=startingObject; i<totalObjects; i++) { //loop through the objects starting with the random one
 				WDN.log('first loop: '+data[i].img);
-				WDN.jQuery("#wdn_Carousel ul").prepend('<li><img src="'+data[i].img+'" height="'+WDN.carousel.imageHeight()+'" width="'+WDN.carousel.imageWidth()+'" /><p>'+data[i].title +'<a href="'+data[i].link+'">'+data[i].linktext+'</a></p>');
+				WDN.jQuery("#wdn_Carousel ul").prepend('<li><div><img src="'+data[i].img+'" height="'+WDN.carousel.imageHeight()+'" width="'+WDN.carousel.imageWidth()+'" /><p>'+data[i].title +'<a href="'+data[i].link+'">'+data[i].linktext+'</a></p></div>');
 				if (i>=(startingObject + WDN.carousel.numberToDisplay - 2)) {
 					break;
 				}
@@ -63,7 +75,7 @@ WDN.carousel = function() {
 			if (remainingObjects > 0) { //now let's go back to the begining and grab more objects to fill in for the remaining spots.
 				for (var j=0; j<remainingObjects; j++) {
 					WDN.log('second loop: '+data[j].img);
-					WDN.jQuery("#wdn_Carousel ul").prepend('<li><img src="'+data[j].img+'" height="'+WDN.carousel.imageHeight()+'" width="'+WDN.carousel.imageWidth()+'" /><p>'+data[j].title +'<a href="'+data[j].link+'">'+data[j].linktext+'</a></p>');
+					WDN.jQuery("#wdn_Carousel ul").prepend('<li><div><img src="'+data[j].img+'" height="'+WDN.carousel.imageHeight()+'" width="'+WDN.carousel.imageWidth()+'" /><p>'+data[j].title +'<a href="'+data[j].link+'">'+data[j].linktext+'</a></p></div>');
 				}
 			}
 			WDN.carousel.start();
