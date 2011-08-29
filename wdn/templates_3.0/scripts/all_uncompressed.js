5944153b84b57e376ab44ecedbd8f89ad01b5c47
Brett Bieber <brett.bieber@gmail.com>
2011-08-29 19:32:03 +0000

    Add UNLAlert url fixes and support for https alert monitoring.

--- wdn/templates_3.0/scripts/all_uncompressed.js
+++ wdn/templates_3.0/scripts/all_uncompressed.js
@@ -14306,16 +14306,13 @@ WDN.unlalert = function() {
 	
 	return {
 		
-		data_url : 'http://alert1.unl.edu/json/unlcap.js',
+		data_url : document.location.protocol+'//alert.unl.edu/json/unlcap.js',
 		//data_url : 'http://ucommbieber.unl.edu/ucomm/templatedependents/templatesharedcode/scripts/alert.master.server.js',
 		
 		initialize : function()
 		{
 			//debug statement removed
-			if ("https:" != document.location.protocol) {
-				// Don't break authenticated sessions
-				WDN.unlalert.checkIfCallNeeded();
-			}
+			WDN.unlalert.checkIfCallNeeded();
 		},
 		
 		checkIfCallNeeded: function() {
