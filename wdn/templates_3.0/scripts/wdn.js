ee722ba508c538dc43a2b2a08cd3dc5557a5e2dc
Eric Rasmussen <erasmussen2@unl.edu>
2011-10-26 20:32:39 +0000

    Whitespace, coding standards consistency, use of $ for WDN.jQuery in WDN.request()

--- wdn/templates_3.0/scripts/wdn.js
+++ wdn/templates_3.0/scripts/wdn.js
@@ -2,21 +2,21 @@
  * This file contains the WDN template javascript code.
  */
 var _gaq = _gaq || [];
-var WDN = function() {
+var WDN = (function() {
 	var loadingJS = {};
 	return {
 		/**
 		 * This stores what javascript files have been loaded already
 		 */
-		loadedJS : {},
+		loadedJS: {},
 
 		/**
 		 * This variable stores the path to the template files.
 		 * It can be set to /, http://www.unl.edu/, or nothing.
 		 */
-		template_path : '',
+		template_path: '',
 
-		/*
+		/**
 		 * Loads an external JavaScript file.
 		 *
 		 * @param {string} url
@@ -24,8 +24,7 @@ var WDN = function() {
 		 * @param {boolean} checkLoaded (optional) - if false, the JS will be loaded without checking whether it's already been loaded
 		 * @param {boolean} callbackIfLoaded (optional) - if false, the callback will not be executed if the JS has already been loaded
 		 */
-
-		loadJS : function(url,callback,checkLoaded,callbackIfLoaded) {
+		loadJS: function (url,callback,checkLoaded,callbackIfLoaded) {
 			if (url.match(/^\/?wdn\/templates_3\.0/)) {
 				// trim off the leading slash
 				if (url.charAt(0) == '/') {
@@ -34,7 +33,7 @@ var WDN = function() {
 				url = WDN.template_path+url;
 			}
 
-			if ((arguments.length>2 && checkLoaded === false) || !WDN.loadedJS[url]){
+			if ((arguments.length>2 && checkLoaded === false) || !WDN.loadedJS[url]) {
 				if (url in loadingJS) {
 					if (callback) {
 						loadingJS[url].push(callback);
@@ -51,7 +50,7 @@ var WDN = function() {
 				if (callback) {
 					loadingJS[url].push(callback);
 				}
-				var executeCallback = function() {
+				var executeCallback = function () {
 					WDN.loadedJS[url] = 1;
 					if (loadingJS[url]) {
 						WDN.log("finished loading JS file: " + url);
@@ -62,8 +61,8 @@ var WDN = function() {
 					}
 				};
 
-				e.onreadystatechange = function() {
-					if (e.readyState == "loaded" || e.readyState == "complete"){
+				e.onreadystatechange = function () {
+					if (e.readyState == "loaded" || e.readyState == "complete") {
 						executeCallback();
 					}
 				};
@@ -71,7 +70,7 @@ var WDN = function() {
 
 			} else {
 				WDN.log("JS file already loaded: " + url);
-				if ((arguments.length > 3 && callbackIfLoaded === false) || !callback){
+				if ((arguments.length > 3 && callbackIfLoaded === false) || !callback) {
 					return;
 				}
 				callback();
@@ -81,7 +80,7 @@ var WDN = function() {
 		/**
 		 * Load an external css file.
 		 */
-		loadCSS : function(url) {
+		loadCSS: function (url) {
 			if (url.match(/^\/?wdn\/templates_3\.0/)) {
 				// trim off the leading slash
 				if (url.charAt(0) == '/') {
@@ -92,7 +91,7 @@ var WDN = function() {
 			var e = document.createElement("link");
 			e.href = url;
 			e.rel = "stylesheet";
-			e.type="text/css";
+			e.type = "text/css";
 			document.getElementsByTagName("head")[0].appendChild(e);
 		},
 
@@ -100,7 +99,7 @@ var WDN = function() {
 		 * This function is called on page load to initialize template related
 		 * data.
 		 */
-		initializeTemplate : function() {
+		initializeTemplate: function () {
 			//gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 			//WDN.loadJS(gaJsHost + "google-analytics.com/ga.js");
 			WDN.loadCSS('wdn/templates_3.0/css/script.css');
@@ -114,7 +113,7 @@ var WDN = function() {
 		 *
 		 * @param callback Called when the document is ready
 		 */
-		loadJQuery : function(callback) {
+		loadJQuery: function (callback) {
 			WDN.loadJS('wdn/templates_3.0/scripts/jquery.js', function(){
 				if (!WDN.jQuery) {
 					WDN.jQuery = jQuery.noConflict(true);
@@ -129,7 +128,7 @@ var WDN = function() {
 		 * All things needed by jQuery can be put in here, and they'll get
 		 * executed when jquery is loaded
 		 */
-		jQueryUsage : function() {
+		jQueryUsage: function () {
 			WDN.initializePlugin('analytics');
 			if (WDN.jQuery('body').hasClass('mobile')) {
 				return;
@@ -154,13 +153,13 @@ var WDN = function() {
 		 *
 		 * To see, open firebug's console.
 		 */
-		log: function(data) {
+		log: function (data) {
 			if ("console" in window && "log" in console) {
 				console.log(data);
 			}
 		},
 
-		browserAdjustments : function() {
+		browserAdjustments: function () {
 			if (WDN.jQuery.browser.msie && (WDN.jQuery.browser.version == '6.0') && (!navigator.userAgent.match(/MSIE 8.0/))) {
 				WDN.jQuery('body').prepend('<div id="wdn_upgrade_notice"></div>');
 				fetchURLInto('http://www.unl.edu/wdn/templates_3.0/includes/browserupgrade.html', 'wdn_upgrade_notice');
@@ -170,16 +169,16 @@ var WDN = function() {
 				WDN.loadCSS('wdn/templates_3.0/css/content/columns.css');
 			}
 
-            if ((navigator.userAgent.match(/applewebkit/i) && !navigator.userAgent.match(/Version\/[34]/)) ||
-                (navigator.userAgent.match(/firefox/i) && (navigator.userAgent.match(/firefox\/[12]/i) || navigator.userAgent.match(/firefox\/3.[01234]/i))) ||
-                (navigator.userAgent.match(/msie/i))){
-                // old browser needs help zebra striping
-                WDN.jQuery('.zentable tbody tr:nth-child(odd)').addClass('rowOdd');
-                WDN.jQuery('.zentable tbody tr:nth-child(even)').addClass('rowEven');
-            }
+			if ((navigator.userAgent.match(/applewebkit/i) && !navigator.userAgent.match(/Version\/[34]/)) ||
+				(navigator.userAgent.match(/firefox/i) && (navigator.userAgent.match(/firefox\/[12]/i) || navigator.userAgent.match(/firefox\/3.[01234]/i))) ||
+				(navigator.userAgent.match(/msie/i))) {
+				// old browser needs help zebra striping
+				WDN.jQuery('.zentable tbody tr:nth-child(odd)').addClass('rowOdd');
+				WDN.jQuery('.zentable tbody tr:nth-child(even)').addClass('rowEven');
+			}
 		},
 
-		screenAdjustments : function() {
+		screenAdjustments: function () {
 			if (screen.width<=1024) {
 				WDN.jQuery('body').css({'background':'#e0e0e0'});
 				if (WDN.jQuery.browser.msie) {
@@ -188,9 +187,9 @@ var WDN = function() {
 			}
 		},
 
-		contentAdjustments : function () {
+		contentAdjustments: function () {
 			WDN.jQuery('#footer_floater').css("zoom", 1);
-			WDN.jQuery('#maincontent p.caption, #footer p.caption').each(function(i){
+			WDN.jQuery('#maincontent p.caption, #footer p.caption').each(function(i) {
 				if (WDN.jQuery(this).height()>20) {
 					WDN.jQuery(this).css({border:'1px solid #DDD',marginleft:'0'});
 				}
@@ -207,7 +206,7 @@ var WDN = function() {
 			});
 		},
 
-		initializePlugin:function (plugin, callback) {
+		initializePlugin: function (plugin, callback) {
 			if (!callback) {
 				callback = function () {
 					if ("initialize" in WDN[plugin]) {
@@ -221,7 +220,7 @@ var WDN = function() {
 			WDN.loadJS('wdn/templates_3.0/scripts/'+plugin+'.js', callback);
 		},
 
-		setCookie : function(name, value, seconds, path, domain) {
+		setCookie: function (name, value, seconds, path, domain) {
 			var expires = "";
 			if (seconds) {
 				var date = new Date();
@@ -239,7 +238,7 @@ var WDN = function() {
 			document.cookie = name+"="+value+expires+";path="+path+";domain="+domain;
 		},
 
-		getCookie : function(name) {
+		getCookie: function (name) {
 			var nameEQ = name + "=";
 			var ca = document.cookie.split(';');
 			for(var i=0;i < ca.length;i++) {
@@ -261,50 +260,49 @@ var WDN = function() {
 		 * @param {string} base_url The base to use
 		 */
 		toAbs: function (link, base_url) {
-		  if (typeof link == 'undefined')
-			  return;
-		  var lparts = link.split('/');
-		  if (/http:|https:|ftp:/.test(lparts[0])) {
-		    // already abs, return
-		    return link;
-		  }
+			if (typeof link == 'undefined')
+				return;
+			var lparts = link.split('/');
+			if (/http:|https:|ftp:/.test(lparts[0])) {
+				// already abs, return
+				return link;
+			}
 
-		  var i, hparts = base_url.split('/');
-		  if (hparts.length > 3) {
-		    hparts.pop(); // strip trailing thingie, either scriptname or blank
-		  }
+			var i, hparts = base_url.split('/');
+			if (hparts.length > 3) {
+				hparts.pop(); // strip trailing thingie, either scriptname or blank
+			}
 
-		  if (lparts[0] === '') { // like "/here/dude.png"
-		    base_url = hparts[0] + '//' + hparts[2];
-		    hparts = base_url.split('/'); // re-split host parts from scheme and domain only
-		    delete lparts[0];
-		  }
+			if (lparts[0] === '') { // like "/here/dude.png"
+				base_url = hparts[0] + '//' + hparts[2];
+				hparts = base_url.split('/'); // re-split host parts from scheme and domain only
+				delete lparts[0];
+			}
 
-		  for(i = 0; i < lparts.length; i++) {
-		    if (lparts[i] === '..') {
-		      // remove the previous dir level, if exists
-		      if (typeof lparts[i - 1] !== 'undefined') {
-		        delete lparts[i - 1];
-		      } else if (hparts.length > 3) { // at least leave scheme and domain
-		        hparts.pop(); // strip one dir off the host for each /../
-		      }
-		      delete lparts[i];
-		    }
-		    if(lparts[i] === '.') {
-		      delete lparts[i];
-		    }
-		  }
+			for(i = 0; i < lparts.length; i++) {
+				if (lparts[i] === '..') {
+					// remove the previous dir level, if exists
+					if (typeof lparts[i - 1] !== 'undefined') {
+						delete lparts[i - 1];
+					} else if (hparts.length > 3) { // at least leave scheme and domain
+						hparts.pop(); // strip one dir off the host for each /../
+					}
+					delete lparts[i];
+				}
+				if (lparts[i] === '.') {
+					delete lparts[i];
+				}
+			}
 
-		  // remove deleted
-		  var newlinkparts = [];
-		  for (i = 0; i < lparts.length; i++) {
-		    if (typeof lparts[i] !== 'undefined') {
-		      newlinkparts[newlinkparts.length] = lparts[i];
-		    }
-		  }
+			// remove deleted
+			var newlinkparts = [];
+			for (i = 0; i < lparts.length; i++) {
+				if (typeof lparts[i] !== 'undefined') {
+					newlinkparts[newlinkparts.length] = lparts[i];
+				}
+			}
 
-		  return hparts.join('/') + '/' + newlinkparts.join('/');
-
+			return hparts.join('/') + '/' + newlinkparts.join('/');
 		},
 
 		stringToXML: function (string) {
@@ -314,19 +312,17 @@ var WDN = function() {
 					doc = new ActiveXObject('Microsoft.XMLDOM');
 					doc.async = 'false';
 					doc.loadXML(string);
-				}
-				else {
+				} else {
 					var parser = new DOMParser();
 					doc = parser.parseFromString(string, 'text/xml');
 				}
-			}
-			catch(e) {
+			} catch(e) {
 				WDN.log('ERROR parsing XML string for conversion: ' + e);
 			}
 			return doc;
 		},
 
-		/*
+		/**
 		 * This function powers the functions WDN.get and WDN.post and provides cross browser
 		 * support for XHRs and cross-domain requests.
 		 *
@@ -336,7 +332,6 @@ var WDN = function() {
 		 * @param {string=} type [opt] The expected data type of the response
 		 * @param {string=} method The method to perform the request with. Supported are GET and POST
 		 */
-
 		request: function (url, data, callback, type, method) {
 			WDN.log("Using WDN.request");
 			var $ = WDN.jQuery;
@@ -355,12 +350,10 @@ var WDN = function() {
 			method = method.toLowerCase();
 			// first, try using jQuery.get or jQuery.post
 			try {
-				if (url.match(/^https?:/)
-					&& (url.match(/:\/\/(.[^\/]+)/)[1] != window.location.host)) {
+				if (url.match(/^https?:/) && (url.match(/:\/\/(.[^\/]+)/)[1] != window.location.host)) {
 					WDN.log('This is a cross-origin request');
 					// IE9 fails silently, so force it to throw an error and use XDR
-					if (WDN.jQuery.browser.msie
-						&& parseInt(WDN.jQuery.browser.version, 10) < 10) {
+					if ($.browser.msie && parseInt($.browser.version, 10) < 10) {
 						WDN.log('IE detected. Raising an error to force XDR or proxy.');
 						throw("IE, use XDR or proxy");
 					}
@@ -386,7 +379,7 @@ var WDN = function() {
 					WDN.log("WDN.request: Converting data object to query string.");
 					params = '';
 					for (var key in data) {
-					    params = params+'&'+key+'='+data[key];
+						params = params+'&'+key+'='+data[key];
 					}
 				}
 
@@ -450,7 +443,6 @@ var WDN = function() {
 					}
 				}
 			}
-
 		},
 
 		get: function (url, data, callback, type) {
@@ -461,4 +453,4 @@ var WDN = function() {
 			WDN.request(url, data, callback, type, "POST");
 		}
 	};
-}();
+})();
