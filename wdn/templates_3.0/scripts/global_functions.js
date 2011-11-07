cc9a4479c9fefd1506e784d0e0c27d29ec1e4805
Kevin Abel <kabel2@unl.edu>
2011-11-07 18:05:13 +0000

    Refactored the remaining global functions to use the jQuery AJAX methods.

--- wdn/templates_3.0/scripts/global_functions.js
+++ wdn/templates_3.0/scripts/global_functions.js
@@ -5,21 +5,14 @@
  * @param err [optional] Error message on failure.
  */
 function fetchURLInto(url,id,err) {
-	
-	WDN.get(url,null,function(data, textStatus){
-		
-		if (textStatus=='success') {
-			document.getElementById(id).innerHTML = data;
-		} else {
-			if (undefined === err) {
-				document.getElementById(id).innerHTML = 'Error loading results.';
-			} else {
-				document.getElementById(id).innerHTML = err;
+	WDN.jQuery('#' + id).load(url, function(data, status, jqXHR) {
+		if (jqXHR.isRejected()) {
+			if (err === undefined) {
+				err = 'Error loading results.';
 			}
+			WDN.jQuery(this).html(err);
 		}
-	}
-
-	);
+	});
 };
 
 function rotateImg(imgArray,elemId,secs,i) {
@@ -85,42 +78,32 @@ function rotateImg(imgArray,elemId,secs,i) {
 };
 
 function newRandomPromo(xmluri) {
-	var promoContent = new WDN.proxy_xmlhttp();
-	promoContent.open("GET", xmluri, true);
-	promoContent.onreadystatechange = function(){
-		if (promoContent.readyState == 4) {
-			if (promoContent.status == 200) {
-				var xmlObj = promoContent.responseXML.documentElement;
-				var promoNum = xmlObj.getElementsByTagName('promo').length;	
-				//generates random number
-				var aryId=Math.floor(Math.random()*promoNum);
-				
-				//pull promo data
-				var contentContainer = xmlObj.getElementsByTagName('contentContainer')[0].childNodes[0].nodeValue;
-				var promoTitle = xmlObj.getElementsByTagName('promo')[aryId].getAttribute("id");
-				var promoMediaType = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('media')[0].getAttribute("type");
-				var promoText = ' ';
-				try {
-					var promoMediaURL = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('media')[0].childNodes[0].nodeValue;
-					promoText = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('text')[0].childNodes[0].nodeValue;
-				} catch(e) {
-					promoText = ' ';
-				}
-				var promoLink = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('link')[0].childNodes[0].nodeValue;
-				
-				//different mime type embed
-				if (promoMediaType == 'image') {
-					document.getElementById(contentContainer).innerHTML = '<a class="imagelink" href="' + promoLink + '" title="' + promoTitle + '" /><img src="' + promoMediaURL + '" alt="promo" class="frame" /></a>\n<p>' + promoText + '</p>\n';
-				} else if (promoMediaType == 'flash') {
-					document.getElementById(contentContainer).innerHTML = '<div class="image_small_short">\n<object width="210" height="80" wmode="opaque"><param name="movie" value="' + promoMediaURL + '"><embed src="' + promoMediaURL + '" width="210" height="80"></embed></object>\n</div>\n<p>' + promoText +'</p>\n';
-				}
-			} else {
-				// Error loading file!
-			}
+	WDN.jQuery.get(xmluri, function(data) {
+		var xmlObj = data.documentElement;
+		var promoNum = xmlObj.getElementsByTagName('promo').length;	
+		//generates random number
+		var aryId=Math.floor(Math.random()*promoNum);
+		
+		//pull promo data
+		var contentContainer = xmlObj.getElementsByTagName('contentContainer')[0].childNodes[0].nodeValue;
+		var promoTitle = xmlObj.getElementsByTagName('promo')[aryId].getAttribute("id");
+		var promoMediaType = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('media')[0].getAttribute("type");
+		var promoText = ' ';
+		try {
+			var promoMediaURL = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('media')[0].childNodes[0].nodeValue;
+			promoText = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('text')[0].childNodes[0].nodeValue;
+		} catch(e) {
+			promoText = ' ';
 		}
-		promoContent = new XMLHTTP();
-	};
-	promoContent.send(null);
+		var promoLink = xmlObj.getElementsByTagName('promo')[aryId].getElementsByTagName('link')[0].childNodes[0].nodeValue;
+		
+		//different mime type embed
+		if (promoMediaType == 'image') {
+			document.getElementById(contentContainer).innerHTML = '<a class="imagelink" href="' + promoLink + '" title="' + promoTitle + '" /><img src="' + promoMediaURL + '" alt="promo" class="frame" /></a>\n<p>' + promoText + '</p>\n';
+		} else if (promoMediaType == 'flash') {
+			document.getElementById(contentContainer).innerHTML = '<div class="image_small_short">\n<object width="210" height="80" wmode="opaque"><param name="movie" value="' + promoMediaURL + '"><embed src="' + promoMediaURL + '" width="210" height="80"></embed></object>\n</div>\n<p>' + promoText +'</p>\n';
+		}
+	}, 'xml');
 };
 
 function addLoadEvent(func) {
