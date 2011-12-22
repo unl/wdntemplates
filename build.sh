710f03a6ccd77b38b1fa2b32b966745d62e7573e
Kevin Abel <kevin.abel.0@gmail.com>
2011-12-22 16:24:27 -0600

    Updated the build script Rewrote the compress.php script to use OOP goodness and reduce code duplication. Also added the ability to use the ugilify-js compiler. Updated the build.sh to reflect that.

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-echo 'Compressing CSS and JS'
+echo 'Compressing Desktop and Mobile CSS and JS'
 php wdn/templates_3.0/build/compress.php
-
-echo 'Compressing Mobile CSS and JS'
-php wdn/templates_3.0/build/compress_mobile.php
 
 echo 'Done'
 
-echo 'Running svn status wdn/templates_3.0'
-svn status wdn/templates_3.0 | grep -v "^X" | grep -v "external item"
+echo 'Running git status wdn/templates_3.0'
+git status wdn/templates_3.0
 
-echo 'Any modified files are ready to be committed with svn commit wdn/templates_3.0'
+echo 'Any modified files are ready to be committed with git commit wdn/templates_3.0'
 
 echo 'Commit compressed files, then execute zip_builder.sh to update .zip files.'
