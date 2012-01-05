893be19cb2f1a04da343e0d55e197e2823d158aa
Kevin Abel <kevin.abel.0@gmail.com>
2012-01-04 20:52:02 -0600

    Updates build location

--- wdn/templates_3.0/scripts/plugins/validator/build.sh
+++ wdn/templates_3.0/scripts/plugins/validator/build.sh
@@ -1,1 +1,1 @@
-java -jar ../../../build/compiler.jar --js=jquery.validator.js --js_output_file=jquery.validator.min.js
+java -jar ../../../../../build/compiler.jar --js=jquery.validator.js --js_output_file=jquery.validator.min.js
