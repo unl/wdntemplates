fc9574ddf826010d21f05612046697e98b0d38d7
Kevin Abel <kevin.abel.0@gmail.com>
2012-01-06 09:35:00 -0600

    Removes modernizr refs

--- wdn/templates_3.0/scripts/analytics.js
+++ wdn/templates_3.0/scripts/analytics.js
@@ -97,7 +97,6 @@ WDN.analytics = function() {
 				}
 				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
-			//WDN.analytics.setupHTML5tracking.intialize();
 		},
 		
 		trackNavigationPreferredState : function(preferredState) {
@@ -105,6 +104,7 @@ WDN.analytics = function() {
 				WDN.analytics.callTrackEvent('Navigation Preference', preferredState, WDN.analytics.thisURL);
 			} catch(e){}
 		},
+		
 		callTrackPageview: function(thePage){
 			WDN.log('we can now track the page');
 			if (!thePage) {
@@ -124,6 +124,7 @@ WDN.analytics = function() {
 				WDN.log("Pageview tracking for local site didn't work."); 
 			}
 		},
+		
 		callTrackEvent: function(category, action, label, value) {
 			if (value === undefined) {
 				value = 0;
@@ -142,74 +143,13 @@ WDN.analytics = function() {
 				WDN.log("Event tracking for local site didn't work.");
 			}
 		},
+		
 		isDefaultTrackerReady: function() {
 			if (typeof _gat != "undefined") {
 				return _gat._getTrackerByName()._getAccount() != 'UA-XXXXX-X';
 			}
 			//assume the account is set async (we could check the _gaq queue, but that seems like overkill)
 			return true;
-		}/*,
-		
-		setupHTML5tracking: function() {
-			
-			return {
-				intialize : function() {
-					WDN.loadJS(
-						'wdn/templates_3.0/scripts/plugins/modernizr/modernizr_1.5.js', 
-						function(){
-							WDN.analytics.setupHTML5tracking.checkCookie(mondernizrVersion);
-						}
-					);	
-				},
-				
-				checkCookie : function(mdVersion){
-					var userAgent = navigator.userAgent.toLowerCase();//grab the broswer User Agent
-					uAgent = userAgent.replace(/;/g, ''); //strip out the ';' so as not to bork the cookie
-					var __html5 = WDN.getCookie('__html5'); //Previous UNL HTML5 test
-					
-					if (!__html5) { //We haven't run this test before, so let's do it.
-						WDN.log('We have not run this test yet, let us track this client!');
-						WDN.analytics.setupHTML5tracking.setCookie(uAgent, mdVersion);
-						return;
-					}
-					WDN.log(uAgent +'|+|'+mdVersion);
-					WDN.log(__html5);
-					//Let's check to see if either the browser or modernizr has changed since the last tracking
-					if ((uAgent +'|+|'+mdVersion) != (__html5)){
-						WDN.log('We don\'t have a match, let us track this client!');
-						WDN.analytics.setupHTML5tracking.setCookie(uAgent, mdVersion);
-					} else { //we have a match and nothing has changed, so do nothing more.
-						WDN.log('Already have this client tracked');
-						return;
-					}
-				},
-				
-				setCookie : function(uAgent, mdVersion) {
-					var name = '__html5';
-					var value = uAgent +'|+|'+mdVersion; //combine gaVisitorID and Modernizr version
-					WDN.setCookie(name, value, 31556926); //set a cookie for one year
-					WDN.analytics.setupHTML5tracking.testBrowser();
-				},
-				
-				testBrowser : function(){
-					for (var prop in Modernizr) {
-						if (typeof Modernizr[prop] === 'function') continue;
-						if (prop == 'inputtypes' || prop == 'input') {
-							for (var field in Modernizr[prop]) {
-								if (Modernizr[prop][field]){
-									//WDN.log(prop + ' ('+field+') ' + Modernizr[prop][field]);
-									WDN.analytics.callTrackEvent('HTML5/CSS3 Support', prop + '-('+field+')', '');
-								}
-							}
-						} else {
-							if(Modernizr[prop]){
-								//WDN.log(prop + ': ' + Modernizr[prop]);
-								WDN.analytics.callTrackEvent('HTML5/CSS3 Support', prop, '');
-							}
-						}
-					}
-				}
-			};
-		}()*/
+		}
 	};
 }();
