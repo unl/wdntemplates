b25bf4021a13956400f259e1ffce61681a3daa54
Kevin Abel <kevin.abel.0@gmail.com>
2012-01-10 09:49:26 -0600

    Updates weather tool with default markup

--- wdn/templates_3.0/scripts/toolbar_weather.js
+++ wdn/templates_3.0/scripts/toolbar_weather.js
@@ -1,14 +1,18 @@
 WDN.toolbar_weather = function() {
     return {
-        initialize : function() {
-            
-        },
-        setupToolContent : function() {
-            return '<div class="col left"><h3>Local Weather</h3><div id="currentcond" class="toolbarMask"></div></div><div class="col middle"><h3>Lincoln Forecast</h3><div id="weatherforecast" class="toolbarMask"></div></div><div class="two_col right"><h3>Local Radar</h3><div id="showradar"><a href="http://radar.weather.gov/radar_lite.php?rid=oax&product=N0R&overlay=11101111&loop=yes"><img src="'+WDN.template_path+'wdn/templates_3.0/css/images/transpixel.gif" /></a></div></div>';
+        initialize : function() {},
+        setupToolContent : function(contentCallback) {
+        	WDN.jQuery.ajax({
+            	url: WDN.template_path + 'wdn/templates_3.0/includes/tools/weather.html',
+            	success: function(data) {
+            		contentCallback(data);
+            	},
+            	error: function() {
+            		contentCallback("An error occurred while loading this section");
+            	}
+            });
         },
         display : function() {
-            WDN.jQuery('#showradar img').css({background:'url(http://radar.weather.gov/lite/N0R/OAX_loop.gif)  -5px -140px no-repeat'});
-            
             var reqs = {
         		"#currentcond": "http://www.unl.edu/wdn/templates_3.0/includes/weatherCurrent.html",
         		"#weatherforecast": "http://www.unl.edu/wdn/templates_3.0/includes/weatherForecast.html"
