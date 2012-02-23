62de2f449f1aca8f98633de8eda0b585fa0276e8
Kevin Abel <kevin.abel.0@gmail.com>
2012-02-23 17:20:08 -0600

    Load jQuery in the mediaelement_wdn plugin

--- wdn/templates_3.1/scripts/mediaelement_wdn.js
+++ wdn/templates_3.1/scripts/mediaelement_wdn.js
@@ -6,7 +6,9 @@ WDN.mediaelement_wdn = function() {
 	return {
 		initialize : function(callback) {
 			WDN.loadCSS(WDN.getTemplateFilePath('scripts/plugins/mediaelement/css/mediaelementplayer.css'));
-			WDN.loadJS(WDN.getTemplateFilePath('scripts/plugins/mediaelement/mediaelement-and-player.min.js'), callback);
+			WDN.loadJQuery(function() {
+				WDN.loadJS(WDN.getTemplateFilePath('scripts/plugins/mediaelement/mediaelement-and-player.min.js'), callback);
+			});
 		}
 	};
 }();
