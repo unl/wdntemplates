fcda51042616e6ee909137dc31372a4653b019c7
Kevin Abel <kevin.abel.0@gmail.com>
2012-03-02 10:52:25 -0600

    Ensure smudge command is passed as a string

--- scripts/git-rcs-keywords.sh
+++ scripts/git-rcs-keywords.sh
@@ -10,6 +10,6 @@ smudgeValue=".git_filters/rcs-keywords.smudge %f"
 if [ -z "`git config $cleanKey`" -o -z "`git config $smudgeKey`" ]; then
 	echo "Writing rcs-keywords filter to git-config"
 	git config $cleanKey $cleanValue
-	git config $smudgeKey $smudgeValue
+	git config $smudgeKey "$smudgeValue"
 	echo "Done"
 fi
