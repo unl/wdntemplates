f7b746958062492c9830c3e78d588a54b388d4ec
Eric Rasmussen <ericrasmussen1@gmail.com>
2012-05-21 14:40:11 -0500

    [gh-304] Fix mediaelement embed script for IE8 & non-UNL sites
    * Convert single quotes to double quotes
    * Add onreadystate listener for IE8
    * Some trickery with window.WDN to get multiple videos to work in non-UNL sites

--- wdn/templates_3.1/scripts/mediaelement_embed.js
+++ wdn/templates_3.1/scripts/mediaelement_embed.js
@@ -1,30 +1,43 @@
 /**
- * Sample embed code, note that the id attribute of the video tag must be unique:
+ * Sample embed code (the id attribute of the video tag must be unique):
  *
- * <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
- * <video id="wdn-player-2699" src="http://mediahub.unl.edu/uploads/f39aea3b4141ec47720d78f9ac75d9de.mp4" controls poster="http://itunes.unl.edu/thumbnails.php?url=http%3A%2F%2Fmediahub.unl.edu%2Fuploads%2Ff39aea3b4141ec47720d78f9ac75d9de.mp4">
- *   <track src="http://mediahub.unl.edu/media/2699/vtt" kind="subtitles" type="text/vtt" srclang="en" />
- * </video>
- * <script type="text/javascript" src="http://www.unl.edu/wdn/templates_3.1/scripts/mediaelement_embed.js"></script>
+<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<video id="wdn-player-2699" controls src="http://mediahub.unl.edu/uploads/f39aea3b4141ec47720d78f9ac75d9de.mp4"
+  poster="http://itunes.unl.edu/thumbnails.php?url=http%3A%2F%2Fmediahub.unl.edu%2Fuploads%2Ff39aea3b4141ec47720d78f9ac75d9de.mp4">
+  <track src="http://mediahub.unl.edu/media/2699/vtt" kind="subtitles" type="text/vtt" srclang="en" />
+</video>
+<script src="http://www.unl.edu/wdn/templates_3.1/scripts/mediaelement_embed.js"></script>
  */
 
 (function() {
-  var scripts = document.getElementsByTagName('script');
+  var scripts = document.getElementsByTagName("script");
   var thisScriptTag = scripts[scripts.length-1];
+  var templatesRoot = "http://www.unl.edu/wdn/templates_3.1/";
 
   if (typeof(WDN) === "undefined") {
-    var WDN;
+    var WDN = new Object;
     if (typeof(window.WDN) !== "undefined") {
       WDN = window.WDN;
+      if (typeof(window.WDN.getTemplateFilePath) === "function") {
+        templatesRoot = WDN.getTemplateFilePath("", true);
+      }
+    } else {
+      window.WDN = WDN;
     }
   }
+
+  var mediaElementLoad = function() {
+    var v = WDN.jQuery(thisScriptTag).prev("video");
+    var id = mediaElementRegisterPlayer(v.attr("id"), v.attr("poster"));
+    mediaElementAttach(mediaElementPlayers[id][0], mediaElementPlayers[id][1]);
+  };
 
   var mediaElementRegisterPlayer = function(id, poster) {
     if (typeof(mediaElementPlayers) === "undefined") {
       mediaElementPlayers = [];
     }
-    mediaElementPlayers.push([ id, poster ]);
-    return mediaElementPlayers.length - 1;
+    mediaElementPlayers.push([id, poster]);
+    return mediaElementPlayers.length-1;
   };
 
   var mediaElementAttach = function(playerId, src) {
@@ -33,12 +46,12 @@
       WDN.jQuery("#" + playerId).attr("width", parentWidth);
 
       var i = new Image();
-      WDN.jQuery(i).bind('load', function(event) {
-        WDN.jQuery("#" + playerId).attr('height', parentWidth * (this.height / this.width));
+      WDN.jQuery(i).bind("load", function(event) {
+        WDN.jQuery("#" + playerId).attr("height", parentWidth * (this.height / this.width));
         WDN.jQuery("#" + playerId).mediaelementplayer();
       });
-      WDN.jQuery(i).bind('error', function(event) {
-        WDN.jQuery("#" + playerId).attr('height', parentWidth * (9 / 16));
+      WDN.jQuery(i).bind("error", function(event) {
+        WDN.jQuery("#" + playerId).attr("height", parentWidth * (9 / 16));
         WDN.jQuery("#" + playerId).mediaelementplayer();
       });
       i.src = src;
@@ -48,26 +61,38 @@
       var c = document.createElement("link");
       c.setAttribute("type", "text/css");
       c.setAttribute("rel", "stylesheet");
-      c.setAttribute("href", "http://www.unl.edu/wdn/templates_3.1/scripts/plugins/mediaelement/css/mediaelementplayer.css");
+      if (typeof(WDN.hasDocumentClass) === "function" && WDN.hasDocumentClass("debug") == true) {
+        c.setAttribute("href", templatesRoot+"scripts/plugins/mediaelement/css/mediaelementplayer.css");
+      } else {
+        c.setAttribute("href", templatesRoot+"scripts/plugins/mediaelement/css/mediaelementplayer.min.css");
+      }
       document.getElementsByTagName("head")[0].appendChild(c);
+
       var j = document.createElement("script");
       j.setAttribute("type", "text/javascript");
-      j.setAttribute("src", "http://www.unl.edu/wdn/templates_3.0/scripts/mediaelement.js");
-      //j.setAttribute("src", "http://www.unl.edu/wdn/templates_3.1/scripts/plugins/mediaelement/mediaelement-and-player.js");
+      if (typeof(WDN.hasDocumentClass) === "function" && WDN.hasDocumentClass("debug") == true) {
+        j.setAttribute("src", templatesRoot+"scripts/plugins/mediaelement/mediaelement-and-player.js");
+      } else {
+        j.setAttribute("src", templatesRoot+"scripts/plugins/mediaelement/mediaelement-and-player.min.js");
+      }
+
+      // Modern browsers
       j.onload = function() {
         WDN.mediaElementPlayerLoaded = true;
         attachPlayer();
       };
+      // IE8
+      j.onreadystatechange = function() {
+        if (j.readyState == "loaded") {
+          WDN.mediaElementPlayerLoaded = true;
+          attachPlayer();
+        }
+      };
+
       document.getElementsByTagName("head")[0].appendChild(j);
     } else {
       attachPlayer();
     }
-  };
-
-  var mediaElementLoad = function() {
-    var v = WDN.jQuery(thisScriptTag).prev('video');
-    var id = mediaElementRegisterPlayer(v.attr('id'), v.attr('poster'));
-    mediaElementAttach(mediaElementPlayers[id][0], mediaElementPlayers[id][1]);
   };
 
   if (typeof(WDN.loadJQuery) !== "function") {
