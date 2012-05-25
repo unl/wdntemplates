cff966ec0566d43822f680e5aa2ca3da6400ef26
Kevin Abel <kevin.abel.0@gmail.com>
2012-05-25 16:57:44 -0500

    Remove sample embed script. Update MediaElement examples. Embed script should be updated in MediaHub project. (Discussion: Do we need to add embed code support for templates_3.0?)

--- wdn/templates_3.1/scripts/mediaelement_wdn.js
+++ wdn/templates_3.1/scripts/mediaelement_wdn.js
@@ -5,9 +5,26 @@
 WDN.mediaelement_wdn = function() {
 	return {
 		initialize : function(callback) {
-			WDN.loadCSS(WDN.getTemplateFilePath('scripts/plugins/mediaelement/css/mediaelementplayer.css'));
+			var min = '';
+			if (!WDN.jQuery('body').hasClass('debug')) {
+				min = '.min';
+			}
+			
+			WDN.loadCSS(WDN.getTemplateFilePath('scripts/plugins/mediaelement/css/mediaelementplayer' + min + '.css'));
 			WDN.loadJQuery(function() {
-				WDN.loadJS(WDN.getTemplateFilePath('scripts/plugins/mediaelement/mediaelement-and-player.min.js'), callback);
+				WDN.loadJS(WDN.getTemplateFilePath('scripts/plugins/mediaelement/mediaelement-and-player' + min + '.js'), function() {
+					WDN.jQuery('video.wdn_player, audio.wdn_player').each(function() {
+						WDN.jQuery(this).mediaelementplayer({
+							videoWidth: '100%',
+							videoHeight: '100%',
+							audioWidth: '100%'
+						});
+					});
+					
+					if (callback) {
+						callback();
+					}
+				});
 			});
 		}
 	};
