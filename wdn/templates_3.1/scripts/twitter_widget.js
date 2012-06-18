dbee8f5aba9ab7a47a91f5595f7a7c3a58de24eb
Chris Bourke <cbourke@cse.unl.edu>
2012-06-18 13:44:12 -0500

    removed the option allowing users to define which div element would be used for the twitter widget; changed the behavior of the plugin to dynamically load the CSS and wrapped it in a loadJQuery callback (chained to ensure that styling is available when initialized)

--- wdn/templates_3.1/scripts/twitter_widget.js
+++ wdn/templates_3.1/scripts/twitter_widget.js
@@ -6,73 +6,82 @@
 WDN.twitter_widget = function() {
     return {
 
-	initialize : function(userConfig) {
+	config : {
+            twitterAccount : 'UNLNews',
+            rpp : 5,
+            width: 'auto',
+            height: 'auto',
+            themeColors : { color: '#000000', background: '#a7cee5'},
+        },
+
+	userConfig : null,
+
+	initialize : function(usrConfig) {
 	    WDN.log ('initializing twitter_widget...');
+            WDN.twitter_widget.userConfig = usrConfig;
+	    //chaining required as the CSS info must be loaded first
+            WDN.loadCSS(WDN.getTemplateFilePath('css/content/twitterbox.css'), function() {
+		WDN.loadJQuery(WDN.twitter_widget.setup);
+	    });
+	},
 
-	    var config = {
-		twitterAccount : 'UNLNews',
-                id : 'wdn_twitter_box',
-                rpp : 5,
-                width: 'auto',
-                height: 'auto',
-                themeColors : { color: '#000000', background: '#a7cee5'},
-	    };
+	setup : function () {
 
-	    if(!userConfig) {
+	    if(WDN.twitter_widget.userConfig == null) {
                 WDN.log('using defaults for twitter_widget...');
-	    } else if( (typeof userConfig) === 'string') {
-		WDN.log('using twitterAccount = ' + userConfig + ' and defaults for twitter_widget...');
-		config.twitterAccount = userConfig;
+	    } else if( (typeof WDN.twitter_widget.userConfig) === 'string') {
+		WDN.log('using twitterAccount = ' + WDN.twitter_widget.userConfig + ' and defaults for twitter_widget...');
+		WDN.twitter_widget.config.twitterAccount = WDN.twitter_widget.userConfig;
 	    } else {
 		WDN.log('using provided userConfig settings for twitter_widget...');
-                config.twitterAccount = userConfig.twitterAccount != null ? userConfig.twitterAccount : config.twitterAccount;
-		config.id = userConfig.id != null ? userConfig.id : config.id;
-                config.rpp = userConfig.rpp != null ? userConfig.rpp : config.rpp;
-                config.width = userConfig.width != null ? userConfig.width : config.width;
-                config.height = userConfig.height != null ? userConfig.height : config.height;
+                WDN.twitter_widget.config.twitterAccount = WDN.twitter_widget.userConfig.twitterAccount != null ? WDN.twitter_widget.userConfig.twitterAccount : WDN.twitter_widget.config.twitterAccount;
+		WDN.twitter_widget.config.rpp = WDN.twitter_widget.userConfig.rpp != null ? WDN.twitter_widget.userConfig.rpp : WDN.twitter_widget.config.rpp;
+                WDN.twitter_widget.config.width = WDN.twitter_widget.userConfig.width != null ? WDN.twitter_widget.userConfig.width : WDN.twitter_widget.config.width;
+                WDN.twitter_widget.config.height = WDN.twitter_widget.userConfig.height != null ? WDN.twitter_widget.userConfig.height : WDN.twitter_widget.config.height;
 	    }
 
-	    if(!WDN.jQuery('#'+config.id).length) {
-                WDN.log('unable to initialize twitter_widget as element with id = '+config.id+' does not exist');
+	    if(!WDN.jQuery('#wdn_twitter_box').length) {
+                WDN.log('unable to initialize twitter_widget as wdn_twitter_box element does not exist');
                 return;
-	    } else {
-		WDN.log('initializing twitter_widget on element with id = '+config.id+'...');
 	    }
-	    WDN.loadCSS(WDN.getTemplateFilePath('css/content/twitterbox.css'));
 	    //grab theme from div's style rather than configuration
-            config.themeColors.color = WDN.jQuery('#'+config.id).css('color');
-            config.themeColors.background = WDN.jQuery('#'+config.id).css('background-color');
+            WDN.twitter_widget.config.themeColors.color = WDN.jQuery('#wdn_twitter_box').css('color');
+            WDN.twitter_widget.config.themeColors.background = WDN.jQuery('#wdn_twitter_box').css('background-color');
 	    //but then immediately clear it out so that the twitter widget can handle styling
-	    WDN.jQuery('#'+config.id).css('color', 'rgba(255,255,255,0)');
-	    WDN.jQuery('#'+config.id).css('background-color', 'rgba(255,255,255,0)');
+	    WDN.jQuery('#wdn_twitter_box').css('color', 'rgba(255,255,255,0)');
+            WDN.jQuery('#wdn_twitter_box').css('background-color', 'rgba(255,255,255,0)');
+
+	    //format the widget args:
+	    var widget_config = {
+		version: 2,
+                type: 'profile',
+                id : 'wdn_twitter_box',
+                rpp: WDN.twitter_widget.config.rpp,
+                interval: 30000,
+                width: WDN.twitter_widget.config.width,
+                height: WDN.twitter_widget.config.height,
+                theme: {
+                    shell: {
+                        background: WDN.twitter_widget.config.themeColors.background,
+                        color: WDN.twitter_widget.config.themeColors.color
+                    },
+                    tweets: {
+                        background: '#f7f7f7',
+                        color: '#000000',
+                        links: '#c40302'
+                    }
+                },
+                features: {
+                    scrollbar: true,
+                    loop: false,
+                    live: true,
+                    behavior: 'all'
+                }
+	    };
 
 	    WDN.jQuery.getScript('http://widgets.twimg.com/j/2/widget.js', function () {
-		new TWTR.Widget({
-		    version: 2,
-		    type: 'profile',
-		    id : config.id,
-		    rpp: 10,
-		    interval: 30000,
-		    width: config.width,
-		    height: config.height,
-		    theme: {
-			shell: {
-			    background: config.themeColors.background,
-			    color: config.themeColors.color
-			},
-			tweets: {
-			    background: '#f7f7f7',
-			    color: '#000000',
-			    links: '#c40302'
-			}
-		    },
-		    features: {
-			scrollbar: true,
-			loop: false,
-			live: true,
-			behavior: 'all'
-		    }
-		}).render().setUser(config.twitterAccount).start();});
+		new TWTR.Widget(widget_config).render().setUser(WDN.twitter_widget.config.twitterAccount).start();
+	    });
 	}
     };
 }();
