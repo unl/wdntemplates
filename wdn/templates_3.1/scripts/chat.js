189c4f91deb8b7a50fd102163af4dd88df2a9693
mfairchild365 <mfairchild365@gmail.com>
2012-07-16 12:53:12 -0500

    load https chat resources if currently viewing in https.

--- wdn/templates_3.1/scripts/chat.js
+++ wdn/templates_3.1/scripts/chat.js
@@ -1,7 +1,7 @@
 WDN.chat = function() {
     return {
         initialize : function() {
-            WDN.loadJS("http://ucommchat.unl.edu/js/chat.php?version=" + WDN.getHTMLVersion());
+            WDN.loadJS(('https:' == document.location.protocol ? 'https://' : 'http://') + "ucommchat.unl.edu/js/chat.php?version=" + WDN.getHTMLVersion());
         }
     };
 }();
