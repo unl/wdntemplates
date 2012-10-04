5f95110a0df7b68567b8cc024173d40cfad961e4
Eric Rasmussen <ericrasmussen1@gmail.com>
2012-10-04 14:11:29 -0500

    [gh-337] Replace comment form in feedback area with link
    The chat project now supplies a feedback form and only needs
    an with id to hook onto. So just use a link for no-js browsing.

--- wdn/templates_3.1/scripts/feedback.js
+++ wdn/templates_3.1/scripts/feedback.js
@@ -20,41 +20,6 @@ WDN.feedback = function() {
 			    		}
 			    );
 			});
-		},
-		commentSetup : function() {
-			if (WDN.hasDocumentClass('no-placeholder')) {
-				WDN.loadJS(WDN.getTemplateFilePath('scripts/plugins/placeholder/jquery.placeholder.min.js'), function() {
-					WDN.jQuery('#wdn_feedback_comments').find('[placeholder]').placeholder();
-				});
-			}
-
-			WDN.jQuery('#wdn_feedback_comments textarea').attr('x-webkit-speech', 'x-webkit-speech').keyup(
-				function(event) {
-					if (this.value.length > 0) {
-						// Add the submit button
-						WDN.jQuery('.wdn_comment_email,.wdn_comment_name,#wdn_feedback_comments input').slideDown();
-					}
-				}
-			);
-			WDN.jQuery('#wdn_feedback_comments').submit(
-				function(event) {
-					var comments = WDN.jQuery('#wdn_feedback_comments textarea').val();
-					if (comments.split(' ').length < 4) {
-						// Users must enter in at least 4 words.
-						alert('Please enter more information, give me at least 4 words!');
-						return false;
-					}
-					WDN.jQuery('#wdn_feedback_comments form input[type="submit"]').attr('disabled', 'disabled');
-					WDN.post(
-						'http://www1.unl.edu/comments/',
-						WDN.jQuery('#wdn_feedback_comments').serialize()
-					);
-					WDN.jQuery('#wdn_feedback_comments').hide();
-					WDN.jQuery('#wdn_footer_feedback').append('<h4>Thanks!</h4>');
-					event.stopPropagation();
-					return false;
-				}
-			);
 		}
 	};
 }();
