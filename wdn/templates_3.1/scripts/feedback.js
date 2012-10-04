b0d8b271738400fc1c88dfcc2e05c47e464e543b
Eric Rasmussen <ericrasmussen1@gmail.com>
2012-10-04 14:42:13 -0500

    [gh-337] Code formatting and cleanup

--- wdn/templates_3.1/scripts/feedback.js
+++ wdn/templates_3.1/scripts/feedback.js
@@ -1,24 +1,24 @@
-WDN.feedback = function() {
+WDN.feedback = (function($) {
 
-	return {
-        initialize : function() {
-			//WDN.log("initialize feedback");
-			WDN.feedback.ratingSetup();
-		},
-		ratingSetup : function() {
-			WDN.log("setting up rating");
-			WDN.jQuery('#wdn_feedback input').click(function() {
-			    selectedRating = WDN.jQuery(this).attr('value');
-			    WDN.log("rating="+ selectedRating);
-			    WDN.analytics.callTrackEvent('Page Rating', 'Rated a '+selectedRating, WDN.analytics.thisURL, selectedRating);
-			    var url = 'http://www1.unl.edu/comments/';
-			    WDN.post(
-			    		url,
-			    		{ rating: selectedRating },
-			    		function() {
-			    		}
-			    );
-			});
-		}
-	};
-}();
+    return {
+        initialize: function() {
+            WDN.feedback.ratingSetup();
+        },
+
+        ratingSetup: function() {
+            WDN.log('Setting up star rating');
+            $('#wdn_feedback input').click(function() {
+                selectedRating = $(this).attr('value');
+                WDN.log('rating='+ selectedRating);
+                WDN.analytics.callTrackEvent('Page Rating', 'Rated a '+selectedRating, WDN.analytics.thisURL, selectedRating);
+                var url = 'http://www1.unl.edu/comments/';
+                WDN.post(
+                    url,
+                    {rating: selectedRating},
+                    function() {}
+                );
+            });
+        }
+    };
+
+})(WDN.jQuery);
