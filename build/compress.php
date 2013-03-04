b13c6f1dd1dff8fda26d45999f268b13b4116be5
Seth Meranda <seth@meranda.org>
2013-03-04 13:17:13 -0600

    Add separate styles for IE8 overrides
    * IE8 doesn't support `rem` measurements
    * IE8 doesn't support media queries

--- build/compress.php
+++ build/compress.php
@@ -144,6 +144,7 @@ class UNL_WDNTemplates_Compressor
         'variations/ie' => array('ignore' => true),
         'variations/touch' => array('ignore' => true),
         'variations/print' => array('ignore' => true),
+        'variations/grid-v3-ie' => array('ignore' => true),
         'variations/regions',
     );
 
