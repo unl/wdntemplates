dc8459d8e444e2ea6840adaad744851694a4be29
Kevin Abel <kevin.abel.0@gmail.com>
2013-07-12 19:23:37 -0500

    Allow the legacy module to re-run when necessary

--- wdn/templates_4.0/scripts/legacy.js
+++ wdn/templates_4.0/scripts/legacy.js
@@ -1,7 +1,10 @@
 define(['jquery', 'wdn'], function($, WDN) {
-	$(function() {
+	var setup = function() {
 		if ($('ul.wdn_tabs').length) {
 			WDN.initializePlugin('tabs');
 		}
-	});
+	};
+	$(setup);
+	
+	return setup;
 });
