3006e927296631f56d443d7f6c3a12d9cac22218
Kevin Abel <kevin.abel.0@gmail.com>
2013-07-17 15:32:38 -0500

    Update NOAA weather proxy to match markup for 4.0 apps drawer

--- scripts/getNOAAWeather.php
+++ scripts/getNOAAWeather.php
@@ -3,50 +3,39 @@
 // to generate the static html fragment.
 
 $observationData = simplexml_load_file('http://www.nws.noaa.gov/data/current_obs/KLNK.xml');
-$observationOutput = 'weatherCurrent.html';
-$forecastData = file_get_contents('http://forecast.weather.gov/MapClick.php?lat=40.80686&lon=-96.68167900000003&unit=0&lg=english&FcstType=text&TextType=1');
-$forecastOutput = 'weatherForecast.html';
+$observationOutput = 'weather.html';
 
-chdir(realpath(dirname(__FILE__) . '/../wdn/templates_3.1/includes'));
+chdir(realpath(dirname(__FILE__) . '/../wdn/templates_4.0/includes'));
 
-if ($forecastData) {
-    $start = $end = strpos($forecastData, '<b>', strpos($forecastData, '</table><table'));
-    for ($i = 0; $i < 3; $i++) {
-        $end = strpos($forecastData, '<b>', $end + 3);
-    }
-
-
-    $forecastData = substr($forecastData, $start, $end - $start);
-    $forecastData = str_replace(array('<br>', '<b>', '</b>'), array('<br />', '<strong>', '</strong>'), $forecastData);
-    $forecastData .= '<a href="http://forecast.weather.gov/MapClick.php?lat=40.80686&lon=-96.68167900000003&unit=0&lg=english&FcstType=text&TextType=1">Complete forecast</a><br /><a href="http://snr.unl.edu/LincolnWeather/">Lincoln Weather and Climate</a>
-';
-} else {
-    $forecastData = '<p>Currently unavailable</p>';
-}
-file_put_contents($forecastOutput, $forecastData);
-
-if ($observationData && $observationData->temp_f != 'NA') {
-    ob_start(); ?>
-
-<div id="unltemp"><span id="unlweathertempf" class="weathertempf"><?php echo $observationData->temp_f ?>&#176;</span> F (<span id="unlweathertempc" class="weathertempc"><?php echo $observationData->temp_c ?>&#176;</span> C)</div>
-<div id="unlcurrent" class="weatherconditions"><strong>Currently: </strong><?php echo $observationData->weather ?></div>
-<?php if (isset($observationData->heat_index_f) || isset($observationData->windchill_f)): ?>
-<div id="unladjtemp" class="weatherconditions">
-<?php if (isset($observationData->heat_index_f)): ?>
-    Heat Index: <span id="unladjtempf"><?php echo $observationData->heat_index_f ?>&#176;</span> F (<span id="unladjtempc"><?php echo $observationData->heat_index_c ?>&#176;</span> C)
-<?php else: ?>
-    Windchill: <span id="unladjtempf"><?php echo $observationData->windchill_f ?>&#176;</span> F (<span id="unladjtempc"><?php echo $observationData->windchill_c ?>&#176;</span> C)
-<?php endif; ?>
+ob_start();
+?>
+<div id="wdn_current_weather">
+    <?php if ($observationData && $observationData->temp_f != 'NA') : ?>
+    <div id="wdn_temp_wrapper">
+        <span id="wdn_temp_f" class="wdn-temp"><?php echo $observationData->temp_f ?>&#176;<span class="wdn-weather-label wdn-weather-context">F</span>
+        </span>
+        <span id="wdn_temp_c" class="wdn-weather-secondary wdn-temp"><?php echo $observationData->temp_c ?>&#176;<span class="wdn-weather-label wdn-weather-context">C</span>
+        </span>
+    </div>
+    <div id="wdn_current_condition_wrapper">
+        <span class="wdn-weather-label wdn-sub-resource-title">
+            Conditions: 
+        </span>
+        <span id="wdn_current_condition" class="wdn-weather-conditions"><?php echo $observationData->weather ?></span>
+    </div>
+    <?php else : ?>
+    <p>Currently unavailable</p>
+    <?php endif; ?>
+    <a class="external wdn-action-link" href="http://forecast.weather.gov/MapClick.php?lat=40.8068620&amp;lon=-96.6816790">View the local forecast
+    </a>
 </div>
-<?php endif;?>
-<div id="unlhumidity" class="weatherconditions">Relative Humidity: <?php echo $observationData->relative_humidity?>%</div>
-<div id="unlwind" class="weatherconditions">Winds <?php echo $observationData->wind_string ?></div>
-<div id="unlweatherasof" class="weatherasof" title="<?php echo $observationData->observation_time ?>">Last Update: <?php echo date('g:i a', strtotime($observationData->observation_time_rfc822))?></div>
-<div id="unlfullforecast"><a class="external" href="http://forecast.weather.gov/MapClick.php?lat=40.8068620&lon=-96.6816790">Complete local weather</a></div>
+<!--
+<div id="weatherforecast">
 
-<?php
-    $observationData = ob_get_clean();
-} else {
-    $observationData = '<p>Currently unavailable</p>';
-}
-file_put_contents($observationOutput, $observationData);
+</div>
+-->
+<div id="wdn_radar_wrapper">
+    <a class="external wdn-action-link" title="Review the local radar at the National Weather Service" href="http://radar.weather.gov/radar_lite.php?rid=oax&amp;product=N0R&amp;overlay=11101111&amp;loop=yes">
+    View the local radar</a>
+</div>
+<?php file_put_contents($observationOutput, ob_get_clean());
