388cafd6d4936beaad711d40c97385eff479b2fb
Brett Bieber <brett.bieber@gmail.com>
2013-10-01 11:34:27 -0500

    Use classes for wdn-locked region changes, where possible

--- wdn/templates_4.0/scripts/band_imagery.js
+++ wdn/templates_4.0/scripts/band_imagery.js
@@ -50,34 +50,18 @@ define(['jquery', 'wdn'], function($, WDN) {
 
 				if (depth < parent_offset) {
 					// Above locked region
-					/*$this.css({
-						position : 'absolute',
-						top      : '0',
-						bottom   : 'auto'
-					}); */
-
 					$this.removeClass("fixed bottom")
-					
 					
 				} else if ((depth >= parent_offset)
 					&& ((depth + window_height) < (parent_height + parent_offset)) ) {
 					// Currently viewing locked region
-					/*$this.css({
-						position : 'fixed',
-						top      : '0'
-					});*/
-					
-					$this.addClass("fixed");
-					$this.removeClass("bottom");
+					$this.css('top', '0');
+					$this.addClass("fixed").removeClass("bottom");
 				} else {
 					// Below locked region
 					//$this.css('position', 'absolute');
 					$this.removeClass("fixed");
 					if (window_height < $shownFigure.height()) {
-						/*$this.css({
-							top    : 'auto',
-							bottom : '0'
-						});*/
 						$this.addClass("bottom");
 					} else {
 						var pinned_top = parent_height - window_height;
