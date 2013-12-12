4ac04183ff61059c4f90f096dd00ea9f3da4e44b
Kevin Abel <kevin.abel.0@gmail.com>
2013-12-12 15:02:15 -0600

    Implement a notice to Windows XP users for end-of-life. Closes #666

--- wdn/templates_4.0/scripts/legacy.js
+++ wdn/templates_4.0/scripts/legacy.js
@@ -21,26 +21,46 @@ define(['jquery', 'wdn', 'require', 'modernizr'], function($, WDN, require, Mode
 	}
 
 	var showBar = false,
-	msg = 'This page may not be displayed correctly in this browser. ',
-	url,
-	$html = $('html');
+	msg = '', url,
+	$html = $('html'),
+	reXPAgent = /Windows (?:NT 5.1|XP)/,
+	setCookie = false,
+	xpCookie = 'unlXPAck',
+	xpCookieLifetime = 30 * 24 * 60 * 60; // 30 days in seconds
+
+	if (window.navigator.userAgent.match(reXPAgent) && !WDN.getCookie(xpCookie)) {
+		showBar = true;
+		setCookie = true;
+		msg += 'According to Microsoft, your operating system (Windows XP) is reaching its "end of life". You are strongly encouraged to upgrade.';
+		url = 'http://www.unl.edu/helpcenter/xp';
+	}
 
 	// old IE
 	if ($html.hasClass('ie6') || $html.hasClass('ie7')) {
-		showBar = true;
-		msg += 'You are strongly encouraged to update. <a href="http://its.unl.edu/standards">Read More</a>';
-		url = 'http://windows.microsoft.com/en-us/internet-explorer/download-ie';
+		if (showBar) {
+			msg += '<br />';
+		} else {
+			showBar = true;
+			url = 'http://windows.microsoft.com/en-us/internet-explorer/download-ie';
+		}
+		msg += 'This page may not be displayed correctly in this browser. You are strongly encouraged to update. <a href="http://its.unl.edu/standards">Read More</a>';
 	}
 
 	if (showBar) {
 		WDN.loadJQuery(function() {
 			require(['plugins/activebar/activebar2'], function() {
-				$('<div>').html(msg)
-					.activebar({
-						icon: WDN.getTemplateFilePath('images/activebar-information.png', true),
-						button: WDN.getTemplateFilePath('images/activebar-closebtn.png', true),
-						url: url
+				var cnt = $('<div>').html(msg);
+				cnt.activebar({
+					icon: WDN.getTemplateFilePath('images/activebar-information.png', true),
+					button: WDN.getTemplateFilePath('images/activebar-closebtn.png', true),
+					url: url
+				});
+
+				if (setCookie) {
+					cnt.parent().siblings('.close').click(function() {
+						WDN.setCookie(xpCookie, 1, xpCookieLifetime);
 					});
+				}
 			});
 		});
 	}
