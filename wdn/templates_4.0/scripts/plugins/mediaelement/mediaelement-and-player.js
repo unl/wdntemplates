2ed477f8b1d4e7522d89ca87b9132eb39f3a8c63
Michael Fairchild <mfairchild365@gmail.com>
2014-04-25 16:54:24 -0500

    Add alt text poster image in the fallback link

--- wdn/templates_4.0/scripts/plugins/mediaelement/mediaelement-and-player.js
+++ wdn/templates_4.0/scripts/plugins/mediaelement/mediaelement-and-player.js
@@ -1193,7 +1193,7 @@ mejs.HtmlMediaElementShim = {
       errorContainer.innerHTML = options.customError;
     } else {
       errorContainer.innerHTML = (poster !== '') ?
-        '<a href="' + playback.url + '"><img src="' + poster + '" width="100%" height="100%" /></a>' :
+        '<a href="' + playback.url + '"><img src="' + poster + '" width="100%" height="100%" alt="view video" /></a>' :
         '<a href="' + playback.url + '"><span>' + mejs.i18n.t('Download File') + '</span></a>';
     }
 
