d92912df3e6a17963ea5ff0c6d86164f55457850
Michael Fairchild <mfairchild365@gmail.com>
2014-08-05 09:00:00 -0500

    Update the event bad to use new json and moment.js

--- wdn/templates_4.0/scripts/events-band.js
+++ wdn/templates_4.0/scripts/events-band.js
@@ -1,4 +1,4 @@
-define(['jquery', 'wdn', 'require'], function($, WDN, require) {
+define(['jquery', 'wdn', 'require', 'moment'], function($, WDN, require, moment) {
     var getLocalEventSettings = function() {
         var $eventLink = $('link[rel=events]'),
             eventParams = WDN.getPluginParam('events');
@@ -18,90 +18,19 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 
     var fetchEvents = function(localConfig) {
             var url = localConfig.url + '?format=json&limit=' + localConfig.limit;
-                $.getJSON(url, function(data) {
-                    if (!data.Events.Event) {
+                $.getJSON(url, function(data) {console.log(data);
+                    if (!data.Events) {
                         return;
                     }
 
-                    $.each(data.Events.Event, function(index, event) {
-                        var eventURL = event.WebPages.WebPage;
-                        if($.isArray(eventURL)) {
-                            var eventURL = event.WebPages.WebPage[0].URL;
-                        }
-                        else {
-                            var eventURL = event.WebPages.WebPage.URL;
-                        }
-
-
-                        var parts   = event.DateTime.StartDate.split('-');
-                        var year    = parts[0];
-                        var month = parts[1];
-                        var day     = parseInt(parts[2], 10);
-                        switch (month) {
-                            case '01':
-                                month = 'Jan';
-                            break;
-
-                            case '02':
-                                month = 'Feb';
-                            break;
-
-                            case '03':
-                                month = 'March';
-                            break;
-
-                            case '04':
-                                month = 'April';
-                            break;
-
-                            case '05':
-                                month = 'May';
-                            break;
-
-                            case '06':
-                                month = 'June';
-                            break;
-
-                            case '07':
-                                month = 'July';
-                            break;
-
-                            case '08':
-                                month = 'Aug';
-                            break;
-
-                            case '09':
-                                month = 'Sept';
-                            break;
-
-                            case '10':
-                                month = 'Oct';
-                            break;
-
-                            case '11':
-                                month = 'Nov';
-                            break;
-
-                            case '12':
-                                month = 'Dec';
-                            break;
-                        }
-                        var time    = '';
-                        var ampm = '';
-                        var hour    = '';
-                        if (event.DateTime.StartTime) {
-                            time = event.DateTime.StartTime.substring(0, 5);
-                            hour = event.DateTime.StartTime.substring(0, 2);
-                            if (hour > 12) {
-                                ampm = 'pm';
-                                hour = hour - 12;
-                                time = hour + time.substring(2);
-                            } else {
-                                ampm = 'am';
-                            }
-                        }
-
-                        var location = event.Locations.Location.Address.BuildingName;
+                    $.each(data.Events, function(index, event) {
+                        var eventURL = event.WebPages[0].URL;
+                        var date     = moment(event.DateTime.StartDate);
+                        var month    = date.format('MMM');
+                        var day      = date.format('D');
+                        var time     = date.format('h:m');
+                        var ampm     = date.format('a');
+                        var location = event.Locations[0].Address.BuildingName;
 
                         $('#events-band').append('<div class="wdn-col"> <a href="' + eventURL + '" target="_blank"><div class="event"> <div class="dateTime">' + '<span class="month">'+month+'</span><span class="day">'+day+'</span><span class="time">'+time+' '+ampm+'<\/span>' + '<\/div> <div class="eventInfo"><p class="eventTitle">'
                             + event.EventTitle + '</p><span class="location">' + location + ' </span>' + '</div></div></a></div>');
