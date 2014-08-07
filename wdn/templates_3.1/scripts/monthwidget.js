838afd5fc65241829d45c78f0456c111296ae8b3
Michael Fairchild <mfairchild365@gmail.com>
2014-08-07 13:28:18 -0500

    Allow both 1 and 2 digit months an days

--- wdn/templates_3.1/scripts/monthwidget.js
+++ wdn/templates_3.1/scripts/monthwidget.js
@@ -90,7 +90,7 @@ WDN.monthwidget = function() {
                     			infoBox.addClass('pos2');
                     		}
                     		var eventBox = WDN.jQuery('.eventBox', this);
-                            var regex = /\d{4}\/\d{2}\/\d{2}/;
+                            var regex = /\d{4}\/\d{1,2}\/\d{1,2}/;
                             date = moment(regex.exec(WDN.jQuery('a', this)[0].href)[0]);
                     		WDN.jQuery.ajax({
                     			url: WDN.jQuery('a', this)[0].href + '?format=xml',
