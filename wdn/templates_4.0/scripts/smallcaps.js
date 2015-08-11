db5597e27a1e3c4d43c394c3c217bd3831a6d676
Ryan Dee <ryan@skoolbus39.com>
2015-08-11 11:10:13 -0500

    Update with Kevin's suggested JavaScript

--- wdn/templates_4.0/scripts/smallcaps.js
+++ wdn/templates_4.0/scripts/smallcaps.js
@@ -1,14 +1,26 @@
-define(['jquery', 'wdn', 'require'], function($, WDN, require) {
+var initd = false;
 
-    // Load Mercury ScreenSmart Small Caps fonts from Cloud.typography
-    var $smcaps = $("<link>", {
-        "rel" : "stylesheet",
-        "type" : "text/css",
-        "href" : "//cloud.typography.com/7717652/679648/css/fonts.css"
-    });
+return {
+    initializePlugin: function() {
+        // protect against multiple initializations
+        if (initd) {
+            return;
+        }
+        initd = true;
 
-    $('head').append($smcaps);
+        define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 
-    // Load WDN small caps styles
-    WDN.loadCSS(WDN.getTemplateFilePath('css/layouts/smallcaps.css'));
-});
+            // Load Mercury ScreenSmart Small Caps fonts from Cloud.typography
+            var $smcaps = $("<link>", {
+                "rel" : "stylesheet",
+                "type" : "text/css",
+                "href" : "//cloud.typography.com/7717652/679648/css/fonts.css"
+            });
+        
+            $('head').append($smcaps);
+        
+            // Load WDN small caps styles
+            WDN.loadCSS(WDN.getTemplateFilePath('css/layouts/smallcaps.css'));
+        });        
+    }
+};
