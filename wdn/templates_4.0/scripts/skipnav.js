d43c8dbd83c936f93cee836d18fa4bd114d29c99
Michael Fairchild <mfairchild365@gmail.com>
2015-08-28 16:16:05 -0500

    Wait until the document is ready
    #maincontent might not exist yet.

--- wdn/templates_4.0/scripts/skipnav.js
+++ wdn/templates_4.0/scripts/skipnav.js
@@ -1,4 +1,6 @@
-define([], function() {
+define(['jquery'], function($) {
 	//Fix skipnav to maincontent (maincontent was not focuable, and thus the tab order would revert to the top of the page)
-	document.getElementById('maincontent').setAttribute('tabindex', '-1');
+	$(function() {
+		$('#maincontent').attr('tabindex', '-1');
+	})
 });
