6e37c393af1681e27b677eaef5a5b44798b284f3
Kevin Abel <kevin.abel.0@gmail.com>
2015-09-04 22:35:56 -0500

    Enable require-css plugin in main configuration
    This will catch all dependencies prefixes like "css!"

--- wdn/templates_4.0/scripts/main.js
+++ wdn/templates_4.0/scripts/main.js
@@ -1,6 +1,11 @@
 // WDN must be loaded synchronously for BC and config support
 requirejs.config({
-    'baseUrl': WDN.getTemplateFilePath('scripts', true),
+	baseUrl: WDN.getTemplateFilePath('scripts', true),
+	map: {
+		"*": {
+			css: 'require-css/css'
+		}
+	}
 });
 
 requirejs([
