836059af0140717cca7df3bdd1899c67d543eb51
Kevin Abel <kabel2@unl.edu>
2015-12-04 16:13:59 -0600

    Clean up code and default feed on RSS widget

--- wdn/templates_4.1/scripts/rss_widget.js
+++ wdn/templates_4.1/scripts/rss_widget.js
@@ -5,10 +5,10 @@ define([
 	'css!js-css/rsswidget'
 ], function($) {
 	var initd = false;
-	
+
     var Plugin = {
 		defaultConfig : {
-		    url: 'http://ucommxsrv1.unl.edu/rssfeeds/unlinthenewsrss.xml',
+		    url: 'http://newsroom.unl.edu/inthenews/?format=xml',
 		    elementId : 'wdn_rss_widget',
 		    pause: 5000,
 		    width: 'auto',
@@ -19,48 +19,47 @@ define([
 
 		initialize : function(userConfig) {
 			var configs = [], i;
-			
-		    if (!userConfig || arguments.length == 0) {
+
+		    if (!userConfig || !arguments.length) {
 				configs.push(Plugin.defaultConfig);
 		    } else {
-				for (i=0; i<arguments.length; i++) {
+				var config;
+				for (i = 0; i < arguments.length; i++) {
 				    userConfig = arguments[i];
 				      //allow them to pass a string (url) or an object
-				    if ( (typeof userConfig) === 'string' ) {  
-						var config = $.extend({}, Plugin.defaultConfig);
+				    if ( (typeof userConfig) === 'string' ) {
+						config = $.extend({}, Plugin.defaultConfig);
 						config.url = userConfig;
 						configs.push(config);
 				    } else {
-						var config = $.extend({}, Plugin.defaultConfig, userConfig);
+						config = $.extend({}, Plugin.defaultConfig, userConfig);
 						configs.push(config);
 				    }
 				}
 		    }
-		    
+
 		    $(function() {
 		    	Plugin.setup(configs);
 		    });
 		},
-		
+
 		setup : function(configs) {
-			var i, config, jqElem;
-			
-		    for (i=0; i < configs.length; i++) {
-				config = configs[i];
-				jqElem = $('#'+config.elementId);
-				
-				if (jqElem.length) {
-				    jqElem.rssfeed(config.url,{ 
-				        titletag: 'div', 
-				        limit: config.limit, 
-				    },
-				    function(e) {
-				        $(e).find('div.rssBody').vTicker({ showItems: config.num_show, pause: config.pause });
-				    });
+			$.each(configs, function(i, config) {
+				var jqElem = $('#'+config.elementId);
+
+				if (!jqElem.length) {
+					return;
 				}
-		    }
+
+				jqElem.rssfeed(config.url,{
+			        titletag: 'div',
+			        limit: config.limit,
+			    }, function(e) {
+			        $(e).find('div.rssBody').vTicker({ showItems: config.num_show, pause: config.pause });
+			    });
+			});
 		}
     };
-    
+
     return Plugin;
 });
