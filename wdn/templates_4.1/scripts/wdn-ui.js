e34ac4b9cdaa78bbd76f563cb35f00f4e18ebb11
Michael Fairchild <mfairchild365@gmail.com>
2015-12-07 13:25:45 -0600

    close dropdowns on esc or click outside

--- wdn/templates_4.1/scripts/wdn-ui.js
+++ wdn/templates_4.1/scripts/wdn-ui.js
@@ -2,6 +2,16 @@ define(['jquery'], function($) {
 	"use strict";
 
 	var initd = false;
+
+	var closeDropDown = function(selector) {
+		$.each($(selector), function(index, element) {
+			var container_id = $(element).attr('aria-controls');
+			var $container = $('#'+container_id);
+
+			$container.attr('aria-hidden', true);
+			$(element).attr('checked', false);
+		});
+	};
 	
 	return {
 		initialize : function () {
@@ -12,6 +22,30 @@ define(['jquery'], function($) {
 			}
 			
 			this.setUpDropDownWidget('.wdn-dropdown-widget-toggle');
+
+			//Close search on escape
+			$(document).on('keydown', function(e) {
+				if (e.keyCode === 27) {
+					//Close on escape
+					closeDropDown('.wdn-dropdown-widget-toggle');
+				}
+			});
+
+			//listen for clicks on the document and close dropdowns if they don't come from a dropdown
+			$(document).on('click', function(e) {
+				var $target = $(e.target);
+				
+				//dont close this way if the control was clicked
+				if ($target.filter('.wdn-dropdown-widget-toggle, .wdn-dropdown-widget-label').length) {
+					return;
+				}
+				
+				//close all dropdown widgets
+				if (!$('.wdn-dropdown-widget-content').find(e.target).length) {
+					closeDropDown('.wdn-dropdown-widget-toggle');
+				}
+			});
+			
 			initd = true;
 		},
 
@@ -24,14 +58,20 @@ define(['jquery'], function($) {
 			//Set up the initial state for the widget
 			$(selector).each(function(index, element) {
 				//Mark this control as having a popup
-				$(element).attr('aria-haspopup', true);
+				var $element = $(element);
+
+				$element.attr('aria-haspopup', true);
 
 				//Get the dropdown-container for this control.
-				var container_id = $(element).attr('aria-controls');
+				var container_id = $element.attr('aria-controls');
 				var $container = $('#'+container_id);
 				
 				//Mark it as hidden
 				$container.attr('aria-hidden', true);
+				
+				//Add a helper class to labels
+				var $label = $('label[for="'+$element.attr('id')+'"');
+				$label.addClass('wdn-dropdown-widget-label');
 			});
 			
 			$(selector).change(function() {
@@ -41,18 +81,9 @@ define(['jquery'], function($) {
 					var $container = $('#'+container_id);
 					$container.attr('aria-hidden', false);
 				}
-				
-				
-				//Close other widgets
-				$.each($('.wdn-dropdown-widget-toggle').not(this), function(index, element) {
-					var container_id = $(element).attr('aria-controls');
-					var $container = $('#'+container_id);
 
-					$container.attr('aria-hidden', true);
-					
-					
-					$(element).attr('checked', false);
-				});
+				//Close other widgets
+				closeDropDown($('.wdn-dropdown-widget-toggle').not(this));
 			});
 		}
 	};
