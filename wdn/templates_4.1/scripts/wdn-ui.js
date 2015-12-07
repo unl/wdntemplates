fc4bf3c50370cd7e0dfc677d1426c35f3df4d101
Michael Fairchild <mfairchild365@gmail.com>
2015-12-07 10:09:20 -0600

    Improve a11y

--- wdn/templates_4.1/scripts/wdn-ui.js
+++ wdn/templates_4.1/scripts/wdn-ui.js
@@ -21,9 +21,36 @@ define(['jquery'], function($) {
 		 * @param selector
 		 */
 		setUpDropDownWidget : function(selector) {
+			//Set up the initial state for the widget
+			$(selector).each(function(index, element) {
+				//Mark this control as having a popup
+				$(element).attr('aria-haspopup', true);
+
+				//Get the dropdown-container for this control.
+				var container_id = $(element).attr('aria-controls');
+				var $container = $('#'+container_id);
+				
+				//Mark it as hidden
+				$container.attr('aria-hidden', true);
+			});
+			
 			$(selector).change(function() {
+				var $control = $(this);
+				if ($control.is(':checked')) {
+					var container_id = $control.attr('aria-controls');
+					var $container = $('#'+container_id);
+					$container.attr('aria-hidden', false);
+				}
+				
+				
 				//Close other widgets
 				$.each($('.wdn-dropdown-widget-toggle').not(this), function(index, element) {
+					var container_id = $(element).attr('aria-controls');
+					var $container = $('#'+container_id);
+
+					$container.attr('aria-hidden', true);
+					
+					
 					$(element).attr('checked', false);
 				});
 			});
