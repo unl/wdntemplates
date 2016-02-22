c2273c0e23b0813af8acef36de8e5e0a5daf775e
Kevin Abel <kabel2@unl.edu>
2016-02-22 11:12:49 -0600

    Upgrade MediaElement plugin to v2.19.0 from upstream
    Maintains patch from
    https://github.com/johndyer/mediaelement/commit/0d5201490166ff77c79cdfc9685c0005caf05fbe

--- wdn/templates_4.1/scripts/plugins/mediaelement/mep-feature-googleanalytics.js
+++ wdn/templates_4.1/scripts/plugins/mediaelement/mep-feature-googleanalytics.js
@@ -1,9 +1,3 @@
-/*
-* Google Analytics Plugin
-* Requires
-*
-*/
-
 define(['plugins/mediaelement/mediaelement-and-player', 'jquery', 'analytics'], function(mejs, $, wdn_ga) {
 	$.extend(mejs.MepDefaults, {
 		googleAnalyticsTitle: '',
@@ -13,30 +7,30 @@ define(['plugins/mediaelement/mediaelement-and-player', 'jquery', 'analytics'], 
 		googleAnalyticsEventEnded: 'Ended',
 		googleAnalyticsEventTime: 'Time'
 	});
-	
+
 	$.extend(MediaElementPlayer.prototype, {
 		buildgoogleanalytics: function(player, controls, layers, media) {
-	
+
 			media.addEventListener('play', function() {
-				wdn_ga.callTrackEvent( 
-					player.options.googleAnalyticsCategory, 
-					player.options.googleAnalyticsEventPlay, 
+				wdn_ga.callTrackEvent(
+					player.options.googleAnalyticsCategory,
+					player.options.googleAnalyticsEventPlay,
 					player.options.googleAnalyticsTitle || media.title || player.currentSrc
 	            );
 			}, false);
-			
+
 			media.addEventListener('pause', function() {
-				wdn_ga.callTrackEvent( 
-						player.options.googleAnalyticsCategory, 
-						player.options.googleAnalyticsEventPause, 
+				wdn_ga.callTrackEvent(
+						player.options.googleAnalyticsCategory,
+						player.options.googleAnalyticsEventPause,
 						player.options.googleAnalyticsTitle || media.title || player.currentSrc
 				    );
-				}, false);	
-			
+				}, false);
+
 			media.addEventListener('ended', function() {
-				wdn_ga.callTrackEvent( 
-						player.options.googleAnalyticsCategory, 
-						player.options.googleAnalyticsEventEnded, 
+				wdn_ga.callTrackEvent(
+						player.options.googleAnalyticsCategory,
+						player.options.googleAnalyticsEventEnded,
 						player.options.googleAnalyticsTitle || media.title || player.currentSrc
 				    );
 				}, false);
