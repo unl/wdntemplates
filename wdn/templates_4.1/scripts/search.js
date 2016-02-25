d94e7d7b5a0ff9ba289251ef7581d78b204f7f32
Kevin Abel <kabel2@unl.edu>
2016-02-25 16:08:05 -0600

    Cancel the auto-submit search if the search is closed
    The user has focused away or escaped, so the search should not be
    issued.

--- wdn/templates_4.1/scripts/search.js
+++ wdn/templates_4.1/scripts/search.js
@@ -135,6 +135,7 @@ define(['jquery', 'wdn', 'require', 'modernizr', 'navigation'], function($, WDN,
 				};
 
 				var closeSearch = function() {
+					clearTimeout(autoSubmitTimeout);
 					domSearchForm.parent().removeClass('active');
 					$progress.hide();
 					domSearchForm[0].reset();
