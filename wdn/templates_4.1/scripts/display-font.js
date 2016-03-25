78b69c0675d3f69e12e486fe55a5ec22b030029f
Ryan Dee <ryan@skoolbus39.com>
2016-03-25 10:53:07 -0500

    Move Mercury Display to optional plugin
    Cloud.typography updates for both optional Mercury Display and UNL 4.x core fonts will need to be coordinated during deployment

---
+++ wdn/templates_4.1/scripts/display-font.js
@@ -0,0 +1,25 @@
+define([
+	'jquery',
+	'css!js-css/display-font'
+], function($, WDN, require) {
+    var initd = false;
+
+    return {
+        initialize: function() {
+            // protect against multiple initializations
+            if (initd) {
+                return;
+            }
+            initd = true;
+
+            // Load Mercury Display fonts from Cloud.typography
+            var $displayFont = $("<link>", {
+                "rel" : "stylesheet",
+                "type" : "text/css",
+                "href" : "//cloud.typography.com/7717652/7503352/css/fonts.css"
+            });
+
+            $('head').append($displayFont);
+        }
+    };
+});
