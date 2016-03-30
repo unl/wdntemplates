438568a6569d2b95dd0ddaff11f221cc740de86b
Kevin Abel <kabel2@unl.edu>
2016-03-30 15:38:01 -0500

    Whitespace!

--- wdn/templates_4.1/scripts/tabs.js
+++ wdn/templates_4.1/scripts/tabs.js
@@ -1,58 +1,58 @@
 define(['jquery', 'wdn', 'require'], function($, WDN, require) {
-	var 
+	var
 		useHashChange = true,
-		
+
 		hashPlugin = 'plugins/hashchange/jquery.hashchange',
-		
+
 		tabSelector = 'ul.wdn_tabs',
-		
+
 		contentSelector = '.wdn_tabs_content',
-		
+
 		selected = 'selected',
-		
+
 		validRE = /^[a-z][\w:\-\.]*$/i,
-		
+
 		jq = function(id) {
 			return '#' + id.replace(/(:|\.)/g, '\\$1');
 		},
-		
+
 		getHashFromLink = function(link) {
 			var uri = link.href.split('#');
-			
+
 			if (!uri[1]) {
 				return false;
 			}
-			
+
 			var currentPage = window.location.href.split('#')[0],
 				base = document.getElementsByTagName('base')[0];
-			
+
 			if (currentPage !== uri[0] && base && base.href !== uri[0]) {
 				return false;
 			}
-			
+
 			return uri[1];
 		},
-		
+
 		getCleanHash = function() {
 			return window.location.hash.replace('#', '');
 		},
-		
+
 		updateInterface = function(trig) {
 			var tabs = trig.closest(tabSelector),
 				curr = trig.closest('li').siblings('.' + selected),
 				sibs = trig.siblings(),
 				parentTabs = trig.parents('li'),
 				nsel;
-			
+
 			// Remove any selected tab class
 			$('li.' + selected, tabs).removeClass(selected);
-			
+
 			// Hide any subtabs
 			$('ul', tabs).hide();
-			
+
 			// Add the selected class to the tab (and sub-tab)
 			parentTabs.addClass(selected);
-			
+
 			// Show any relevant sub-tabs
 			if (sibs.length || parentTabs.length > 1) {
 				if (!sibs.length) {
@@ -60,34 +60,34 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 				}
 				sibs.show();
 			}
-			
+
 			nsel = trig.closest('li').siblings('.' + selected);
 			trig.trigger('tabchanged', [curr, nsel, tabs]);
 		},
-		
+
 		firstTrig;
-	
+
 	var Plugin = {
 		initialize : function() {
 			$(function() {
 				if (WDN.getPluginParam('tabs', 'useHashChange') === false) {
 					useHashChange = false;
 				}
-				
+
 				// Set up the event for when a tab is clicked
 				var hashFromTabClick = false,
 					$tabsWithSwitch = $(tabSelector).not('.disableSwitching');
-				
+
 				$tabsWithSwitch.on('click', 'a', function() { //do something when a tab is clicked
 					var trig = $(this),
 						hash = getHashFromLink(this);
-					
+
 					if (!hash) {
 						return true;
 					}
-					
+
 					updateInterface(trig);
-					
+
 					if (!useHashChange) {
 						Plugin.displayFromHash(hash);
 					} else {
@@ -96,13 +96,13 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 							window.location.hash = hash;
 						}
 					}
-					
+
 					return false;
 				});
-				
+
 				// If we have some tabs setup the hash stuff
 				if ($tabsWithSwitch.length) {
-					var 
+					var
 						isValidTabHash = function(hash) {
 							return validRE.test(hash);
 						},
@@ -114,14 +114,14 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 							if (hash) {
 								ignoreTabs = $(jq(hash)).closest(contentSelector).prev(tabSelector);
 							}
-							
+
 							var tabs = $tabsWithSwitch.not(ignoreTabs);
-							
+
 							if (!firstTrig) {
 								firstTrig = [];
 								tabs.each(function() {
 									var selPrefix = 'li.selected';
-									
+
 									if (!$(selPrefix, this).length) {
 										selPrefix = '> li';
 									}
@@ -129,7 +129,7 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 								});
 								firstTrig = $(firstTrig);
 							}
-							
+
 							firstTrig.each(function() {
 								var innerTrig = $(this);
 								var hash = getHashFromLink(this);
@@ -140,7 +140,7 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 								Plugin.displayFromHash(hash);
 							});
 						};
-					
+
 					if (useHashChange) {
 						var setupHashChange = function() {
 							var firstRun = true;
@@ -149,10 +149,10 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 								if (hash && !isValidTabHash(hash)) {
 									return true;
 								}
-								
+
 								if (isTabExists(hash)) {
 									Plugin.displayFromHash(hash, firstRun || !hashFromTabClick);
-									
+
 									if (firstRun) {
 										setupFirstHash(hash);
 										firstRun = false;
@@ -169,7 +169,7 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 							});
 							$(window).hashchange();
 						};
-						
+
 						if (!$.fn.hashchange) {
 							require([hashPlugin], setupHashChange);
 						} else {
@@ -188,14 +188,14 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 				}
 			});
 		},
-		
+
 		displayFromHash: function(hash, forceUpdate) {
 			var sel = $(jq(hash)),
 				tabContents = sel.closest(contentSelector);
 			tabContents.children().hide();
 			sel.show().parentsUntil(contentSelector).show();
 			sel.find('ul.slides').css({'height':'auto'});
-			
+
 			if (forceUpdate) {
 				var trig = $(tabSelector + ' li a[href$='+jq(hash)+']');
 				if (trig.length) {
@@ -205,6 +205,6 @@ define(['jquery', 'wdn', 'require'], function($, WDN, require) {
 			}
 		}
 	};
-	
+
 	return Plugin;
 });
