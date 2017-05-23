d2fe68c64d879e1a0d0ce7df7303b58a8355c67e
Tyler Lemburg <lemburg@unl.edu>
2017-05-23 15:18:50 -0500

    Change process to not use sessionStorage to determine cache state
    And always use loadCSS to load the stylesheet.

--- wdn/templates_4.1/scripts/load-deferred-css.js
+++ wdn/templates_4.1/scripts/load-deferred-css.js
@@ -1,25 +1,11 @@
 (function() {
-    var WDNLoadDeferredStyles = function() {
-        var sheet = document.createElement('link');
-        sheet.rel = 'stylesheet';
-        sheet.href = 'https://unlcms.unl.edu/wdn/templates_4.1/css/all.css?dep=$DEP_VERSION$';
-        document.getElementsByTagName('head')[0].appendChild(sheet);
-    };
-
     var WDNRemoveCriticalStyles = function() {
         document.getElementById('wdn-critical-styles').remove();
     };
 
-    if (sessionStorage.getItem('wdn-css-is-cached')) {
-        WDNLoadDeferredStyles();
-        setTimeout(function() {
-            WDNRemoveCriticalStyles();
-        }, 1000);
-    } else {
-        var deferredStyles = loadCSS('https://unlcms.unl.edu/wdn/templates_4.1/css/all.css?dep=$DEP_VERSION$');
-        onloadCSS(deferredStyles, function() {
-            sessionStorage.setItem('wdn-css-is-cached', true);
-            WDNRemoveCriticalStyles();
-        });
-    }
+    var deferredStyles = loadCSS('https://unlcms.unl.edu/wdn/templates_4.1/css/all.css?dep=$DEP_VERSION$');
+    onloadCSS(deferredStyles, function() {
+        WDNRemoveCriticalStyles();
+    });
+
 })();
