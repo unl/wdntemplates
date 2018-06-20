24932928485b3e97a7c207f498b1684d83ad85cf
Ryan Dee <ryan@skoolbus39.com>
2018-06-20 14:29:33 -0500

    Show/hide breadcrumbs in nav/menu region

--- wdn/templates_5.0/js-src/navigation.babel.js
+++ wdn/templates_5.0/js-src/navigation.babel.js
@@ -1,6 +1,6 @@
 define([], function() {
     "use strict";
-    
+
     let initialized = false;
 
     let Plugin = {
@@ -11,7 +11,7 @@ define([], function() {
 
             // grab an element
             let mobileActions = document.querySelectorAll('.hrjs');
-            
+
             // construct an instance of Headroom, passing the element
             mobileActions.forEach((elem)=>{
                 let headroom = new Headroom(elem, {
@@ -22,13 +22,16 @@ define([], function() {
                 });
                 headroom.init();
             });
-            
+
+
+
             let toggleMenu = document.getElementById('dcf-menu-toggle');
             let mobileNav = document.getElementById('dcf-navigation');
             let modalParent = toggleMenu.closest('.dcf-modal-parent');
             let body = document.querySelector('body');
+            let breadcrumbs = document.getElementById('dcf-breadcrumbs');
             let firstLink = mobileNav.querySelector('a');
-            
+
             function onKeyUp(e) {
                 if (e.keyCode === 27) {
                     closeModal();
@@ -39,15 +42,19 @@ define([], function() {
                 body.classList.remove('dcf-overflow-auto');
                 body.classList.add('dcf-overflow-hidden');
                 modalParent.classList.add('dcf-modal-open');
+                breadcrumbs.classList.remove('dcf-d-none');
                 toggleMenu.setAttribute('aria-expanded', 'true');
                 firstLink.focus();
                 modalParent.addEventListener('keyup', onKeyUp);
             }
-            
+
             function closeModal() {
                 body.classList.remove('dcf-overflow-hidden');
                 body.classList.add('dcf-overflow-auto');
                 modalParent.classList.remove('dcf-modal-open');
+                setTimeout(function(){
+                  breadcrumbs.classList.add('dcf-d-none');
+                }, 400);
                 toggleMenu.setAttribute('aria-expanded', 'false');
                 toggleMenu.focus();
                 modalParent.removeEventListener('keyup', onKeyUp);
