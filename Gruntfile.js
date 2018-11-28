9ebaf9f65f94875c8588522c943682b7145dcadb
Jeffrey Sturek <jsturek8@unl.edu>
2018-11-28 11:27:30 -0600

    Implement CTA navigation

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,7 +107,8 @@ module.exports = function (grunt) {
 		'!skipnav.*',
 		'!socialmediashare.*',
 		'!unlalert.*',
-		'!wdn*'
+		'!wdn*',
+		'!cta-nav.*'
 	];
 
 	// requirejs configuration and customization options
--- wdn/templates_5.0/js-src/main-wdn-plugins.babel.js
+++ wdn/templates_5.0/js-src/main-wdn-plugins.babel.js
@@ -1,12 +1,13 @@
 "use strict";
 
 define([
-'analytics', //this also loads the IDM plugin
-'navigation', //TODO: do we need navigation js?
-'search', //TODO: add search
-'unlalert',
-'legacy',
-'hover_intent'
+	'analytics', //this also loads the IDM plugin
+	'navigation',
+	'search',
+	'unlalert',
+	'legacy',
+	'hover_intent',
+	'cta-nav'
 //'images', //TODO: do we need 'images'?
 //'form_validation', //TODO: do we need to init form_validation here?
 //'wdn-ui' //TODO: do we need js?
