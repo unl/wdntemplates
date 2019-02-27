70a2126d739ad0e799883a70596c0a47aa0fda70
Ryan Dee <ryan@skoolbus39.com>
2019-02-27 14:37:03 -0600

    Add JavaScript test for WebP browser support

--- wdn/templates_5.0/js-src/mustard-initializer.babel.js
+++ wdn/templates_5.0/js-src/mustard-initializer.babel.js
@@ -1,4 +1,4 @@
-// loading polyfill.io
+// Load Polyfill.io
 (function() {
   var el = document.createElement("script");
   el.async = false;
@@ -6,10 +6,17 @@
   document.body.appendChild(el);
 })();
 
+
 require(['require',], () => {
+
+  // Test for object-fit browser support, load polyfill if needed
 	if(!('objectFit' in document.body.style)) {
 		require(['mustard/ofi'], () => {
 			objectFitImages();
 		})
 	}
+
+  // Require test for WebP browser support
+  require(['utility-scripts/testForWebP',], () => {});
+
 });
