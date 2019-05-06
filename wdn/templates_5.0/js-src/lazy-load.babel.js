0955394912edff77f128855e70f024bc33240934
Ryan Dee <ryan@skoolbus39.com>
2019-05-06 12:55:45 -0500

    Use loading attribute to lazy load images
    Use `loading` attribute instead of `dcf-lazy-img` class for lazy loading.

--- wdn/templates_5.0/js-src/lazy-load.babel.js
+++ wdn/templates_5.0/js-src/lazy-load.babel.js
@@ -1,6 +1,6 @@
 require (['dcf-lazyLoad'], (LazyLoad) => {
 
-  const images = document.querySelectorAll('.dcf-lazy-img');
+  const images = document.querySelectorAll('[loading=lazy]');
   const observerConfig = {
   	// extend intersection root margin by 50px to start intersection earlier by 50px
   	rootMargin: '0px 0px 50px 0px',
