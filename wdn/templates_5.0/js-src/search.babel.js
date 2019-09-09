d2d1b2952c3a58e6f927889d66002efd90865ead
Ryan Dee <ryan@skoolbus39.com>
2019-09-09 13:59:23 -0500

    Update search JavaScript
    - Remove inert-polyfill
    - Change search toggle links to buttons and set button attributes
    - Add constants for page elements and tab-focusable elements
    - Remove `hidden` attribute from search modal on page load and replace with utility classes
    - Adjust classes and ARIA attributes toggled for search modal
    - Trap keyboard focus in modal when open

--- wdn/templates_5.0/js-src/search.babel.js
+++ wdn/templates_5.0/js-src/search.babel.js
@@ -1,4 +1,4 @@
-define(['wdn', 'require', 'plugins/body-scroll-lock', 'mustard/inert-polyfill'], function(WDN, require, bodyScrollLock) {
+define(['wdn', 'require', 'plugins/body-scroll-lock'], function(WDN, require, bodyScrollLock) {
   const disableBodyScroll = bodyScrollLock.disableBodyScroll;
   const enableBodyScroll = bodyScrollLock.enableBodyScroll;
   let autoSearchDebounceDelay = 1000;
@@ -23,17 +23,51 @@ define(['wdn', 'require', 'plugins/body-scroll-lock', 'mustard/inert-polyfill'],
 
       initd = true;
 
-      let domQ = document.getElementById('dcf-search_query'),
-        domSearchForm = document.getElementById('dcf-search-form'),
+      // Change search links to button and set button attributes
+      let domDesktopSearchLink = document.getElementById('dcf-search-toggle-link'),
+        domDesktopSearchBtn = document.createElement('button');
+      domDesktopSearchLink.replaceWith(domDesktopSearchBtn);
+      domDesktopSearchBtn.classList.add('dcf-nav-toggle-btn', 'dcf-nav-toggle-btn-search', 'dcf-search-toggle', 'dcf-d-flex', 'dcf-ai-center', 'dcf-jc-between', 'dcf-w-100%', 'dcf-p-0', 'dcf-txt-2xs', 'dcf-bg-transparent', 'dcf-b-1', 'dcf-b-solid', 'unl-b-scarlet', 'unl-font-sans', 'unl-scarlet');
+      domDesktopSearchBtn.setAttribute('id', 'dcf-search-toggle');
+      domDesktopSearchBtn.setAttribute('type', 'button');
+      domDesktopSearchBtn.setAttribute('aria-expanded', 'false');
+      domDesktopSearchBtn.setAttribute('aria-label', 'Open search');
+      domDesktopSearchBtn.innerHTML = '<span class="dcf-search-toggle-label dcf-flex-grow-1 dcf-txt-left">Search</span><span class="dcf-d-flex dcf-ai-center dcf-jc-center dcf-w-8 dcf-pt-3 dcf-pb-3 unl-bg-scarlet unl-cream"><svg class="dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 48 48"><path d="M18 36a17.9 17.9 0 0 0 11.27-4l15.31 15.41a2 2 0 0 0 2.84-2.82L32.08 29.18A18 18 0 1 0 18 36zm0-32A14 14 0 1 1 4 18 14 14 0 0 1 18 4z"/></svg></span>';
+
+      let domMobileSearchLink = document.getElementById('dcf-mobile-search-link'),
+        domMobileSearchBtn = document.createElement('button');
+      domMobileSearchLink.replaceWith(domMobileSearchBtn);
+      domMobileSearchBtn.classList.add('dcf-nav-toggle-btn', 'dcf-nav-toggle-btn-search', 'dcf-d-flex', 'dcf-flex-col', 'dcf-ai-center', 'dcf-jc-center', 'dcf-flex-grow-1', 'dcf-h-9', 'dcf-p-0', 'dcf-b-0', 'dcf-bg-transparent', 'unl-scarlet');
+      domMobileSearchBtn.setAttribute('id', 'dcf-mobile-toggle-search');
+      domMobileSearchBtn.setAttribute('type', 'button');
+      domMobileSearchBtn.setAttribute('aria-expanded', 'false');
+      domMobileSearchBtn.setAttribute('aria-label', 'Open search');
+      domMobileSearchBtn.innerHTML = '<svg class="dcf-txt-sm dcf-h-6 dcf-w-6 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 24 24"><g id="dcf-nav-toggle-icon-open-search" class=""><path d="M22.5 21.8L15 14.3c1.2-1.4 2-3.3 2-5.3 0-4.4-3.6-8-8-8S1 4.6 1 9s3.6 8 8 8c2 0 3.9-.8 5.3-2l7.5 7.5c.2.2.5.2.7 0 .2-.2.2-.5 0-.7zM9 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7z"></path></g><g id="dcf-nav-toggle-icon-close-search" class="dcf-d-none"><path d="M20.5 4.2L4.2 20.5c-.2.2-.5.2-.7 0-.2-.2-.2-.5 0-.7L19.8 3.5c.2-.2.5-.2.7 0 .2.2.2.5 0 .7z"></path><path d="M3.5 4.2l16.3 16.3c.2.2.5.2.7 0s.2-.5 0-.7L4.2 3.5c-.2-.2-.5-.2-.7 0-.2.2-.2.5 0 .7z"></path></g></svg><span class="dcf-nav-toggle-label-search dcf-mt-1 dcf-txt-2xs">Search</span>';
+
+      let domDialog = document.getElementById('dcf-search-results'),
         domSearchResultWrapper = document.getElementById('dcf-search-results-wrapper'),
-        domDialog = document.getElementById('dcf-search-results'),
+        domQ = document.getElementById('dcf-search_query'),
+        domSearchForm = document.getElementById('dcf-search-form'),
         domToggleButtons = document.querySelectorAll('.dcf-nav-toggle-btn-search'),
         domToggleIconOpen = document.getElementById('dcf-nav-toggle-icon-open-search'),
         domToggleIconClose = document.getElementById('dcf-nav-toggle-icon-close-search'),
         domToggleLabel = document.querySelector('.dcf-nav-toggle-label-search'),
         domClose = document.getElementById('dcf-close-search'),
+        skipNav = document.getElementById('dcf-skip-nav'),
+        institutionTitle = document.getElementById('dcf-institution-title'),
+        idm = document.getElementById('dcf-idm'),
+        logo = document.getElementById('dcf-logo-lockup'),
+        nav = document.getElementById('dcf-navigation'),
         main = document.querySelector('main'),
         footer = document.getElementById('dcf-footer'),
+        tabFocusEls = domDialog.querySelectorAll('button:not([hidden]):not([disabled]), ' +
+          '[href]:not([hidden]), input:not([hidden]):not([type="hidden"]):not([disabled]), ' +
+          'select:not([hidden]):not([disabled]), textarea:not([hidden]):not([disabled]), ' +
+          '[tabindex="0"]:not([hidden]):not([disabled]), summary:not([hidden]), ' +
+          '[contenteditable]:not([hidden]), audio[controls]:not([hidden]), ' +
+          'video[controls]:not([hidden])'),
+        firstTabFocusEl = tabFocusEls[0],
+        lastTabFocusEl = tabFocusEls[tabFocusEls.length - 1],
         domActiveToggleButton,
         domEmbed,
         $unlSearch,
@@ -57,6 +91,14 @@ define(['wdn', 'require', 'plugins/body-scroll-lock', 'mustard/inert-polyfill'],
         return;
       }
 
+      // Check if search modal has `hidden` attribute
+      if (domDialog.hasAttribute('hidden')) {
+        // Remove `hidden` attribute
+        domDialog.removeAttribute('hidden');
+        // Replace with these classes
+        domDialog.classList.add('dcf-opacity-0', 'dcf-pointer-events-none', 'dcf-invisible');
+      }
+
       function modalTransition(event) {
         domDialog.removeEventListener('transitionend', modalTransition);
         if (!domDialog.classList.contains('dcf-invisible')) {
@@ -71,14 +113,20 @@ define(['wdn', 'require', 'plugins/body-scroll-lock', 'mustard/inert-polyfill'],
           // Search is currently closed, so open it.
           for (let i = 0; i < domToggleButtons.length; i++) {
             domToggleButtons[i].setAttribute('aria-expanded', 'true');
-            domToggleButtons[i].setAttribute('aria-label', 'close search');
+            domToggleButtons[i].setAttribute('aria-label', 'Close search');
           }
           domToggleIconOpen.classList.add('dcf-d-none');
           domToggleIconClose.classList.remove('dcf-d-none');
           domToggleLabel.textContent = 'Close';
-          main.setAttribute('inert', '');
-          footer.setAttribute('inert', '');
-          domDialog.classList.remove('dcf-invisible');
+          skipNav.setAttribute('aria-hidden', 'true');
+          institutionTitle.setAttribute('aria-hidden', 'true');
+          idm.setAttribute('aria-hidden', 'true');
+          logo.setAttribute('aria-hidden', 'true');
+          nav.setAttribute('aria-hidden', 'true');
+          main.setAttribute('aria-hidden', 'true');
+          footer.setAttribute('aria-hidden', 'true');
+          domDialog.classList.remove('dcf-opacity-0', 'dcf-pointer-events-none', 'dcf-invisible');
+          domDialog.classList.add('dcf-opacity-100', 'dcf-pointer-events-auto');
           domDialog.setAttribute('aria-hidden', 'false');
           domActiveToggleButton = this;
 
@@ -208,13 +256,20 @@ define(['wdn', 'require', 'plugins/body-scroll-lock', 'mustard/inert-polyfill'],
         clearTimeout(autoSubmitTimeout);
         domQ.value = '';
         domSearchForm.parentElement.classList.remove('active');
-        main.removeAttribute('inert');
-        footer.removeAttribute('inert');
+        skipNav.setAttribute('aria-hidden', 'false');
+        institutionTitle.setAttribute('aria-hidden', 'false');
+        idm.setAttribute('aria-hidden', 'false');
+        logo.setAttribute('aria-hidden', 'false');
+        nav.setAttribute('aria-hidden', 'false');
+        main.setAttribute('aria-hidden', 'false');
+        footer.setAttribute('aria-hidden', 'false');
         domDialog.addEventListener('transitionend', modalTransition);
+        domDialog.classList.remove('dcf-opacity-100', 'dcf-pointer-events-auto');
+        domDialog.classList.add('dcf-opacity-0', 'dcf-pointer-events-none');
         domDialog.setAttribute('aria-hidden', 'true');
         for (let i = 0; i < domToggleButtons.length; i++) {
           domToggleButtons[i].setAttribute('aria-expanded', 'false');
-          domToggleButtons[i].setAttribute('aria-label', 'open search');
+          domToggleButtons[i].setAttribute('aria-label', 'Open search');
         }
         domToggleIconOpen.classList.remove('dcf-d-none');
         domToggleIconClose.classList.add('dcf-d-none');
@@ -235,6 +290,32 @@ define(['wdn', 'require', 'plugins/body-scroll-lock', 'mustard/inert-polyfill'],
           domActiveToggleButton.focus();
         }
       };
+
+      // Trap focus inside the search modal
+      domDialog.addEventListener('keydown', function(e) {
+
+        const keycodeTab = 9;
+
+        let isTabPressed = e.key === 'Tab' || e.keyCode === keycodeTab;
+
+        if (!isTabPressed) {
+          return;
+        }
+
+        if (e.key === 'Tab' || e.keyCode === keycodeTab) {
+          if ( e.shiftKey ) { // Tab backwards (shift + tab)
+            if (document.activeElement === firstTabFocusEl) {
+              e.preventDefault();
+              lastTabFocusEl.focus();
+            }
+          } else { // Tab forwards
+            if (document.activeElement === lastTabFocusEl) {
+              e.preventDefault();
+              firstTabFocusEl.focus();
+            }
+          }
+        }
+      });
 
       // Add an event listener for closeSearchEvent
       document.addEventListener('closeSearch', function(e) {
