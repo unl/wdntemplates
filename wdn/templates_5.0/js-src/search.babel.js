6b9e55273adcfffb1501de3e6b4f718133f878dc
Jeffrey Sturek <jsturek8@unl.edu>
2019-09-24 09:49:34 -0500

    Fixes to prevent errors if dom elements are missing

--- wdn/templates_5.0/js-src/search.babel.js
+++ wdn/templates_5.0/js-src/search.babel.js
@@ -26,26 +26,38 @@ define(['wdn', 'require', 'plugins/body-scroll-lock'], function(WDN, require, bo
       // Change search links to button and set button attributes
       let domDesktopSearchLink = document.getElementById('dcf-search-toggle-link'),
         domDesktopSearchBtn = document.createElement('button');
-      domDesktopSearchLink.replaceWith(domDesktopSearchBtn);
-      domDesktopSearchBtn.classList.add('dcf-nav-toggle-btn', 'dcf-nav-toggle-btn-search', 'dcf-search-toggle', 'dcf-d-flex', 'dcf-ai-center', 'dcf-jc-between', 'dcf-w-100%', 'dcf-p-0', 'dcf-txt-2xs', 'dcf-bg-transparent', 'dcf-b-1', 'dcf-b-solid', 'unl-b-scarlet', 'unl-font-sans', 'unl-scarlet');
-      domDesktopSearchBtn.setAttribute('id', 'dcf-search-toggle');
-      domDesktopSearchBtn.setAttribute('type', 'button');
-      domDesktopSearchBtn.setAttribute('aria-expanded', 'false');
-      domDesktopSearchBtn.setAttribute('aria-label', 'Open search');
-      domDesktopSearchBtn.innerHTML = '<span class="dcf-search-toggle-label dcf-flex-grow-1 dcf-txt-left">Search</span><span class="dcf-d-flex dcf-ai-center dcf-jc-center dcf-w-8 dcf-pt-3 dcf-pb-3 unl-bg-scarlet unl-cream"><svg class="dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 48 48"><path d="M18 36a17.9 17.9 0 0 0 11.27-4l15.31 15.41a2 2 0 0 0 2.84-2.82L32.08 29.18A18 18 0 1 0 18 36zm0-32A14 14 0 1 1 4 18 14 14 0 0 1 18 4z"/></svg></span>';
+
+      if (domDesktopSearchLink) {
+        domDesktopSearchLink.replaceWith(domDesktopSearchBtn);
+        domDesktopSearchBtn.classList.add('dcf-nav-toggle-btn', 'dcf-nav-toggle-btn-search', 'dcf-search-toggle', 'dcf-d-flex', 'dcf-ai-center', 'dcf-jc-between', 'dcf-w-100%', 'dcf-p-0', 'dcf-txt-2xs', 'dcf-bg-transparent', 'dcf-b-1', 'dcf-b-solid', 'unl-b-scarlet', 'unl-font-sans', 'unl-scarlet');
+        domDesktopSearchBtn.setAttribute('id', 'dcf-search-toggle');
+        domDesktopSearchBtn.setAttribute('type', 'button');
+        domDesktopSearchBtn.setAttribute('aria-expanded', 'false');
+        domDesktopSearchBtn.setAttribute('aria-label', 'Open search');
+        domDesktopSearchBtn.innerHTML = '<span class="dcf-search-toggle-label dcf-flex-grow-1 dcf-txt-left">Search</span><span class="dcf-d-flex dcf-ai-center dcf-jc-center dcf-w-8 dcf-pt-3 dcf-pb-3 unl-bg-scarlet unl-cream"><svg class="dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 48 48"><path d="M18 36a17.9 17.9 0 0 0 11.27-4l15.31 15.41a2 2 0 0 0 2.84-2.82L32.08 29.18A18 18 0 1 0 18 36zm0-32A14 14 0 1 1 4 18 14 14 0 0 1 18 4z"/></svg></span>';
+      }
+
 
       let domMobileSearchLink = document.getElementById('dcf-mobile-search-link'),
         domMobileSearchBtn = document.createElement('button');
-      domMobileSearchLink.replaceWith(domMobileSearchBtn);
-      domMobileSearchBtn.classList.add('dcf-nav-toggle-btn', 'dcf-nav-toggle-btn-search', 'dcf-d-flex', 'dcf-flex-col', 'dcf-ai-center', 'dcf-jc-center', 'dcf-flex-grow-1', 'dcf-h-9', 'dcf-p-0', 'dcf-b-0', 'dcf-bg-transparent', 'unl-scarlet');
-      domMobileSearchBtn.setAttribute('id', 'dcf-mobile-toggle-search');
-      domMobileSearchBtn.setAttribute('type', 'button');
-      domMobileSearchBtn.setAttribute('aria-expanded', 'false');
-      domMobileSearchBtn.setAttribute('aria-label', 'Open search');
-      domMobileSearchBtn.innerHTML = '<svg class="dcf-txt-sm dcf-h-6 dcf-w-6 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 24 24"><g id="dcf-nav-toggle-icon-open-search" class=""><path d="M22.5 21.8L15 14.3c1.2-1.4 2-3.3 2-5.3 0-4.4-3.6-8-8-8S1 4.6 1 9s3.6 8 8 8c2 0 3.9-.8 5.3-2l7.5 7.5c.2.2.5.2.7 0 .2-.2.2-.5 0-.7zM9 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7z"></path></g><g id="dcf-nav-toggle-icon-close-search" class="dcf-d-none"><path d="M20.5 4.2L4.2 20.5c-.2.2-.5.2-.7 0-.2-.2-.2-.5 0-.7L19.8 3.5c.2-.2.5-.2.7 0 .2.2.2.5 0 .7z"></path><path d="M3.5 4.2l16.3 16.3c.2.2.5.2.7 0s.2-.5 0-.7L4.2 3.5c-.2-.2-.5-.2-.7 0-.2.2-.2.5 0 .7z"></path></g></svg><span class="dcf-nav-toggle-label-search dcf-mt-1 dcf-txt-2xs">Search</span>';
 
-      let domDialog = document.getElementById('dcf-search-results'),
-        domSearchResultWrapper = document.getElementById('dcf-search-results-wrapper'),
+      if (domMobileSearchLink) {
+        domMobileSearchLink.replaceWith(domMobileSearchBtn);
+        domMobileSearchBtn.classList.add('dcf-nav-toggle-btn', 'dcf-nav-toggle-btn-search', 'dcf-d-flex', 'dcf-flex-col', 'dcf-ai-center', 'dcf-jc-center', 'dcf-flex-grow-1', 'dcf-h-9', 'dcf-p-0', 'dcf-b-0', 'dcf-bg-transparent', 'unl-scarlet');
+        domMobileSearchBtn.setAttribute('id', 'dcf-mobile-toggle-search');
+        domMobileSearchBtn.setAttribute('type', 'button');
+        domMobileSearchBtn.setAttribute('aria-expanded', 'false');
+        domMobileSearchBtn.setAttribute('aria-label', 'Open search');
+        domMobileSearchBtn.innerHTML = '<svg class="dcf-txt-sm dcf-h-6 dcf-w-6 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 24 24"><g id="dcf-nav-toggle-icon-open-search" class=""><path d="M22.5 21.8L15 14.3c1.2-1.4 2-3.3 2-5.3 0-4.4-3.6-8-8-8S1 4.6 1 9s3.6 8 8 8c2 0 3.9-.8 5.3-2l7.5 7.5c.2.2.5.2.7 0 .2-.2.2-.5 0-.7zM9 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7z"></path></g><g id="dcf-nav-toggle-icon-close-search" class="dcf-d-none"><path d="M20.5 4.2L4.2 20.5c-.2.2-.5.2-.7 0-.2-.2-.2-.5 0-.7L19.8 3.5c.2-.2.5-.2.7 0 .2.2.2.5 0 .7z"></path><path d="M3.5 4.2l16.3 16.3c.2.2.5.2.7 0s.2-.5 0-.7L4.2 3.5c-.2-.2-.5-.2-.7 0-.2.2-.2.5 0 .7z"></path></g></svg><span class="dcf-nav-toggle-label-search dcf-mt-1 dcf-txt-2xs">Search</span>';
+      }
+
+      let domDialog = document.getElementById('dcf-search-results');
+      if (!domDialog) {
+        domDialog = document.createElement('div');
+        domDialog.setAttribute('id', 'dcf-search-results');
+      }
+
+      let domSearchResultWrapper = document.getElementById('dcf-search-results-wrapper'),
         domQ = document.getElementById('dcf-search_query'),
         domSearchForm = document.getElementById('dcf-search-form'),
         domToggleButtons = document.querySelectorAll('.dcf-nav-toggle-btn-search'),
@@ -61,13 +73,13 @@ define(['wdn', 'require', 'plugins/body-scroll-lock'], function(WDN, require, bo
         main = document.querySelector('main'),
         footer = document.getElementById('dcf-footer'),
         tabFocusEls = domDialog.querySelectorAll('button:not([hidden]):not([disabled]), ' +
-          '[href]:not([hidden]), input:not([hidden]):not([type="hidden"]):not([disabled]), ' +
-          'select:not([hidden]):not([disabled]), textarea:not([hidden]):not([disabled]), ' +
-          '[tabindex="0"]:not([hidden]):not([disabled]), summary:not([hidden]), ' +
-          '[contenteditable]:not([hidden]), audio[controls]:not([hidden]), ' +
-          'video[controls]:not([hidden])'),
-        firstTabFocusEl = tabFocusEls[0],
-        lastTabFocusEl = tabFocusEls[tabFocusEls.length - 1],
+            '[href]:not([hidden]), input:not([hidden]):not([type="hidden"]):not([disabled]), ' +
+            'select:not([hidden]):not([disabled]), textarea:not([hidden]):not([disabled]), ' +
+            '[tabindex="0"]:not([hidden]):not([disabled]), summary:not([hidden]), ' +
+            '[contenteditable]:not([hidden]), audio[controls]:not([hidden]), ' +
+            'video[controls]:not([hidden])'),
+            firstTabFocusEl = tabFocusEls[0],
+            lastTabFocusEl = tabFocusEls[tabFocusEls.length - 1],
         domActiveToggleButton,
         domEmbed,
         $unlSearch,
