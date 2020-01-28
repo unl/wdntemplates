6965cc7b81bcedb186456459c26ee6c7084789fd
Peter Nguyen <petervnguyen99@gmail.com>
2020-01-28 11:37:51 -0600

    Added missing semicolon.

--- wdn/templates_5.1/js-src/tabs.babel.js
+++ wdn/templates_5.1/js-src/tabs.babel.js
@@ -2,4 +2,4 @@ require(['dcf-utility', 'dcf-tabs'], (Utility, Tabs) => {
     const tabs = document.querySelectorAll('.dcf-tabs')
     const unlTabs = new DCFTabs(tabs);
     unlTabs.initialize();
-})
+});
