38b6551aa6e6291ba0f2c97b70367600a4d150e2
Ryan Dee <ryan@skoolbus39.com>
2020-01-29 10:08:16 -0600

    Prefix `webp`/`no-webp` classes with `dcf-`

--- wdn/templates_5.1/js-src/utility-scripts/testForWebP.js
+++ wdn/templates_5.1/js-src/utility-scripts/testForWebP.js
@@ -23,24 +23,24 @@
     webP.src = "data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wA" + "iMwAgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA";
   };
 
-  // Add 'webp' class to html element if supported.
-  // Add 'no-webp' class to html element if not supported.
+  // Add 'dcf-webp' class to html element if supported.
+  // Add 'dcf-no-webp' class to html element if not supported.
   // @param {Boolean} support - WebP format support.
   function addWebPClass(support) {
     var el = document.documentElement;
 
     if (support) {
       if (el.classList) {
-        el.classList.add("webp");
+        el.classList.add("dcf-webp");
       } else {
-        el.className += " webp";
+        el.className += " dcf-webp";
       }
       window.sessionStorage.setItem("webpSupport", true);
     } else {
       if (el.classList) {
-        el.classList.add("no-webp");
+        el.classList.add("dcf-no-webp");
       } else {
-        el.className += " no-webp";
+        el.className += " dcf-no-webp";
       }
     }
   };
