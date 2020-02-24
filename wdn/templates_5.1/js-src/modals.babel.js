a846d5deeaa52f7436d260921515dbc5b2097eea
Ryan Dee <ryan@skoolbus39.com>
2020-02-24 16:08:40 -0600

    Move modal styles from js-src to Sass components

--- wdn/templates_5.1/js-src/modals.babel.js
+++ wdn/templates_5.1/js-src/modals.babel.js
@@ -1,4 +1,4 @@
-require (['dcf-utility', 'dcf-modal', 'plugins/body-scroll-lock', 'css!js-css/modals'], (Utility, Modal, bodyScrollLock) => {
+require (['dcf-utility', 'dcf-modal', 'plugins/body-scroll-lock'], (Utility, Modal, bodyScrollLock) => {
   const modals = document.querySelectorAll('.dcf-modal');
   const unlModal = new DCFModal(modals, bodyScrollLock);
   unlModal.initialize();
