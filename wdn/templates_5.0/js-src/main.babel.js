e8ce54ae11233beb49b3ff05bda56791d1b56fec
Jeff Sturek <jsturek8@unl.edu>
2020-03-13 12:08:14 -0500

    Undo change

--- wdn/templates_5.0/js-src/main.babel.js
+++ wdn/templates_5.0/js-src/main.babel.js
@@ -21,24 +21,6 @@ requirejs([
         require([unlchat_url + '?for=client&version=' + WDN.getHTMLVersion()], function(){});
     });
 
-    // WDN Notice Banner Display
-    var bannerEnabled = true;
-    if (bannerEnabled) {
-	    var xhr = new XMLHttpRequest();
-	    var bannerContentURL = 'http://its-unl-cms-prd-s3.s3.amazonaws.com/test2.html';
-		xhr.open('GET', bannerContentURL, false);
-		xhr.send(null);
-		if (xhr.status === 200) {
-			var xhrContent = xhr.responseText;
-			if (xhrContent) {
-				var banner = document.createElement('div');
-				banner.innerHTML = xhrContent;
-				document.body.prepend(banner);
-				window.scrollTo(0, 0);
-			}
-		}
-	}
-
     // Process deferred inline scripts
     window.dispatchEvent(new Event('inlineJSReady'));
 });
