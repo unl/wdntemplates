03c885030e71c746440c24e5c02052b8c81ce11d
Jeff Sturek <jsturek8@unl.edu>
2020-03-13 13:29:28 -0500

    Move banner display to markup

--- wdn/templates_5.0/js-src/main.babel.js
+++ wdn/templates_5.0/js-src/main.babel.js
@@ -20,26 +20,7 @@ requirejs([
     WDN.loadJQuery(function() {
         require([unlchat_url + '?for=client&version=' + WDN.getHTMLVersion()], function(){});
     });
-
-    // WDN Notice Banner Display
-    var bannerEnabled = true;
-    if (bannerEnabled) {
-	    var xhr = new XMLHttpRequest();
-	    var bannerContentURL = 'http://its-unl-cms-prd-s3.s3.amazonaws.com/test2.html';
-		xhr.open('GET', bannerContentURL, false);
-		xhr.send(null);
-		if (xhr.status === 200) {
-			var xhrContent = xhr.responseText;
-			if (xhrContent) {
-				var banner = document.createElement('div');
-				banner.setAttribute('role', 'navigation');
-				banner.innerHTML = xhrContent;
-				document.body.prepend(banner);
-				window.scrollTo(0, 0);
-			}
-		}
-	}
-
+    
     // Process deferred inline scripts
     window.dispatchEvent(new Event('inlineJSReady'));
 });
