5db75fcffed16b7a2094af2706429517c12193ed
Ryan Dee <ryan@skoolbus39.com>
2020-06-03 16:58:32 -0500

    Update theme class prefix to `unl-`

--- wdn/templates_5.1/js-src/slideshows.babel.js
+++ wdn/templates_5.1/js-src/slideshows.babel.js
@@ -1,4 +1,4 @@
-require(['dcf-utility', 'dcf-slideshow', 'css!js-css/slideshows'], (Utility, DCFSlideshow) => {
+require(['dcf-utility', 'dcf-slideshow', 'css!js-css/slideshows'], () => {
   const openCaptionEvent = new Event('openCaption');
   const closeCaptionEvent = new Event('closeCaption');
   const slideshows = document.querySelectorAll('.dcf-slideshow');
@@ -85,17 +85,17 @@ require(['dcf-utility', 'dcf-slideshow', 'css!js-css/slideshows'], (Utility, DCF
         captionBtn.innerHTML =
           '<svg class="dcf-h-4 dcf-w-4 dcf-fill-current" ' +
           'width="24" height="24" viewBox="0 0 24 24" aria-hidden="true">' +
-          '<path class="theme-icon-slide-caption-open" ' +
+          '<path class="unl-icon-slide-caption-open" ' +
           'd="M1,3h19c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1C0.4,1,0,1.4,0,2C0,2.6,0.4,3,1,3z"/>' +
-          '<path class="theme-icon-slide-caption-open" ' +
+          '<path class="unl-icon-slide-caption-open" ' +
           'd="M1,8h15c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1C0.4,6,0,6.4,0,7C0,7.6,0.4,8,1,8z"/>' +
-          '<path class="theme-icon-slide-caption-close-1" ' +
+          '<path class="unl-icon-slide-caption-close-1" ' +
           'd="M1,13h22c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,12.6,0.4,13,1,13z"/>' +
-          '<path class="theme-icon-slide-caption-close-2" ' +
+          '<path class="unl-icon-slide-caption-close-2" ' +
           'd="M1,13h22c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,12.6,0.4,13,1,13z"/>' +
-          '<path class="theme-icon-slide-caption-open" ' +
+          '<path class="unl-icon-slide-caption-open" ' +
           'd="M1,18h18c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,17.6,0.4,18,1,18z"/>' +
-          '<path class="theme-icon-slide-caption-open" ' +
+          '<path class="unl-icon-slide-caption-open" ' +
           'd="M1,23h15c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1C0,22.6,0.4,23,1,23z"/>' +
           '</svg>';
       }
@@ -108,8 +108,8 @@ require(['dcf-utility', 'dcf-slideshow', 'css!js-css/slideshows'], (Utility, DCF
   let buttons = document.querySelectorAll('.dcf-btn-slide-caption');
   Array.prototype.forEach.call(buttons, (button) => {
     let caption = button.previousElementSibling;
-    let close1 = button.querySelector('.theme-icon-slide-caption-close-1');
-    let close2 = button.querySelector('.theme-icon-slide-caption-close-2');
+    let close1 = button.querySelector('.unl-icon-slide-caption-close-1');
+    let close2 = button.querySelector('.unl-icon-slide-caption-close-2');
 
     caption.addEventListener('openCaption', () => {
       close1.animate(keyframesClose1, options);
