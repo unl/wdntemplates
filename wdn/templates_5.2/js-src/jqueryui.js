7dc62bae04086fc8dbce6815bc95799148e87ccf
Ryan Dee <ryan@skoolbus39.com>
2020-07-10 13:22:44 -0500

    Add 5.2 folder; restore Visit, Apply, Give

---
+++ wdn/templates_5.2/js-src/jqueryui.js
@@ -0,0 +1,11 @@
+define([
+	'jquery', 
+	'plugins/ui/jquery-ui',
+	'css!plugins/ui/css/jquery-ui-wdn'
+], function($) {
+	return {
+		initialize: function(callback) {
+			$(callback);
+		}
+	};
+});
