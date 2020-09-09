8e7b2d600a9afe6c7b105335c78cbbd95a7f82e0
Ryan Dee <ryan@skoolbus39.com>
2020-09-09 14:35:01 -0500

    Add scroll animations to WDN theme

---
+++ wdn/templates_5.2/js-src/scroll-animations.babel.js
@@ -0,0 +1,10 @@
+require (['dcf-scrollAnimation'], function() {
+  const itemList = document.querySelectorAll('.dcf-animate-on-scroll');
+  const observerConfig = {
+    rootMargin: '0px',
+    threshold: [0.5, 1]
+  };
+  const animationClassNames = ['dcf-animated'];
+  const unlScrollAnimation = new DCFScrollAnimation(itemList, observerConfig, animationClassNames);
+  unlScrollAnimation.initialize();
+});
