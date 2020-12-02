1766ada3bae0db2a8d771f1e592a557e57a6646b
Jeff Sturek <jsturek8@unl.edu>
2020-12-02 16:18:16 -0600

    Start of video modal support

--- wdn/templates_5.2/js-src/modals.babel.js
+++ wdn/templates_5.2/js-src/modals.babel.js
@@ -2,4 +2,43 @@ require (['dcf-utility', 'dcf-modal', 'plugins/body-scroll-lock'], (Utility, Mod
   const modals = document.querySelectorAll('.dcf-modal');
   const unlModal = new DCFModal(modals, bodyScrollLock);
   unlModal.initialize();
+
+  // Define custom open and close event
+  modals.forEach((modal) => {
+    setVideoEventListeners(modal);
+  });
+
+  function setVideoEventListeners(modal) {
+    // Get any videos in modal
+    const videos = modal.getElementsByTagName('video');
+
+    if (videos && videos.length > 0) {
+      const playBtns = modal.getElementsByClassName('mejs-overlay-play');
+
+      const autoplay = modal.getAttribute('data-autoplay');
+      if (autoplay && autoplay === 'true') {
+        document.addEventListener('ModalOpenEvent_' + modal.id, function (e) {
+          // auto play first video
+          videos[0].play();
+        }, false);
+      }
+
+      document.addEventListener('ModalCloseEvent_' + modal.id, function(e) {
+        // pause and reset all modal videos
+        Array.prototype.forEach.call(videos,(video) => {
+          video.pause();
+          video.load();
+        });
+
+        // Show any hidden overlay play buttons
+        if (playBtns && playBtns.length > 0) {
+          Array.prototype.forEach.call(playBtns, (playBtn) => {
+            if (playBtn.style.display === 'none') {
+              playBtn.style.display = 'block';
+            }
+          });
+        }
+      }, false);
+    }
+  }
 });
