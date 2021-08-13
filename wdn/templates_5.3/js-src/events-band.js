a4a05b58a65ce833940074df51cc0fade57b59e7
Jeff Sturek <jsturek@gmail.com>
2021-08-13 13:27:20 -0500

    Typo Fix

--- wdn/templates_5.3/js-src/events-band.js
+++ wdn/templates_5.3/js-src/events-band.js
@@ -2,7 +2,7 @@ define([
   'events',
   'jquery'
 ], function(events, $) {
-  console.warn('Waring: The WDN events-band plugin is deprecated. Use WDN events plugin with band layout instead.');
+  console.warn('Warning: The WDN events-band plugin is deprecated. Use WDN events plugin with band layout instead.');
 
   return {
     initialize : function(config) {
