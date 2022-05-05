c9220036fbd562aa89d66aea2ba6a1a301ec8100
Ryan Dee <ryan@skoolbus39.com>
2022-05-05 09:02:35 -0500

    Use GSAP/ScrollTrigger for scroll animations

--- wdn/templates_5.3/js-src/scroll-animations.babel.js
+++ wdn/templates_5.3/js-src/scroll-animations.babel.js
@@ -1,10 +1,17 @@
-require (['dcf-scrollAnimation'], (scrollAnimationModule) => {
-  const itemList = document.querySelectorAll('.dcf-animate-on-scroll');
-  const observerConfig = {
-    rootMargin: '0px',
-    threshold: [0.5, 1]
-  };
-  const animationClassNames = ['dcf-animated'];
-  const unlScrollAnimation = new scrollAnimationModule.DCFScrollAnimation(itemList, observerConfig, animationClassNames);
-  unlScrollAnimation.initialize();
+require(['plugins/gsap/gsap', 'plugins/gsap/ScrollTrigger'], (gsapModule, ScrollTriggerModule) => {
+
+  const gsap = gsapModule.gsap;
+  gsap.registerPlugin(ScrollTriggerModule.ScrollTrigger);
+
+  ScrollTriggerModule.ScrollTrigger.config( { limitCallbacks: true } );
+
+  ScrollTriggerModule.ScrollTrigger.batch('.unl-fade-in', {
+    interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
+    batchMax: 6,   // maximum batch size (targets)
+    onEnter: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
+    onLeave: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true}),
+    onEnterBack: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
+    onLeaveBack: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true})
+  });
+
 });
