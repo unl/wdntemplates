ecbeddc20577cf8cd2ead728c4b07893205386cf
Ryan Dee <ryan@skoolbus39.com>
2022-05-10 13:01:37 -0500

    Update scroll animations JS

--- wdn/templates_5.3/js-src/scroll-animations.babel.js
+++ wdn/templates_5.3/js-src/scroll-animations.babel.js
@@ -1,13 +1,17 @@
 require(['plugins/gsap/gsap', 'plugins/gsap/ScrollTrigger'], (gsapModule, ScrollTriggerModule) => {
 
   const gsap = gsapModule.gsap;
-  gsap.registerPlugin(ScrollTriggerModule.ScrollTrigger);
+  const ScrollTrigger = ScrollTriggerModule.ScrollTrigger;
 
-  gsap.set('.unl-fade-in', {autoAlpha: 0});
+  // Register ScrollTrigger plugin
+  gsap.registerPlugin(ScrollTrigger);
 
-  ScrollTriggerModule.ScrollTrigger.config( { limitCallbacks: true } );
+  ScrollTrigger.config( { limitCallbacks: true } );
 
-  ScrollTriggerModule.ScrollTrigger.batch('.unl-fade-in', {
+  // Set opacity to 0 on page load for elements that will fade in
+  gsap.set('.unl-scroll-fx-fade-in', {autoAlpha: 0});
+
+  ScrollTrigger.batch('.unl-scroll-fx-fade-in', {
     interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
     batchMax: 6,   // maximum batch size (targets)
     onEnter: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
