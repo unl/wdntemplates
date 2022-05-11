97016f16422c62583cfda30f6f13c59c0608dd86
Ryan Dee <ryan@skoolbus39.com>
2022-05-11 12:48:01 -0500

    Add scroll animations

--- wdn/templates_5.3/js-src/scroll-animations.babel.js
+++ wdn/templates_5.3/js-src/scroll-animations.babel.js
@@ -8,9 +8,13 @@ require(['plugins/gsap/gsap', 'plugins/gsap/ScrollTrigger'], (gsapModule, Scroll
 
   ScrollTrigger.config( { limitCallbacks: true } );
 
-  // Set opacity to 0 on page load for elements that will fade in
+  // Set defaults on page load
   gsap.set('.unl-scroll-fx-fade-in', {autoAlpha: 0});
+  gsap.set('.unl-scroll-fx-move-left', {x: 100});
+  gsap.set('.unl-scroll-fx-move-right', {x: -100});
+  gsap.set('.unl-scroll-fx-move-up', {y: 100});
 
+  // Fade in
   ScrollTrigger.batch('.unl-scroll-fx-fade-in', {
     interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
     batchMax: 6,   // maximum batch size (targets)
@@ -18,6 +22,36 @@ require(['plugins/gsap/gsap', 'plugins/gsap/ScrollTrigger'], (gsapModule, Scroll
     onLeave: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true}),
     onEnterBack: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
     onLeaveBack: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true})
+  });
+
+  // Move left
+  ScrollTrigger.batch('.unl-scroll-fx-move-left', {
+    interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
+    batchMax: 6,   // maximum batch size (targets)
+    onEnter: batch => gsap.to(batch, {x: 0, stagger: 0.1, overwrite: true}),
+    onLeave: batch => gsap.set(batch, {x: 100, overwrite: true}),
+    onEnterBack: batch => gsap.to(batch, {x: 0, stagger: 0.1, overwrite: true}),
+    onLeaveBack: batch => gsap.set(batch, {x: 100, overwrite: true})
+  });
+
+  // Move right
+  ScrollTrigger.batch('.unl-scroll-fx-move-right', {
+    interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
+    batchMax: 6,   // maximum batch size (targets)
+    onEnter: batch => gsap.to(batch, {x: 0, stagger: 0.1, overwrite: true}),
+    onLeave: batch => gsap.set(batch, {x: -100, overwrite: true}),
+    onEnterBack: batch => gsap.to(batch, {x: 0, stagger: 0.1, overwrite: true}),
+    onLeaveBack: batch => gsap.set(batch, {x: -100, overwrite: true})
+  });
+
+  // Move up
+  ScrollTrigger.batch('.unl-scroll-fx-move-up', {
+    interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
+    batchMax: 6,   // maximum batch size (targets)
+    onEnter: batch => gsap.to(batch, {y: 0, stagger: 0.1, overwrite: true}),
+    onLeave: batch => gsap.set(batch, {y: 100, overwrite: true}),
+    onEnterBack: batch => gsap.to(batch, {y: 0, stagger: 0.1, overwrite: true}),
+    onLeaveBack: batch => gsap.set(batch, {y: 100, overwrite: true})
   });
 
 });
