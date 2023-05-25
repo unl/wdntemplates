564c573d9ac7c2229f943d20c493b6e0f38238b6
Ryan Dee <ryan@skoolbus39.com>
2023-05-25 15:49:53 -0500

    Add icons to ICS and RSS buttons

--- wdn/templates_5.3/js-src/events.babel.js
+++ wdn/templates_5.3/js-src/events.babel.js
@@ -74,8 +74,8 @@ define([
         moreText = `More ${config.title.trim()} Events`;
       }
       let seeAll = `<div><a class="dcf-btn dcf-btn-secondary" href="${config.url}${typePath}">${moreText}</a></div>`;
-      let ics = `<a class="dcf-btn dcf-btn-secondary" href="${config.url}${typePath}?format=ics">ICS</a>`;
-      let rss = `<a class="dcf-btn dcf-btn-secondary" href="${config.url}${typePath}?format=rss">RSS</a>`;
+      let ics = `<a class="dcf-btn dcf-btn-secondary dcf-btn-icon dcf-ai-baseline" href="${config.url}${typePath}?format=ics"><abbr class="dcf-txt-md" title="i Calendar format">ICS</abbr> <svg class="dcf-h-3 dcf-w-3 dcf-fill-current" focusable="false" height="16" width="16" viewBox="0 0 24 24"><path d="M23.5 2H20V.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V2H8V.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V2H.5a.5.5 0 0 0-.5.5V7h24V2.5a.5.5 0 0 0-.5-.5zM7 4H5V1h2v3zm12 0h-2V1h2v3zM0 23.5a.5.5 0 0 0 .5.5h23a.5.5 0 0 0 .5-.5V8H0v15.5zM7 15h4v-4a1 1 0 0 1 2 0v4h4a1 1 0 0 1 0 2h-4v4a1 1 0 0 1-2 0v-4H7a1 1 0 0 1 0-2z"/><path fill="none" d="M0 0h24v24H0z"/></svg></a>`;
+      let rss = `<a class="dcf-btn dcf-btn-secondary dcf-btn-icon dcf-ai-baseline" href="${config.url}${typePath}?format=rss"><abbr class="dcf-txt-md" title="Really Simple Syndication">RSS</abbr> <svg class="dcf-h-3 dcf-w-3 dcf-fill-current" focusable="false" height="16" width="16" viewBox="0 0 24 24"><path d="M.012 8.5v2c7.289 0 13 5.931 13 13.5h2c0-8.691-6.59-15.5-15-15.5z"/><path d="M.012 0v2c12.336 0 22 9.664 22 22h2c0-13.458-10.543-24-24-24z"/><circle cx="3.012" cy="21" r="3"/><path fill="none" d="M0 0h24v24H0z"/></svg></a>`;
       let feeds = `<div class="dcf-btn-group" role="group">${ics}${rss}</div>`;
       let more = `<div class="dcf-d-flex dcf-flex-row dcf-flex-wrap dcf-jc-between dcf-col-gap-5 dcf-row-gap-4">${feeds}${seeAll}</div>`;
       $container.append(more);
