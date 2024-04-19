b8d262bddb241331db9b5a759d07f3dee8562ae2
Thomas Neumann <thomasneumann21800@gmail.com>
2024-04-19 09:25:45 -0500

    Updated groups label styles

--- wdn/templates_5.3/js-src/search-selects.babel.js
+++ wdn/templates_5.3/js-src/search-selects.babel.js
@@ -3,7 +3,7 @@ require(['dcf-search-select'], (DCFSearchSelect) => {
     const selects = document.querySelectorAll('.dcf-search-select');
     const selectTheme = new DCFSearchSelect.DCFSearchSelectTheme();
 
-    selectTheme.setThemeVariable('availableItemsClassList', ['dcf-m-0', 'dcf-b-1', 'dcf-b-solid', 'unl-b-lightest-gray']);
+    selectTheme.setThemeVariable('availableItemsGroupLabelClassList', ['dcf-m-0', 'dcf-p-bold', 'unl-bg-scarlet', 'unl-cream']);
 
 
     // Initialize the selects with the theme
