e8db365fce3ab95edae6ccd07648fe42274d38ad
Thomas Neumann <thomasneumann21800@gmail.com>
2025-05-16 09:17:28 -0500

    Added comments

--- wdn/templates_6.0/js-src/plugin-auto-loader.js
+++ wdn/templates_6.0/js-src/plugin-auto-loader.js
@@ -12,12 +12,14 @@ if (
 
 document.dispatchEvent(new Event('autoLoaderPreLoad'));
 
+// Setting default values for the autoloader config
 const optInSelector = window.UNL.autoLoader.config.optInSelector || null;
 const optOutSelector = window.UNL.autoLoader.config.optOutSelector || null;
 const configPluginList = window.UNL.autoLoader.config.plugins || {};
 const enabled = window.UNL.autoLoader.config.enabled || true;
 const watch = window.UNL.autoLoader.config.watch || true;
 
+// This is the list of plugins we will check with when elements are added to the page
 const watchList = [];
 
 if (enabled) {
@@ -38,6 +40,8 @@ if (enabled) {
             continue;
         }
 
+        // If the single plugins target is not on the page then we will add it to the watch list
+        //   if it is on the page when we will initialize the plugin
         if (pluginModule.getPluginType() === 'single') {
             if (typeof pluginModule.isOnPage !== 'function') {
                 throw new Error(`Invalid isOnPage function in plugin: ${singlePluginName}`);
@@ -62,16 +66,20 @@ if (enabled) {
                 throw new Error(`Invalid loadElement function in plugin: ${singlePluginName}`);
             }
             let matchingElements = Array.from(document.querySelectorAll(pluginModule.getQuerySelector()));
+            // Filter out opt out
             if (optOutSelector !== null) {
                 matchingElements = matchingElements.filter((matchingElement) => {
                     return !(matchingElement.matches(optOutSelector));
                 });
             }
+            // Filter out non-opt in
             if (optInSelector !== null) {
                 matchingElements = matchingElements.filter((matchingElement) => {
                     return matchingElement.matches(optInSelector);
                 });
             }
+
+            // load the rest of the elements and add the plugin to the watch list
             const elements = await pluginModule.loadElements(matchingElements);
             pluginData.elements = pluginData.elements.concat(elements);
 
@@ -88,10 +96,10 @@ if (watch) {
             mutationRecord.addedNodes.forEach(async(nodeAdded) => {
                 if (nodeAdded instanceof Element) {
 
+                    // Double check the added element is not opt out and is opt in
                     if (optOutSelector !== null && nodeAdded.matches(optOutSelector)) {
                         return;
                     }
-
                     if (optInSelector !== null && !nodeAdded.matches(optInSelector)) {
                         return;
                     }
@@ -114,12 +122,6 @@ if (watch) {
                             }
 
                         } else if (pluginModule.getPluginType() === 'multi') {
-                            if (typeof pluginModule.getQuerySelector !== 'function') {
-                                throw new Error(`Invalid getQuerySelector function in plugin: ${singlePluginName}`);
-                            }
-                            if (typeof pluginModule.loadElement !== 'function') {
-                                throw new Error(`Invalid loadElement function in plugin: ${singlePluginName}`);
-                            }
                             if (nodeAdded.matches(pluginModule.getQuerySelector())) {
                                 const element = await pluginModule.loadElement(nodeAdded);
                                 pluginData.elements = pluginData.elements.concat(element);
@@ -141,12 +143,14 @@ if (watch) {
 
 window.UNL.autoLoader.loaded = true;
 
+// Clear out the queue and delete it's key-value pair since it is no longer needed
 if ('loadCallbackQueue' in window.UNL.autoLoader && Array.isArray(window.UNL.autoLoader.loadCallbackQueue)) {
     window.UNL.autoLoader.loadCallbackQueue.forEach((singleCallback) => {
         singleCallback();
     });
     delete window.UNL.autoLoader.loadCallbackQueue;
 }
+// Redefine onload to just call the callback since we have loaded
 window.UNL.autoLoader.onLoad = (callbackFunc) => {
     callbackFunc();
 };
