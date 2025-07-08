b1a3112c60cc2b99fd8ea43c6cc3bb6b98821c15
Ryan Dee <ryan@skoolbus39.com>
2025-07-08 12:10:04 -0500

    Update framework template files
    - Rename “fixed” to “cloud”
    - Update include file paths

---
+++ Templates/php.cloud.dwt.php
@@ -0,0 +1,131 @@
+<?php
+function wdnInclude($path)
+{
+$documentRoot = 'https://unlcms.unl.edu';
+return readfile($documentRoot . $path);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/head-1.html"); ?>
+        <!--
+          Membership and regular participation in the University of Nebraska–Lincoln (UNL) Web Developer Network (WDN) is required to use the UNLedu Web Framework. Visit the WDN site at https://wdn.unl.edu/. Register for our mailing list and add your site or server to UNL Web Audit.
+          All framework code is the property of the UNL Web Developer Network. The code seen in a source code view is not, and may not be used as, a template. You may not use this code, a reverse-engineered version of this code, or its associated visual presentation in whole or in part to create a derivative work.
+          This message may not be removed from any pages based on the UNLedu Web Framework.
+
+          $Id$
+        -->
+        <!-- TemplateBeginEditable name="doctitle" -->
+        <title>Use a descriptive page title | Optional Site Title (use for context) | University of Nebraska&ndash;Lincoln</title>
+        <!-- TemplateEndEditable -->
+        <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/head-2.html"); ?>
+        <!-- TemplateBeginEditable name="head" -->
+        <!-- Place optional header elements here -->
+        <!-- TemplateEndEditable -->
+        <!-- TemplateParam name="class" type="text" value="" -->
+    </head>
+    <body class="@@(_document['class'])@@ unl" data-version="$HTML_VERSION$">
+        <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/skip-nav.html"); ?>
+        <header class="dcf-header" id="dcf-header" role="banner">
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/header-global-1.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/idm.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/search.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/header-global-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/logo-lockup-1.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/site-affiliation-1.html"); ?>
+            <!-- TemplateBeginEditable name="affiliation" -->
+            <a href="#">My site affiliation</a>
+            <!-- TemplateEndEditable -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/site-affiliation-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/site-title-1.html"); ?>
+            <!-- TemplateBeginEditable name="titlegraphic" -->
+            <a class="dcf-txt-h6" href="#">Title of my site</a>
+            <!-- TemplateEndEditable -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/site-title-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/logo-lockup-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-container-1.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-toggle-group.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-menu-1.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-local-1.html"); ?>
+            <!-- TemplateBeginEditable name="navlinks" -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/local/nav-local.html"); ?>
+            <!-- TemplateEndEditable -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-local-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-menu-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-dialog-1.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-local-copy-dialog.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/cta-1.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/visit-1.html"); ?>
+            <!-- TemplateBeginEditable name="navvisit" -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/local/visit-local.html"); ?>
+            <!-- TemplateEndEditable -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/visit-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/apply-1.html"); ?>
+            <!-- TemplateBeginEditable name="navapply" -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/local/apply-local.html"); ?>
+            <!-- TemplateEndEditable -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/apply-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/give-1.html"); ?>
+            <!-- TemplateBeginEditable name="navgive" -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/local/give-local.html"); ?>
+            <!-- TemplateEndEditable -->
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/give-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/cta-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-dialog-2.html"); ?>
+            <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/nav-container-2.html"); ?>
+        </header>
+        <main class="dcf-main" id="dcf-main" role="main" tabindex="-1">
+            <!-- TemplateBeginEditable name="highlighted" -->
+            <!-- TemplateEndEditable -->
+            <!-- TemplateBeginEditable name="hero" -->
+            <div class="dcf-hero dcf-hero-default">
+                <!-- TemplateEndEditable -->
+                <div class="dcf-hero-group-1">
+                    <div class="dcf-breadcrumbs-wrapper">
+                        <nav class="dcf-breadcrumbs" id="dcf-breadcrumbs" role="navigation" aria-label="breadcrumbs">
+                            <!-- TemplateBeginEditable name="breadcrumbs" -->
+                            <ol>
+                                <li><a href="https://www.unl.edu/">Nebraska</a></li>
+                                <li><a href="/">Site Title</a></li>
+                                <li><span aria-current="page">Home</span></li>
+                            </ol>
+                            <!-- TemplateEndEditable -->
+                        </nav>
+                    </div>
+                    <header class="dcf-page-title" id="dcf-page-title">
+                        <!-- TemplateBeginEditable name="pagetitle" -->
+                        <h1>Please Title Your Page Here</h1>
+                        <!-- TemplateEndEditable -->
+                    </header>
+                    <!-- TemplateBeginEditable name="herogroup1" -->
+                    <!-- TemplateEndEditable -->
+                </div>
+                <!-- TemplateBeginEditable name="herogroup2" -->
+                <div class="dcf-hero-group-2">
+                </div>
+                <!-- TemplateEndEditable -->
+            </div>
+            <div class="dcf-main-content dcf-wrapper">
+                <!-- TemplateBeginEditable name="maincontentarea" -->
+                <p>Impress your audience with awesome content!</p>
+                <!-- TemplateEndEditable -->
+            </div>
+        </main>
+        <footer class="dcf-footer" id="dcf-footer" role="contentinfo">
+            <!-- TemplateBeginEditable name="optionalfooter" -->
+            <!-- TemplateEndEditable -->
+                <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/footer-global-1.html"); ?>
+            <!-- TemplateBeginEditable name="contactinfo" -->
+                <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/local/footer-local.html"); ?>
+            <!-- TemplateEndEditable -->
+                <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/footer-global-2.html"); ?>
+                <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/footer-global-3-std.html"); ?>
+                <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/noscript.html"); ?>
+        </footer>
+        <?php wdnInclude("https://unlcms.unl.edu/wdn/templates_6.0/includes/global/js-body.html"); ?>
+        <!-- TemplateBeginEditable name="jsbody" -->
+        <!-- Put your custom JavaScript here -->
+        <!-- TemplateEndEditable -->
+    </body>
+</html>
