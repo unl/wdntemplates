a54da824b5cfff87b61820100e731a03af07f599
Thomas Neumann <thomasneumann21800@gmail.com>
2025-07-08 10:06:53 -0500

    Implemented idm rendering logic; modified some include files and styles

--- wdn/templates_6.0/js-src/components/unl-idm.js
+++ wdn/templates_6.0/js-src/components/unl-idm.js
@@ -14,6 +14,8 @@ export default class UNLIdm {
 
     userLookupUrl = 'https://directory.unl.edu/people/';
 
+    directoryURL = 'https://directory.unl.edu/';
+
     emailToUidURL = 'https://directory.unl.edu/api/v1/emailToUID?email=';
 
     avatarUrl = 'https://directory.unl.edu/avatar/';
@@ -327,6 +329,7 @@ export default class UNLIdm {
      */
     async #loadServerUser() {
         if (this.serverSideUser === null) {
+            this.#render();
             return;
         }
 
@@ -418,6 +421,23 @@ export default class UNLIdm {
     }
 
     /**
+     * Gets the best full name for the clientSideUser based on available data
+     * 
+     * @returns { String|null } ClientSideUser's full name or null if failed
+     */
+    #getClientUserFullName() {
+        if (this.clientSideUser === null) {
+            return null;
+        }
+
+        if ('displayName' in this.clientSideUser && this.clientSideUser.displayName !== null) {
+            return this.clientSideUser.displayName[0];
+        }
+
+        return null;
+    }
+
+    /**
      * Gets the best display name for the serverSideUser based on available data
      * 
      * @returns { String|null } ServerSideUser's display name or null if failed
@@ -439,6 +459,27 @@ export default class UNLIdm {
             return this.serverSideUser.uid;
         } else if ('email' in this.serverSideUser && this.serverSideUser.email !== null) {
             return this.serverSideUser.email;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the serverSideUser's full name based on available data
+     * 
+     * @returns { String|null } User's full name or null if failed
+     */
+    #getServerUserFullName() {
+        if (this.serverSideUser === null) {
+            return null;
+        }
+
+        if ('data' in this.serverSideUser && this.serverSideUser.data !== null) {
+            if ('displayName' in this.clientSideUser && this.serverSideUser.data.displayName !== null) {
+                return this.serverSideUser.data.displayName[0];
+            }
+        } else if ('uid' in this.serverSideUser && this.serverSideUser.uid !== null) {
+            return this.serverSideUser.uid;
         }
 
         return null;
@@ -467,6 +508,10 @@ export default class UNLIdm {
         return null;
     }
 
+    /**
+     * Gets the current user's primary affiliation
+     * @returns { String } User's primary affiliation or 'None';
+     */
     getPrimaryAffiliation() {
         if (this.clientSideUser !== null) {
             return this.clientSideUser?.eduPersonPrimaryAffiliation?.[0] || 'None';
@@ -477,15 +522,36 @@ export default class UNLIdm {
         return 'None';
     }
 
+    /**
+     * Gets the current user's nick name or first name
+     * @returns { String } User's nick name or first name or empty string
+     */
     getDisplayName() {
         if (this.clientSideUser !== null) {
-            return this.#getClientUserDisplayName();
+            return this.#getClientUserDisplayName() || '';
         } else if (this.serverSideUser !== null) {
-            return this.#getServerUserDisplayName();
+            return this.#getServerUserDisplayName()  || '';
         }
         return '';
     }
 
+    /**
+     * Gets the current user's full name
+     * @returns { String } User's Full Name or empty string
+     */
+    getFullName() {
+        if (this.clientSideUser !== null) {
+            return this.#getClientUserFullName()  || '';
+        } else if (this.serverSideUser !== null) {
+            return this.#getServerUserFullName()  || '';
+        }
+        return '';
+    }
+
+    /**
+     * Gets the current user's email address
+     * @returns { String } User's email address or empty string
+     */
     getEmailAddress() {
         if (this.clientSideUser !== null) {
             return this.clientSideUser?.mail?.[0] || 'None';
@@ -493,6 +559,19 @@ export default class UNLIdm {
             return this.serverSideUser?.data?.mail?.[0] || 'None';
         }
         return '';
+    }
+
+    /**
+     * Gets the current user's directory profile url
+     * @returns { String } Directory profile URL or directory home page url
+     */
+    getProfileUrl() {
+        if (this.clientSideUser !== null) {
+            return `${this.userLookupUrl}${this.clientSideUser.uid}`;
+        } else if (this.serverSideUser !== null) {
+            return `${this.userLookupUrl}${this.serverSideUser.uid}`;
+        }
+        return this.directoryURL;
     }
 
     /**
@@ -507,8 +586,9 @@ export default class UNLIdm {
             this.renderLoggedOutState();
         } else if (this.clientSideUser !== null && this.serverSideUser === null) {
             // Display Quasi-Logged In State
+            const userDisplayName = this.getDisplayName();
             const userAvatarUrl = `${this.avatarUrl}${this.clientSideUser.uid}`;
-            this.renderQuasiLoggedInState(userAvatarUrl);
+            this.renderQuasiLoggedInState(userDisplayName, userAvatarUrl);
         } else if (this.clientSideUser === null && this.serverSideUser !== null) {
             // Display Logged In State
             // Using server side user data
@@ -529,7 +609,43 @@ export default class UNLIdm {
      * @return { Void }
      */
     renderLoggedOutState() {
-        console.log('logged Out', this.logInUrl);
+        const labels = Array.from(document.querySelectorAll('.dcf-idm-label'));
+        labels.forEach((singleLabel) => {
+            singleLabel.innerHTML = 'Log In';
+        });
+
+        const bigLabels = Array.from(document.querySelectorAll('.unl-idm-firstname-lastname'));
+        bigLabels.forEach((singleLabel) => {
+            singleLabel.innerHTML = `<a class="unl-cream" href="${this.logInUrl}">Log in</a>`;
+        });
+
+        const imgs = Array.from(document.querySelectorAll('.dcf-idm-img'));
+        imgs.forEach((singleImg) => {
+            singleImg.classList.remove('unl-idm-status-quasi');
+            singleImg.innerHTML = `<svg class="dcf-h-6 dcf-w-6 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 48 48">
+    <path d="M47.9 24C47.9 10.8 37.2.1 24 .1S.1 10.8.1 24c0 6.3 2.5 12.3 6.9 16.8 4.5 4.6 10.6 7.1 17 7.1s12.5-2.5 17-7.1c4.5-4.5 6.9-10.5 6.9-16.8zm-45 0C2.9 12.4 12.4 2.9 24 2.9c11.6 0 21.1 9.5 21.1 21.1 0 5.2-1.9 10.1-5.3 14-2.1-1.2-5-2.2-8.2-3.4-.7-.3-1.5-.5-2.2-.8v-3.1c1.1-.7 2.6-2.4 2.9-5.7.8-.6 1.2-1.6 1.2-2.9 0-1.1-.4-2.1-1-2.7.5-1.6 1.3-4.2.7-6.5-.7-3-4.6-4-7.7-4-2.7 0-5.9.8-7.2 2.8-1.2 0-2 .5-2.4 1-1.6 1.7-.8 4.8-.3 6.6-.6.6-1 1.6-1 2.7 0 1.3.5 2.3 1.2 2.9.3 3.4 1.8 5 2.9 5.7v3.1c-.7.2-1.4.5-2 .7-3.1 1.1-6.2 2.2-8.4 3.5-3.5-3.7-5.4-8.7-5.4-13.9zm7.5 16.1c2-1 4.6-2 7.2-2.9 1-.4 2-.7 3-1.1.5-.2.9-.7.9-1.3v-4.9c0-.6-.4-1.1-.9-1.3-.1 0-2-.8-2-4.5 0-.7-.5-1.2-1.1-1.4-.1-.3-.1-.9 0-1.2.6-.1 1.1-.7 1.1-1.4 0-.3-.1-.6-.2-1.2-.9-3.2-.7-4-.4-4.3.1-.1.4-.1 1 0 .7.1 1.5-.3 1.6-1 .3-1 2.5-1.9 5-1.9s4.7.8 5 1.9c.4 1.7-.4 4.1-.7 5.2-.2.6-.3.9-.3 1.3 0 .7.5 1.2 1.1 1.4.1.3.1.9 0 1.2-.6.1-1.1.7-1.1 1.4 0 3.7-1.9 4.5-2 4.5-.6.2-1 .7-1 1.3v4.9c0 .6.4 1.1.9 1.3 1.1.4 2.1.8 3.2 1.2 2.7 1 5.2 1.9 7.1 2.8-3.8 3.3-8.6 5-13.7 5-5.2 0-9.9-1.8-13.7-5z"></path>
+</svg>`;
+        });
+
+        const bigImgs = Array.from(document.querySelectorAll('.unl-idm-avatar'));
+        bigImgs.forEach((singleImg) => {
+            singleImg.classList.remove('unl-idm-status-quasi');
+            singleImg.innerHTML = `<svg class="dcf-h-100% dcf-w-100% dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 24 24">
+    <path d="M12 0C5.383 0 0 5.383 0 12c0 3.18 1.232 6.177 3.469 8.438l.001.001A11.92 11.92 0 0 0 12 24c3.234 0 6.268-1.27 8.542-3.573A11.93 11.93 0 0 0 24 12c0-6.617-5.383-12-12-12zm8.095 19.428c-1.055-.626-5.165-2.116-5.595-2.275v-1.848c.502-.309 1.384-1.107 1.49-2.935.386-.226.63-.727.63-1.37 0-.578-.197-1.043-.52-1.294.242-.757.681-2.145.385-3.327C16.138 4.992 14.256 4.5 12.75 4.5c-1.342 0-2.982.391-3.569 1.456-.704-.034-1.096.273-1.29.531-.635.838-.216 2.368.02 3.21-.329.249-.531.718-.531 1.303 0 .643.244 1.144.63 1.37.106 1.828.989 2.626 1.49 2.935v1.848c-.385.144-4.464 1.625-5.583 2.288A10.92 10.92 0 0 1 1 12C1 5.935 5.935 1 12 1s11 4.935 11 11a10.92 10.92 0 0 1-2.905 7.428z"></path>
+    <path fill="none" d="M0 0h24v24H0z"></path>
+</svg>`;
+        });
+
+        const viewProfileLinks = Array.from(document.querySelectorAll('.unl-idm-view-profile'));
+        viewProfileLinks.forEach((singleLink) => {
+            singleLink.classList.add('dcf-d-none');
+        });
+
+        const logOutLinks = Array.from(document.querySelectorAll('.unl-idm-logout'));
+        logOutLinks.forEach((singleLink) => {
+            singleLink.classList.add('dcf-d-none');
+        });
+
         window.dispatchEvent(new Event('idmStateSet'));
     }
 
@@ -540,8 +656,41 @@ export default class UNLIdm {
      * @param { String } userAvatarUrl
      * @return { Void }
      */
-    renderQuasiLoggedInState(userAvatarUrl) {
-        console.log('quasi', userAvatarUrl, this.logInUrl);
+    renderQuasiLoggedInState(userDisplayName, userAvatarUrl) {
+        const labels = Array.from(document.querySelectorAll('.dcf-idm-label'));
+        labels.forEach((singleLabel) => {
+            singleLabel.innerHTML = 'Log in to this site';
+        });
+
+        const bigLabels = Array.from(document.querySelectorAll('.unl-idm-firstname-lastname'));
+        bigLabels.forEach((singleLabel) => {
+            singleLabel.innerHTML = `<a class="unl-cream" href="${this.logInUrl}">Log in to this site as ${this.getFullName()}</a>`;
+        });
+
+        const imgs = Array.from(document.querySelectorAll('.dcf-idm-img'));
+        imgs.forEach((singleImg) => {
+            singleImg.classList.add('unl-idm-status-quasi');
+            singleImg.innerHTML = `<img class="dcf-h-100% dcf-w-100%" src="${userAvatarUrl}" alt>`;
+        });
+
+        const bigImgs = Array.from(document.querySelectorAll('.unl-idm-avatar'));
+        bigImgs.forEach((singleImg) => {
+            singleImg.classList.add('unl-idm-status-quasi');
+            singleImg.innerHTML = `<img class="dcf-h-100% dcf-w-100%" src="${userAvatarUrl}" alt="">`;
+        });
+
+        const viewProfileLinks = Array.from(document.querySelectorAll('.unl-idm-view-profile a'));
+        viewProfileLinks.forEach((singleLink) => {
+            singleLink.classList.remove('dcf-d-none');
+            singleLink.setAttribute('href', this.getProfileUrl());
+        });
+
+        const logOutLinks = Array.from(document.querySelectorAll('.unl-idm-logout a'));
+        logOutLinks.forEach((singleLink) => {
+            singleLink.classList.remove('dcf-d-none');
+            singleLink.setAttribute('href', this.logOutUrl);
+        });
+
         window.dispatchEvent(new Event('idmStateSet'));
     }
 
@@ -553,7 +702,40 @@ export default class UNLIdm {
      * @return { Void }
      */
     renderLoggedInState(userDisplayName, userAvatarUrl) {
-        console.log('logged in', userDisplayName, userAvatarUrl, this.logOutUrl);
+        const labels = Array.from(document.querySelectorAll('.dcf-idm-label'));
+        labels.forEach((singleLabel) => {
+            singleLabel.innerHTML = userDisplayName;
+        });
+
+        const bigLabels = Array.from(document.querySelectorAll('.unl-idm-firstname-lastname'));
+        bigLabels.forEach((singleLabel) => {
+            singleLabel.innerHTML = this.getFullName();
+        });
+
+        const imgs = Array.from(document.querySelectorAll('.dcf-idm-img'));
+        imgs.forEach((singleImg) => {
+            singleImg.classList.remove('unl-idm-status-quasi');
+            singleImg.innerHTML = `<img class="dcf-h-100% dcf-w-100%" src="${userAvatarUrl}" alt>`;
+        });
+
+        const bigImgs = Array.from(document.querySelectorAll('.unl-idm-avatar'));
+        bigImgs.forEach((singleImg) => {
+            singleImg.classList.remove('unl-idm-status-quasi');
+            singleImg.innerHTML = `<img class="dcf-h-100% dcf-w-100%" src="${userAvatarUrl}" alt="">`;
+        });
+
+        const viewProfileLinks = Array.from(document.querySelectorAll('.unl-idm-view-profile a'));
+        viewProfileLinks.forEach((singleLink) => {
+            singleLink.classList.remove('dcf-d-none');
+            singleLink.setAttribute('href', this.getProfileUrl());
+        });
+
+        const logOutLinks = Array.from(document.querySelectorAll('.unl-idm-logout a'));
+        logOutLinks.forEach((singleLink) => {
+            singleLink.classList.remove('dcf-d-none');
+            singleLink.setAttribute('href', this.logOutUrl);
+        });
+
         window.dispatchEvent(new Event('idmStateSet'));
     }
 }
