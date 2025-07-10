8fcabc929bb389c0793a03e64951897af6d17748
Thomas Neumann <thomasneumann21800@gmail.com>
2025-07-10 15:35:32 -0500

    Delete correct downloads/ zip files

--- vite.wdnCleanupPlugin.js
+++ vite.wdnCleanupPlugin.js
@@ -16,12 +16,12 @@ const wdnCleanupPlugin = {
         await rm(resolve(__dirname, './wdn/templates_6.0/assets'), { recursive: true, force: true });
 
         // Delete zipped files if they are there
-        await rm(resolve(__dirname, './downloads/UNLTemplates.tar.gz'), { force: true });
-        await rm(resolve(__dirname, './downloads/UNLTemplates.zip'), { force: true });
-        await rm(resolve(__dirname, './downloads/wdn.tar.gz'), { force: true });
-        await rm(resolve(__dirname, './downloads/wdn.zip'), { force: true });
-        await rm(resolve(__dirname, './downloads/wdn_includes.tar.gz'), { force: true });
-        await rm(resolve(__dirname, './downloads/wdn_includes.zip'), { force: true });
+        await rm(resolve(__dirname, './downloads/UNLTemplates_6.0.tar.gz'), { force: true });
+        await rm(resolve(__dirname, './downloads/UNLTemplates_6.0.zip'), { force: true });
+        await rm(resolve(__dirname, './downloads/wdn_6.0.tar.gz'), { force: true });
+        await rm(resolve(__dirname, './downloads/wdn_6.0.zip'), { force: true });
+        await rm(resolve(__dirname, './downloads/wdn_includes_6.0.tar.gz'), { force: true });
+        await rm(resolve(__dirname, './downloads/wdn_includes_6.0.zip'), { force: true });
     },
     closeBundle() {
         // Copy files from dist to correct places in template_6.0
