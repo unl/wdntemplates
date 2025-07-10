6854bf0f860755b2591133d9945be72e5bad60bf
Thomas Neumann <thomasneumann21800@gmail.com>
2025-07-10 09:27:16 -0500

    Remove 5.3 directory

--- wdn/templates_5.3/js-src/pagination.babel.js
+++
@@ -1,5 +0,0 @@
-require(['dcf-pagination'], (DCFPaginationModule) => {
-  const paginationNavs = document.querySelectorAll('.dcf-pagination');
-  const pagination = new DCFPaginationModule.DCFPagination(paginationNavs);
-  pagination.initialize();
-});
