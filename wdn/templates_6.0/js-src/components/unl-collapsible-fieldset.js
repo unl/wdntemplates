0832eb216e8dd426a40babe631007bc75a274121
Ryan Dee <ryan@skoolbus39.com>
2025-10-29 12:36:59 -0500

    Remove padding from closed collapsible fieldset

--- wdn/templates_6.0/js-src/components/unl-collapsible-fieldset.js
+++ wdn/templates_6.0/js-src/components/unl-collapsible-fieldset.js
@@ -25,6 +25,8 @@ export default class UNLCollapsibleFieldset extends DCFCollapsibleFieldset {
 
         if (!('fieldsetClassListOff' in options)) {
             options.fieldsetClassListOff = [
+                'dcf-pt-0',
+                'dcf-pb-0',
                 'unl-collapsible-fieldset-close',
             ];
         }
